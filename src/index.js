fbd2a7569d6c60f89d8e865c3d13038955ad77e5
Thomas David <tom@mightysmile.com>
2021-01-29 16:48:48 +0100

    merge stash

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
-import DsPlugin from '@dooksa/ds-plugin'
-import plugin from './plugin'
+const plugin = () => ({
+  name: 'uiAvatar',
+  version: '1.0.0',
+  data: {
+    api: 'https://eu.ui-avatars.com/'
+  },
+  methods: {
+    createUrl (params) {
+      const url = new URL(this.api)
+      const searchParams = url.searchParams
 
-export default DsPlugin(plugin)
+      for (const key in params) {
+        if (Object.hasOwnProperty.call(params, key)) {
+          const value = params[key]
+
+          if (value) {
+            params.append(key, value)
+          }
+        }
+      }
+
+      url.search = searchParams.toString()
+
+      return url.toString()
+    },
+    generate (url) {
+      return window.fetch(url)
+        .then(response => {
+          return response.blob()
+        })
+        .then(blob => {
+          return URL.createObjectURL(blob)
+        })
+    }
+  }
+})
+
+module.exports = {
+  plugin,
+  default: plugin
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const path = require('path')
+<<<<<<< Updated upstream:webpack.config.js
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+=======
+const resolve = file => path.resolve(__dirname, file)
+let { plugin } = require('./src')
+plugin = plugin()
+>>>>>>> Stashed changes:webpack.prod.config.js
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
+<<<<<<< Updated upstream:webpack.config.js
     path: path.resolve(__dirname, 'dist')
+=======
+    path: resolve('dist'),
+    library: `${plugin.name}/v${plugin.version}`,
+    libraryTarget: 'global',
+    libraryExport: 'default',
+    globalObject: 'window.pluginLoader'
+>>>>>>> Stashed changes:webpack.prod.config.js
   },
   mode: 'development',
   devServer: {
