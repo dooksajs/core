9d6ca9edf57479b18317f85135e39ab495ae6451
Thomas David <tom@mightysmile.com>
2021-01-29 15:28:51 +0100

    pluginLoder get function

--- src/pluginLoader.js
+++ src/pluginLoader.js
@@ -1,11 +1,38 @@
-// import basePlugins from './basePlugins'
+import ScriptLoader from '@dooksa/script-loader'
+import basePlugins from './basePlugins'
 
-export default (name, version) => {
-  if (basePlugins[name]) {
-    if (version && basePlugins[name].items[version]) {
-      return basePlugins[name].items[version].current
-    } else {
-      return basePlugins[name]
-    }
+export default {
+  get (name, version) {
+    return new Promise((resolve, reject) => {
+      let pluginId = version ? `${name}/v${version}` : name
+      let scriptSrc
+
+      if (basePlugins[name]) {
+        if (version && basePlugins[name].items[version]) {
+          scriptSrc = basePlugins[name].items[version]
+        } else {
+          const current = basePlugins[name].current
+
+          scriptSrc = current.src
+          pluginId = `${name}/v${current.}`
+        }
+      }
+
+      if (scriptSrc) {
+        const script = new ScriptLoader({
+          id: 'plugin-' + pluginId,
+          src: scriptSrc,
+          apiVars: [pluginId]
+        })
+
+        script.load()
+          .then((vars) => resolve(vars[pluginId]))
+          .catch(error => reject(error))
+      } else {
+        const error = new Error('plugin not found: ' + pluginId)
+
+        reject(error)
+      }
+    })
   }
 }
