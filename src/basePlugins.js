264b09c3065845e07bd124dbe75d331d6c37a6d4
Thomas David <tom@mightysmile.com>
2021-01-29 16:15:48 +0100

    fix style

--- src/basePlugins.js
+++ src/basePlugins.js
@@ -1,7 +1,7 @@
 export default {
   uiAvatar: {
     current: {
-      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Findex.js?alt=media&token=71a90c1c-fbdd-45d7-b780-2f66a3b77990', 
+      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Findex.js?alt=media&token=71a90c1c-fbdd-45d7-b780-2f66a3b77990',
       version: '1.0.0'
     },
     items: {
