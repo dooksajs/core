81bb03a0319778387a1330bdbf99b83a00a50fed
Thomas David <tom@mightysmile.com>
2021-02-02 16:53:53 +0100

    add current list of basePlugins

--- src/basePlugins.js
+++ src/basePlugins.js
@@ -1,11 +1,38 @@
 const basePlugins = {
   dsFirebase: {
     current: {
-      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Ffirebase.js?alt=media&token=472a51b9-101c-414e-b020-4d8fd7bf6b09',
+      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F8.2.5%2Fds-firebase.js?alt=media&token=5baa0111-be76-4c05-9899-1d94d8b256a7',
       version: '8.2.5'
     },
     items: {
-      '8.2.5': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Ffirebase.js?alt=media&token=472a51b9-101c-414e-b020-4d8fd7bf6b09'
+      '8.2.5': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F8.2.5%2Fds-firebase.js?alt=media&token=5baa0111-be76-4c05-9899-1d94d8b256a7'
+    }
+  },
+  dsFirebaseAuth: {
+    current: {
+      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F8.2.5%2Fds-firebase-auth.js?alt=media&token=77d0a6d4-1e73-477e-8ff8-282aba79987f',
+      version: '8.2.5'
+    },
+    items: {
+      '8.2.5': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F8.2.5%2Fds-firebase-auth.js?alt=media&token=77d0a6d4-1e73-477e-8ff8-282aba79987f'
+    }
+  },
+  dsFirebaseAnalytics: {
+    current: {
+      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F8.2.5%2Fds-firebase-analytics.js?alt=media&token=dbfb5f77-db21-41d4-bf74-96592322b7bb',
+      version: '8.2.5'
+    },
+    items: {
+      '8.2.5': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F8.2.5%2Fds-firebase-analytics.js?alt=media&token=dbfb5f77-db21-41d4-bf74-96592322b7bb'
+    }
+  },
+  dsFirebasePerformance: {
+    current: {
+      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F8.2.5%2Fds-firebase-performance.js?alt=media&token=dce64687-09ae-4acd-958b-4f3250ba6136',
+      version: '8.2.5'
+    },
+    items: {
+      '8.2.5': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F8.2.5%2Fds-firebase-performance.js?alt=media&token=dce64687-09ae-4acd-958b-4f3250ba6136'
     }
   }
 }
