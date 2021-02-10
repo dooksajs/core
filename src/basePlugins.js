165e463a25bbdaa972001b6b2a42ec81a10732f9
Thomas David <tom@mightysmile.com>
2021-02-10 21:45:57 +0100

    update basePlugin data

--- src/basePlugins.js
+++ src/basePlugins.js
@@ -1,65 +1,59 @@
 const basePlugins = {
   dsFirebase: {
-    current: {
-      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.1%2Fds-firebase.js?alt=media&token=d215e1f7-b7a2-4058-b6a5-6f58c590678d',
-      version: '1.0.1'
-    },
+    currentVersion: '1.0.1',
     items: {
-      '1.0.1': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.1%2Fds-firebase.js?alt=media&token=d215e1f7-b7a2-4058-b6a5-6f58c590678d'
+      '1.0.1': {
+        src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.1%2Fds-firebase.js?alt=media&token=d215e1f7-b7a2-4058-b6a5-6f58c590678d',
+        version: '1.0.1'
+      }
     }
   },
   dsFirebaseAuth: {
-    current: {
-      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-auth.js?alt=media&token=1eb96203-8301-4e15-aec1-a176ebc920a6',
-      version: '1.0.0'
-    },
+    currentVersion: '1.0.0',
     items: {
-      '1.0.0': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-auth.js?alt=media&token=1eb96203-8301-4e15-aec1-a176ebc920a6'
+      '1.0.0': {
+        src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-auth.js?alt=media&token=1eb96203-8301-4e15-aec1-a176ebc920a6'
+      }
     }
   },
   dsFirebaseAnalytics: {
-    current: {
-      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.1%2Fds-firebase-analytics.js?alt=media&token=becfaed6-53d1-4949-bb2a-b024a9f20255',
-      version: '1.0.1'
-    },
+    currentVersion: '1.0.1',
     items: {
-      '1.0.1': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.1%2Fds-firebase-analytics.js?alt=media&token=becfaed6-53d1-4949-bb2a-b024a9f20255'
+      '1.0.1': {
+        src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.1%2Fds-firebase-analytics.js?alt=media&token=becfaed6-53d1-4949-bb2a-b024a9f20255'
+      }
     }
   },
   dsFirebaseFirestore: {
-    current: {
-      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-firestore.js?alt=media&token=b4fbc156-b7d3-4aa2-8101-baa1ff8a1e3d',
-      version: '1.0.0'
-    },
+    currentVersion: '1.0.0',
     items: {
-      '1.0.0': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-firestore.js?alt=media&token=b4fbc156-b7d3-4aa2-8101-baa1ff8a1e3d'
+      '1.0.0': {
+        src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-firestore.js?alt=media&token=b4fbc156-b7d3-4aa2-8101-baa1ff8a1e3d'
+      }
     }
   },
   dsFirebaseFunctions: {
-    current: {
-      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-functions.js?alt=media&token=3b6d151b-52c5-4534-837d-0a14f74b966a',
-      version: '1.0.0'
-    },
+    currentVersion: '1.0.0',
     items: {
-      '1.0.0': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-functions.js?alt=media&token=3b6d151b-52c5-4534-837d-0a14f74b966a'
+      '1.0.0': {
+        src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-functions.js?alt=media&token=3b6d151b-52c5-4534-837d-0a14f74b966a'
+      }
     }
   },
   dsFirebasePerformance: {
-    current: {
-      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-performance.js?alt=media&token=b64908ef-08ca-4104-8765-5ee7142be99e',
-      version: '1.0.0'
-    },
+    currentVersion: '1.0.0',
     items: {
-      '1.0.0': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-performance.js?alt=media&token=b64908ef-08ca-4104-8765-5ee7142be99e'
+      '1.0.0': {
+        src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-performance.js?alt=media&token=b64908ef-08ca-4104-8765-5ee7142be99e'
+      }
     }
   },
   dsFirebaseStorage: {
-    current: {
-      src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-storage.js?alt=media&token=4634e32a-de73-41e6-984a-82f5312bd6ee',
-      version: '1.0.0'
-    },
+    currentVersion: '1.0.0',
     items: {
-      '1.0.0': 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-storage.js?alt=media&token=4634e32a-de73-41e6-984a-82f5312bd6ee'
+      '1.0.0': {
+        src: 'https://firebasestorage.googleapis.com/v0/b/dooksa-sites-eu.appspot.com/o/plugins%2Fcore%2Ffirebase%2F1.0.0%2Fds-firebase-storage.js?alt=media&token=4634e32a-de73-41e6-984a-82f5312bd6ee'
+      }
     }
   }
 }
