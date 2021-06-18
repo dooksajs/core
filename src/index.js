7fcb0258ef0b3e184755d7940dbf90b1af1cbaac
Thomas David <tom@mightysmile.com>
2021-06-18 11:30:27 +0200

    DS-417 arrayMerge function

--- src/index.js
+++ src/index.js
@@ -89,6 +89,35 @@ export default {
       const listEnd = list.splice(indexEnd)
 
       return list.concat(listMiddle, listEnd)
+    },
+    /**
+     * Merge items into array with the option to flatten
+     * @param {Object} arrayMerge - The Object containing the data to merge items into an array
+     * @param {array} arrayMerge.list - The source array
+     * @param {Object[]} arrayMerge.items - An array of objects which contains the item and position it should be placed into the array
+     * @param {*} arrayMerge.items[].value - The value to be merged into the original array
+     * @param {number} arrayMerge.items[].index - The position the value will be placed into the original array
+     * @param {number} arrayMerge.flat - The position the items will be placed in the array
+     */
+    arrayMerge ({ list, items, flat }) {
+      let offset = 0
+      items.sort((a, b) => a._$index - b._$index)
+
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i]
+        const index = item._$index + offset
+        delete item._$index
+        const listMiddle = [item]
+        const listEnd = list.splice(index)
+        list = list.concat(listMiddle, listEnd)
+        ++offset
+      }
+
+      if (flat) {
+        list = list.flat(flat)
+      }
+
+      return list
     }
   }
 }
