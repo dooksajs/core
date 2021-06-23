605fae13d966c6bdd2b9e5b63aeed44f8bcbab71
Anthony David <adavid@adavid.com.au>
2021-06-23 15:01:22 +1000

    DS-220 Create pipeline build

---
+++ cypress/integration/ds-plugins.spec.js
@@ -0,0 +1,25 @@
+/* const plugins = new DsPlugins({ isDev: true })
+
+plugins.action('googleMaps/addMarker',
+  {
+    mapId: 'map',
+    options: {
+      lat: 0,
+      lng: 0
+    }
+  },
+  {
+    onSuccess: (result) => {
+      console.log(result)
+    },
+    onError: (error) => {
+      console.error(error)
+    }
+  }
+)
+*/
+describe('Dummy', () => {
+  it('does nothing', () => {
+    expect(true)
+  })
+})
