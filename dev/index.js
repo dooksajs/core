8033583f7821c01034c00ed80b0f0e2b737bc9eb
Thomas David <tom@mightysmile.com>
2021-07-08 13:07:12 +0200

    Updated dev to work with latest ds-plugins

--- dev/index.js
+++ dev/index.js
@@ -1,5 +1,19 @@
-import dsPlugins from '@dooksa/ds-plugins'
-import myPlugin from 'plugin'
+import DsPlugins from '@dooksa/ds-plugins'
+import { name, filename, version } from '../ds.plugin.config'
 
-const plugins = dsPlugins(myPlugin)
+const plugins = new DsPlugins({ isDev: true })
+
+// add plugin metadata
+plugins.addMetadata([
+  [[name], {
+    version,
+    script: {
+      src: `/dist/${filename}.js`,
+      integrity: 'sha512-NHj3yHLIurjFff1+vOIPJo7atTwujzZW+1YTG8hzJMqFhxED3JZx7Vpv+Pz/IEx7Hj38MCrNqalo+XkQqmjqNQ=='
+    }
+  }]
+])
+
+plugins.use({ name })
+
 console.log(plugins)
