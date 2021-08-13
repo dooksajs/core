d3c45007c850cfd9d587af1fbb81cd8812aae187
Anthony David <adavid@adavid.com.au>
2021-08-13 14:06:11 +1000

    DS-465 update tests for input form

--- cypress/integration/ds-plugin-operators-e2e.spec.js
+++ cypress/integration/ds-plugin-operators-e2e.spec.js
@@ -6,6 +6,12 @@ describe('ds-plugins e2e test run, using dev webpack', () => {
     cy.get('script').should('contain', 'main')
   })
   it('plugin operator ++x loads and increments', () => {
+    cy.get('#numeric-operand-0').type('1')
+    cy.get('button').click()
     cy.get('#data-increment').should('have.text', '2')
+  })
+  it('plugin operator ++x loads and nothing input', () => {
+    cy.get('button').click()
+    cy.get('#data-increment').should('have.text', '1')
   })
 })
