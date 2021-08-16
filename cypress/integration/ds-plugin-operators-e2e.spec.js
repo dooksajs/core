b148246b70009900a4be4ee47d2f007805445e87
Anthony David <adavid@adavid.com.au>
2021-08-16 10:17:44 +1000

    DS-471  test ds-plugin-operators compare()
    Initial tests for method
    bitbucketpipeline updated to document package versions for QA

--- cypress/integration/ds-plugin-operators-e2e.spec.js
+++ cypress/integration/ds-plugin-operators-e2e.spec.js
@@ -1,4 +1,4 @@
-describe('ds-plugin-operators e2e test run, using dev webpack', () => {
+describe('ds-plugin-operators e2e simple tests, using dev webpack', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080/')
   })
@@ -10,16 +10,16 @@ describe('ds-plugin-operators e2e test run, using dev webpack', () => {
     cy.get('#operand-0').type('1')
     cy.get('#operator').type('++x')
     cy.get('button').click()
-    cy.get('#data-eval').should('have.text', '1 ++x -> 2')
+    cy.get('#data-eval').should('have.text', '1 ++x  -> 2')
   })
   it('plugin operator ++x loads and nothing input', () => {
     cy.get('button').click()
     cy.get('#operator').type('++x')
-    cy.get('#data-eval').should('have.text', '  -> 1')
+    cy.get('#data-eval').should('have.text', '   -> 1')
   })
 })
 describe('ds-plugin-operators e2e operands input', () => {
-  it('get all the operators', () => {
+  it('test all the eval operators', () => {
     cy.exec('getOperators.sh')
       .then((result) => {
         if (result.code === 0) {
@@ -30,10 +30,33 @@ describe('ds-plugin-operators e2e operands input', () => {
             cy.get('#operator').clear()
             cy.get('#operator').type(bareOp)
             cy.get('button').click()
-            cy.get('#data-eval').should('not.contain', 'Sars-COV-2')
+            cy.get('#data-eval').should('not.contain', 'Sars-COV-2') // could be improved
             cy.log(op)
           })
         }
       })
+  })
+})
+
+describe('ds-plugin-operators compare() tests', () => {
+  it('test compare(values), values = ["1==1","&&","2==2"]', () => {
+    cy.debug()
+    cy.get('#operand-0').clear()
+    cy.get('#operand-0').type('1==1')
+    cy.get('#operand-1').clear()
+    cy.get('#operand-0').type('2==2')
+    cy.get('#operator').clear()
+    cy.get('#operator').type('&&')
+    cy.get('button').click()
+    cy.get('#data-compare').should('to.equal', 'true')
+  })
+  it('test compare(values), values = [,"&&",]', () => {
+    cy.debug()
+    cy.get('#operand-0').clear()
+    cy.get('#operand-1').clear()
+    cy.get('#operator').clear()
+    cy.get('#operator').type('&&')
+    cy.get('button').click()
+    cy.get('#data-compare').should('not.contain', 'Sars-COV-2')
   })
 })
--- dev/index.js
+++ dev/index.js
@@ -20,20 +20,37 @@ plugins.addMetadata({
 // use plugin eval with numeric inputs
 plugins.use({ name, plugin: dsOperators, onDemand: false })
 const params = (new URL(document.location)).searchParams
+const compareOps = ['&&', '||']
 if (params.has('operator')) {
   const op = ((`${params.get('operator')}` || '++x')) // default operator
-  plugins.action('dsOperators/eval', {
-    name: op,
-    values: [`${params.get('operand-0')}`, `${params.get('operand-1')}`]
-  },
-  {
-    onSuccess: (data) => {
-      const evalDisplay = document.querySelector('#data-eval')
-      evalDisplay.innerHTML = `${params.get('operand-0')} ${params.get('operator')}${params.get('operand-1')} -> ${data}`
+  if (compareOps.includes(op)) {
+    plugins.action('dsOperators/compare', {
+      values: [`${params.get('operand-0')}`, op, `${params.get('operand-1')}`]
     },
-    onError: (error) => {
-      const evalDisplay = document.querySelector('#data-eval')
-      evalDisplay.innerHTML = `${error}`
-    }
-  })
+    {
+      onSuccess: (data) => {
+        const compareDisplay = document.querySelector('#data-compare')
+        compareDisplay.innerHTML = `${params.get('operand-0')} ${params.get('operator')} ${params.get('operand-1')} -> ${data}`
+      },
+      onError: (error) => {
+        const compareDisplay = document.querySelector('#data-compare')
+        compareDisplay.innerHTML = `${error}`
+      }
+    })
+  } else {
+    plugins.action('dsOperators/eval', {
+      name: op,
+      values: [`${params.get('operand-0')}`, `${params.get('operand-1')}`]
+    },
+    {
+      onSuccess: (data) => {
+        const evalDisplay = document.querySelector('#data-eval')
+        evalDisplay.innerHTML = `${params.get('operand-0')} ${params.get('operator')} ${params.get('operand-1')} -> ${data}`
+      },
+      onError: (error) => {
+        const evalDisplay = document.querySelector('#data-eval')
+        evalDisplay.innerHTML = `${error}`
+      }
+    })
+  }
 }
