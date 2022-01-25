cdcd308276e261d59abfad64ffcf7afce3fad0fc
Thomas David <tom@mightysmile.com>
2022-01-25 15:32:27 +0100

    init

--- dev/index.js
+++ dev/index.js
@@ -1,16 +1,133 @@
-import DsPlugins from '@dooksa/ds-plugins'
-import { name, version } from '../ds.plugin.config'
-import myPlugin from 'plugin'
+import DsPlugin from '../../ds-plugins'
+import DsPluginTokens from '../../ds-plugin-tokens'
+import DsPluginOperators from '../../ds-plugin-operators'
+import DsPluginEvents from '../../ds-plugin-event'
+import DsPluginWorkflow from '../../ds-plugin-workflow'
+import DsPluginParameters from '../../ds-plugin-params'
+import DsPluginUtilities from '../../ds-plugin-utilities'
+import plugin from 'plugin'
 
-const plugins = new DsPlugins({ isDev: true })
+// these are part of the app build
+const plugins = [
+  {
+    item: {
+      name: DsPluginWorkflow.name,
+      version: DsPluginWorkflow.version
+    },
+    plugin: DsPluginWorkflow,
+    options: {
+      setupOnRequest: false
+    }
+  },
+  {
+    item: {
+      name: DsPluginEvents.name,
+      version: DsPluginEvents.version
+    },
+    plugin: DsPluginEvents,
+    options: {
+      setupOnRequest: false
+    }
+  },
+  {
+    item: {
+      name: DsPluginParameters.name,
+      version: DsPluginParameters.version
+    },
+    plugin: DsPluginParameters,
+    options: {
+      setupOnRequest: false
+    }
+  },
+  {
+    item: {
+      name: DsPluginTokens.name,
+      version: DsPluginTokens.version
+    },
+    plugin: DsPluginTokens,
+    options: {
+      setupOnRequest: false
+    }
+  },
+  {
+    item: {
+      name: DsPluginOperators.name,
+      version: DsPluginOperators.version
+    },
+    plugin: DsPluginOperators,
+    options: {
+      setupOnRequest: false
+    }
+  },
+  {
+    item: {
+      name: DsPluginUtilities.name,
+      version: DsPluginUtilities.version
+    },
+    plugin: DsPluginUtilities,
+    options: {
+      setupOnRequest: false
+    }
+  },
+  {
+    item: {
+      name: 'dsContent',
+      version: 1,
+      script: {
+        src: '/ds-content-1.0.0.js',
+        integrity: 'sha512-z5UgOiTBL4qHC08JHij7jkWr88NaQCioZ76gtJZzzY0VNtu0rPTfHl1h5hGzledJT3FOv0b1ReNw7eLu+5au0w=='
+      }
+    },
+    options: {
+      setupOnRequest: false,
+      onDemand: true
+    }
+  }
+]
 
-// add plugin metadata
-plugins.addMetadata([
-  [[name], {
-    version
-  }]
-])
+const additionalPlugins = [
+  {
+    item: {
+      name: 'dsTextEditor',
+      version: 1,
+      script: {
+        src: '/ds-text-editor-1.js',
+        integrity: 'sha512-FrkBOYWlLYD88oi0TyNZ78ETqifG6Wvbh6TLeWzNnDFnm9yexG9lFVZoSGOqUe5aMTsVdM2WeqXhp5aW+44xtw=='
+      }
+    },
+    options: {
+      onDemand: true,
+      setupOnRequest: false
+    }
+  }
+]
 
-plugins.use({ name, plugin: myPlugin })
+const pluginManager = new DsPlugin(plugin)
 
-console.log(plugins)
+pluginManager.init({
+  buildId: 1,
+  plugins,
+  additionalPlugins,
+  isDev: true
+})
+// pluginManager.use({
+//   name: 'dsManager',
+//   plugin: pluginManager,
+//   onDemand: false,
+//   setupOptions: {
+//     buildId: 1
+//   }
+// })
+console.log(pluginManager)
+// const plugins = new DsPlugins({ isDev: true })
+
+// // add plugin metadata
+// plugins.addMetadata([
+//   [[name], {
+//     version
+//   }]
+// ])
+
+// plugins.use({ name, plugin: myPlugin })
+
+// console.log(plugins)
--- src/index.js
+++ src/index.js
@@ -1,23 +1,350 @@
 import { name, version } from '../ds.plugin.config'
+import DsPlugin from '../../ds-plugins'
+import ScriptLoader from '@dooksa/script-loader'
 
 /**
  * Ds Plugin.
  * @module plugin
  */
-
 export default {
   name,
   version,
-  dependencies: [
-    // (Optional) Dependencies is a list of external dependent plugins
-  ],
   data: {
-    // (Optional) Data is a object of shared data
+    _methods: {},
+    _getter: {},
+    buildId: '',
+    additionalPlugins: {},
+    plugins: {},
+    currentBuildId: '',
+    currentPlugins: [],
+    currentAdditionalPlugins: [],
+    queue: {},
+    isLoaded: {},
+    setupOnRequest: {},
+    setupOnRequestQueue: {},
+    metadata: {},
+    context: {}
   },
-  setup () {
-    // (Optional) Setup initialises the plugin
+  setup ({
+    buildId,
+    plugins = [],
+    additionalPlugins = [],
+    store,
+    isDev
+  }) {
+    window.pluginLoader = {}
+    this.buildId = buildId
+
+    const getter = this._getter.bind(this)
+    const method = this._method.bind(this)
+    const action = this._action.bind(this)
+
+    this.context = {
+      $store: store,
+      isDev,
+      action,
+      method,
+      getter
+    }
+
+    this._add({
+      name,
+      methods: {
+        use: this.use
+      }
+    })
+
+    this.isLoaded[name] = true
+
+    for (let i = 0; i < plugins.length; i++) {
+      const data = plugins[i]
+
+      this.plugins[data.item.name] = data.item.version
+      this.use(data)
+    }
+
+    for (let i = 0; i < additionalPlugins.length; i++) {
+      const data = additionalPlugins[i]
+
+      this.additionalPlugins[data.item.name] = data.item.version
+      this.use(data)
+    }
   },
   methods: {
-    // (Optional) Methods contains the plugins functions
+    _actionExists (name) {
+      return (this._methods[name])
+    },
+    _addMetadata (data) {
+      if (this.queue[data.name]) {
+        delete this.queue[data.name]
+      }
+
+      this.isLoaded[data.name] = false
+      this.metadata[data.name] = data
+    },
+    _callbackWhenAvailable (name, callback) {
+      if (this._actionExists(name)) {
+        return callback()
+      }
+
+      const nameSplit = name.split('/')
+      const baseName = nameSplit[0]
+
+      if (this.queue[baseName] && !this.setupOnRequest[baseName]) {
+        this.isLoading(baseName).then(() => {
+          if (this._actionExists(name)) {
+            callback()
+          } else {
+            console.error('action does not exist: ' + name)
+          }
+        })
+      } else if (this.setupOnRequest[baseName]) {
+        this.isLoading(baseName).then(() => {
+          this.use({ item: { name: baseName } }).then(() => {
+            if (this._actionExists(name)) {
+              callback()
+            } else {
+              console.error('action does not exist: ' + name)
+            }
+          })
+        })
+      } else if (this.metadata[baseName]) {
+        this.use({ item: { name: baseName } }).then(() => {
+          if (this._actionExists(name)) {
+            callback()
+          } else {
+            console.error('action does not exist: ' + name)
+          }
+        })
+      } else {
+        console.error('action does not exist: ' + name)
+      }
+    },
+    _install (name, pluginImport, setupOnRequest) {
+      return new Promise((resolve, reject) => {
+        this.isLoaded[name] = false
+        this.setupOnRequest[name] = !!setupOnRequest
+
+        this._load(name, pluginImport)
+          .then(({ plugin, setupOptions }) => {
+            const queue = []
+
+            if (setupOnRequest) {
+              this.setupOnRequestQueue[name] = plugin
+
+              return resolve()
+            } else if (plugin.dependencies) {
+              const depQueue = []
+
+              for (let i = 0; i < plugin.dependencies.length; i++) {
+                const depPlugin = this.use({
+                  item: {
+                    name: plugin.dependencies[i].name,
+                    version: plugin.dependencies[i].version
+                  }
+                })
+
+                depQueue.push(depPlugin)
+              }
+
+              // wait for all dependencies to load
+              Promise.all(depQueue)
+                .then(() => {
+                  const dsPlugin = new DsPlugin(plugin, this.context)
+                  const setup = this._setup(dsPlugin, setupOptions)
+
+                  if (setup) {
+                    queue.push(setup)
+                  }
+
+                  // wait for plugin to setup
+                  Promise.all(queue)
+                    .then(() => {
+                      this._add(dsPlugin)
+                      this.isLoaded[name] = true
+
+                      resolve()
+                    })
+                    .catch(e => reject(e))
+                })
+                .catch(e => reject(e))
+            } else {
+              const dsPlugin = new DsPlugin(plugin, this.context)
+              const setup = this._setup(dsPlugin, setupOptions)
+
+              if (setup) {
+                queue.push(setup)
+              }
+
+              // wait for plugin to setup
+              Promise.all(queue)
+                .then(() => {
+                  this._add(dsPlugin)
+                  this.isLoaded[name] = true
+
+                  resolve()
+                })
+                .catch(e => reject(e))
+            }
+          })
+          .catch(e => reject(e))
+      })
+    },
+    _isLoading (name) {
+      return Promise.all(this.queue[name])
+    },
+    _load (name, plugin) {
+      return new Promise((resolve, reject) => {
+        let setupOptions = {}
+        let scriptOptions = {
+          id: name
+        }
+
+        if (this.metadata[name]) {
+          const metadata = this.metadata[name]
+
+          if (!metadata) {
+            const error = { statusCode: 404, message: 'Plugin not found: ' + name }
+            reject(error)
+          }
+
+          if (metadata.script) {
+            scriptOptions = { ...scriptOptions, ...metadata.script }
+          }
+
+          if (metadata.setupOptions) {
+            setupOptions = metadata.setupOptions
+          }
+        }
+
+        if (plugin) {
+          return resolve({ plugin, setupOptions })
+        } else if (this.setupOnRequestQueue[name]) {
+          plugin = this.setupOnRequestQueue[name]
+          delete this.setupOnRequestQueue[name]
+
+          return resolve({ plugin, setupOptions })
+        }
+
+        if (scriptOptions.src) {
+          const script = new ScriptLoader(scriptOptions)
+
+          script.load()
+            .then(() => {
+              const plugin = window.pluginLoader[name]
+
+              if (plugin) {
+                return resolve({ plugin, setupOptions })
+              } else {
+                const error = new Error('Plugin was not found: ' + name)
+
+                return reject(error)
+              }
+            })
+            .catch(error => reject(error))
+        } else {
+          const error = { statusCode: 404, message: 'Plugin not found: ' + name }
+
+          reject(error)
+        }
+      })
+    },
+    _action (name, params, callback = {}) {
+      this._callbackWhenAvailable(name, () => {
+        const onSuccess = callback.onSuccess
+        const onError = callback.onError
+        const pluginResult = this.method(name, params)
+
+        if (onError && pluginResult instanceof Error) {
+          if (onError.method) {
+            onError.method({ ...onError.params, results: pluginResult })
+          } else {
+            onError(pluginResult)
+          }
+        } else if (pluginResult instanceof Promise) {
+          Promise.resolve(pluginResult)
+            .then(results => {
+              if (onSuccess) {
+                if (onSuccess.method) {
+                  onSuccess.method({ ...onSuccess.params, results })
+                } else {
+                  onSuccess(results)
+                }
+              }
+            })
+            .catch(error => {
+              if (onError) {
+                if (onError.method) {
+                  onError.method({ ...onError.params, results: error })
+                } else {
+                  onError(error)
+                }
+              }
+            })
+        } else if (onSuccess) {
+          if (onSuccess.method) {
+            onSuccess.method({ ...onSuccess.params, results: pluginResult })
+          } else {
+            onSuccess(pluginResult)
+          }
+        }
+      })
+    },
+    _setup (plugin, options) {
+      const setup = plugin.init(options)
+
+      if (setup instanceof Promise) {
+        return setup
+      }
+    },
+    _add (plugin) {
+      if (plugin.methods) {
+        for (const key in plugin.methods) {
+          if (Object.hasOwnProperty.call(plugin.methods, key)) {
+            const method = plugin.methods[key]
+
+            this._methods[`${plugin.name}/${key}`] = method
+          }
+        }
+      }
+
+      if (plugin.getters) {
+        for (const key in plugin.getters) {
+          if (Object.hasOwnProperty.call(plugin.getters, key)) {
+            const getter = plugin.getters[key]
+
+            Object.defineProperty(this._getters, `${plugin.name}/${key}`, { get: getter })
+          }
+        }
+      }
+    },
+    _getter (name) {
+      return this._getter[name]
+    },
+    _method (name, params) {
+      try {
+        if (this._methods[name]) {
+          return this._methods[name](params)
+        } else {
+          throw new Error('Method "' + name + '" does not exist')
+        }
+      } catch (error) {
+        return error
+      }
+    },
+    use ({ item, options = {}, plugin }) {
+      if (options.onDemand) {
+        return this._addMetadata(item)
+      }
+
+      if (this.isLoaded[item.name]) {
+        return Promise.resolve()
+      } else if (this.queue[item.name] && !this.setupOnRequest[item.name]) {
+        return this._isLoading(item.name)
+      } else {
+        this._addMetadata(item)
+        this.queue[item.name] = [this._install(item.name, plugin, options.setupOnRequest)]
+      }
+    }
   }
 }
