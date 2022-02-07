915691246022a86dd5dde6e8188ee127080e1ced
Anthony David <adavid@adavid.com.au>
2022-02-08 04:56:07 +1000

    DS-567 unit test - check plugin name

--- cypress/integration/ds-plugins.spec.js
+++ cypress/integration/ds-plugins.spec.js
@@ -1,38 +1,22 @@
 // import DsPlugins from '@dooksa/ds-plugins'
-import DsPlugins from '../../src/index'
+import DsPlugin from '../../src/index'
 
-describe('test adding multiple plugin metadata', () => {
-  const plugins = new DsPlugins({ isDev: true })
-  plugins.addMetadata([
-    ['dsTest', {
-      version: '0.0.1'
-    }]
-  ])
-  it('metadata to be added', () => {
-    expect(plugins.metadata.dsTest.version).equal('0.0.1')
-  })
-  it('use a plugin with no methods', () => {
-    // **FIXME** This is rubbish
-    const pluginState = plugins.use({
-      name: 'dsTest'
-    })
-    // **TODO** assertion can be improved to test for an empty object
-    // unsuccessfully tried chai assertions .to.be.empty() and .to.be.an('object')
-    expect(typeof (pluginState)).to.equal('object')
-  })
-  it('use a plugin and add a plugin to it', () => {
-    plugins.use({
-      name: 'dsTest',
-      plugin: {
-        name: 'dsSubTest',
-        version: '0.0.1',
-        data: {
-          say: 'Hi',
-          age: 0
-        }
-      }
-    })
-    // **FIXME**
-    // expect(plugins.name.dsTest.plugin.version).equal('0.0.2')
+const dsTest = {
+  name: 'dsTest',
+  version: '1',
+  data: {
+    text: 'hello'
+  },
+  methods: {
+    sayHi (name) {
+      return this.text + ' ' + name
+    }
+  }
+}
+
+describe('check plugin created', () => {
+  it('check plugin name', () => {
+    const plugin = new DsPlugin(dsTest, [{ name: 'isDev', value: true }])
+    expect(plugin.name).equal('dsTest')
   })
 })
