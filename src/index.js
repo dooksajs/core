8a0d477cd30dc93d49027a2c2d9e8f6fcf2e2fec
Thomas David <tom@mightysmile.com>
2022-02-15 10:28:45 +0100

    Fix recursive rendering

--- src/index.js
+++ src/index.js
@@ -19,41 +19,78 @@ export default {
   ],
   data: {
     metadata: {},
+    content: {},
+    children: {},
     items: {}
   },
   methods: {
-    create (context, { id, items, start = 0 }) {
-      const components = []
+    create (context, id) {
+      const items = this._get(id)
 
-      for (let i = start; i < items.length; i++) {
-        const item = items[i]
+      return this._render(items)
+    },
+    set (context, { id, items, content, children }) {
+      this.children[id] = children
+      this.items[id] = items
+      this.content[id] = content
+    },
+    _content (id, index) {
+      return this.content[id][index]
+    },
+    _get (id) {
+      return {
+        id,
+        items: this.items[id],
+        children: this.children[id]
+      }
+    },
+    _render ({ id, items, children, childItems }) {
+      const fragments = new window.DocumentFragment()
 
-        if (item.component) {
-          const component = this.$method('dsElement/create', item.component)
+      for (let i = 0; i < children.length; i++) {
+        const item = childItems ? childItems[children[i]] : items[children[i]]
+        let component
 
-          if (item.children) {
-            for (let i = 0; i < item.children.length; i++) {
-              const index = item.children[i]
-              const childItem = items[index]
-
-              if (childItem.content) {
-                // normally we get the contentId from the widget
-                this.$action('dsContent/subscribe', { id: 'content123', item: component })
-              } else {
-                const children = this.createLayout(id, items, index)
-
-                for (let i = 0; i < children.length; i++) {
-                  component.appendChild(children[i])
-                }
-              }
-            }
-          }
-
-          components.push(component)
+        if (item.component.type === 'element') {
+          component = this.$method('dsElement/create', item.component)
+        } else {
+          component = this.$method('dsElement/createNode', item.component.id)
         }
+
+        if (item.children) {
+          const sibling = this._sibling(items, item.children)
+          const childComponent = this._render({
+            id,
+            items,
+            children: sibling.children,
+            childItems: sibling.items
+          })
+          component.appendChild(childComponent)
+        } else {
+          const content = this._content(id, item.content)
+
+          this.$method('dsContent/subscribe', { id: content.id, item: component })
+        }
+
+        fragments.append(component)
       }
 
-      return components
+      return fragments
+    },
+    _sibling (items, indexes) {
+      const newItems = {
+        items: [],
+        children: []
+      }
+
+      for (let i = 0; i < indexes.length; i++) {
+        const index = indexes[i]
+
+        newItems.items.push(items[index])
+        newItems.children.push(i)
+      }
+
+      return newItems
     }
   }
 }
