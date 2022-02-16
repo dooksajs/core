0868dc8892c1e600b12170c1e59cbb8297cb4107
Anthony David <adavid@adavid.com.au>
2022-02-16 10:50:36 +1000

    DS-600 fix package.json ds-plugin dependency

--- dev/index.js
+++ dev/index.js
@@ -1,16 +1,38 @@
-import DsPlugins from '@dooksa/ds-plugins'
-import { name, version } from '../ds.plugin.config'
-import Plugin from 'plugin'
+import DsPlugin from '@dooksa/ds-plugin'
+// import { name, version } from '../ds.plugin.config'
+// import Plugin from 'plugin'
 
-const plugins = new DsPlugins({ isDev: true })
+const myPlugin = {
+  name: 'myPlugin',
+  version: 1,
+  dependencies: [
+    // list of plugin dependencies
+  ],
+  data: {
+    //  plugin data
+  },
+  setup () {
+    // setup is the first function executed
+  },
+  getters: {
+    // getters
+    _privateGetter () {
+      // private getter name starts with '_'
+    },
+    publicMethod () {
+      // public getter
+    }
+  },
+  methods: {
+    _privateMethod (params) {
+      // private method name starts with '_'
+    },
+    publicMethod (context, params) {
+      // public method
+    }
+  }
+}
 
-// add plugin metadata
-plugins.addMetadata([
-  [[name], {
-    version
-  }]
-])
+const plugin = new DsPlugin(myPlugin, [])
 
-plugins.use({ name, plugin: Plugin })
-
-console.log(plugins)
+plugin.init()
