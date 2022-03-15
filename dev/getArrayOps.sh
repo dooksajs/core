40d5887395d4d4246a1ddd002c772fb0ca49fa13
Thomas David <tom@mightysmile.com>
2022-03-15 12:23:22 +0100

    DS-653 Removed unused files

--- dev/getArrayOps.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-PATHTOJS=src
-grep array.*\ \( $PATHTOJS/index.js |sed -e 's/^.*\(array[a-zA-Z0-9]*\)\ (.*$/\1/'
--- dev/getOperators.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-PATHTOJS=src
-cut -s -d: -f1 $PATHTOJS/index.js |grep \'|tr '\n' \,|tr -d [:space:]|sed -e 's/\,$//'
