2157b9a7d9953abb36b97a53ee18bd2e75cb0c85
Anthony David <adavid@adavid.com.au>
2022-03-24 05:41:36 +1000

    Eval remainder

--- cypress/integration/ds-plugin.spec.js
+++ cypress/integration/ds-plugin.spec.js
@@ -43,6 +43,52 @@ describe('Eval not equal', function () {
   })
 })
 
+describe('Eval greater than', function () {
+  it('can return true if the first operand is greater than the second operand.', function () {
+    expect(methods.eval({
+      name: '>',
+      values: [2, 1]
+    })).to.be.true
+  })
+
+  it('can return false if the first operand is less than the second operand.', function () {
+    expect(methods.eval({
+      name: '>',
+      values: [1, 2]
+    })).to.be.false
+  })
+
+  it('can return false if the first operand is equal to the second operand.', function () {
+    expect(methods.eval({
+      name: '>',
+      values: [1, 1]
+    })).to.be.false
+  })
+})
+
+describe('Eval greater than or equal', function () {
+  it('can return true if the first operand is greater than the second operand.', function () {
+    expect(methods.eval({
+      name: '>=',
+      values: [2, 1]
+    })).to.be.true
+  })
+
+  it('can return false if the first operand is less than the second operand.', function () {
+    expect(methods.eval({
+      name: '>=',
+      values: [1, 2]
+    })).to.be.false
+  })
+
+  it('can return true if the first operand is equal to the second operand.', function () {
+    expect(methods.eval({
+      name: '>=',
+      values: [1, 1]
+    })).to.be.true
+  })
+})
+
 describe('Eval less than', function () {
   it('can return true if the first operand is less than the second operand.', function () {
     expect(methods.eval({
@@ -89,48 +135,32 @@ describe('Eval less than or equal', function () {
   })
 })
 
-describe('Eval greater than', function () {
-  it('can return true if the first operand is greater than the second operand.', function () {
+describe('Eval remainder', function () {
+  it('can return 0 if the first operand is a multiple of the second operand.', function () {
     expect(methods.eval({
-      name: '>',
-      values: [2, 1]
-    })).to.be.true
+      name: '%',
+      values: [6, 2]
+    })).to.equal(0)
   })
 
-  it('can return false if the first operand is less than the second operand.', function () {
+  it('can return 0 if the first operand, expressed as a string, is a multiple of the second operand.', function () {
     expect(methods.eval({
-      name: '>',
-      values: [1, 2]
-    })).to.be.false
+      name: '%',
+      values: ['6', 2]
+    })).to.equal(0)
   })
 
-  it('can return false if the first operand is equal to the second operand.', function () {
-    expect(methods.eval({
-      name: '>',
-      values: [1, 1]
-    })).to.be.false
-  })
-})
-
-describe('Eval greater than or equal', function () {
-  it('can return true if the first operand is greater than the second operand.', function () {
-    expect(methods.eval({
-      name: '>=',
-      values: [2, 1]
-    })).to.be.true
+  it('can return NaN if the first operand is expressed as a non-numeric string.', function () {
+    assert.isNaN(methods.eval({
+      name: '%',
+      values: ['six', 2]
+    }), 'NaN is NaN')
   })
 
-  it('can return false if the first operand is less than the second operand.', function () {
+  it('can return 2 if the first operand is equal to a multiple of the second operand plus 2.', function () {
     expect(methods.eval({
-      name: '>=',
-      values: [1, 2]
-    })).to.be.false
-  })
-
-  it('can return true if the first operand is equal to the second operand.', function () {
-    expect(methods.eval({
-      name: '>=',
-      values: [1, 1]
-    })).to.be.true
+      name: '%',
+      values: [14, 4]
+    })).to.equal(2)
   })
 })
