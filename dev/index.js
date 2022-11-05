530714ff00f949cfba0a4cf273ebb649293ca889
Thomas David <tom@mightysmile.com>
2022-11-05 12:41:25 +0100

    DS-803 Build plugins as libraries

--- dev/index.js
+++
@@ -1,5 +0,0 @@
-import DsPlugin from '@dooksa/ds-plugin'
-import dsOperators from '../src'
-
-const plugin = new DsPlugin(dsOperators)
-console.log(plugin.methods.eval({ name: '==', values: [1, 1] }))
