431b83bba726170a7d0869cecd838bf7b65d4fe4
Thomas David <tom@mightysmile.com>
2022-11-28 11:18:23 +0100

    Add general components to this plugin

--- src/index.js
+++ src/index.js
@@ -8,6 +8,104 @@ export default {
   data: {
     items: {}
   },
+  components: [
+    {
+      name: 'button',
+      type: 'button',
+      events: ['click']
+    },
+    {
+      name: 'a',
+      type: 'link',
+      set: {
+        type: 'attribute',
+        value: 'href'
+      },
+      get: {
+        type: 'attribute',
+        value: 'href'
+      },
+      events: ['click', 'hover']
+    },
+    {
+      name: 'img',
+      type: 'image',
+      set: {
+        type: 'attribute',
+        value: [
+          {
+            name: 'src',
+            key: 'src'
+          },
+          {
+            name: 'alt',
+            key: 'alt'
+          }
+        ]
+      },
+      get: {
+        type: 'attribute',
+        value: [
+          {
+            name: 'src',
+            key: 'src'
+          },
+          {
+            name: 'alt',
+            key: 'alt'
+          }
+        ]
+      },
+      events: ['click']
+    },
+    {
+      name: 'div',
+      type: 'html',
+      set: {
+        type: 'setter',
+        value: 'innerHTML'
+      },
+      events: ['click']
+    },
+    {
+      name: 'input',
+      type: 'text',
+      events: ['input'],
+      get: {
+        type: 'getter',
+        value: 'value'
+      },
+      set: {
+        type: 'setter',
+        value: 'value'
+      }
+    },
+    {
+      name: '#text',
+      type: 'text',
+      get: {
+        type: 'getter',
+        value: [
+          {
+            name: 'textContent',
+            key: 'text'
+          }
+        ]
+      }
+    },
+    {
+      name: 'iconify-icon',
+      lazy: () => import('iconify-icon'),
+      set: [{
+        type: 'attribute',
+        value: 'icon'
+      }],
+      get: [{
+        type: 'attribute',
+        value: 'icon'
+      }]
+    }
+  ],
   methods: {
     /**
      * Get the component tag needed to create an element
