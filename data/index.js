4ef2fc92def88ec266d5122b80fac76d8a075a50
Thomas David <tom@mightysmile.com>
2022-12-09 10:29:33 +0100

    DS-845 Update plugin to use the parameter naming convention

--- data/index.js
+++ data/index.js
@@ -8,27 +8,12 @@ export default {
   template: 'uti2pklgnca9dot',
   path: 'dvhwaiuq2xvu9fd/',
   widgets: {
-    head: {
+    entry: {
       ve0qktek9pig0iz: ['_dvhwaiuq2xvu9fd_id$001']
     },
     content: {},
-    items: {
-      _dvhwaiuq2xvu9fd_id$001: [
-        // {
-        //   groupId: '_fxsvSTurDPdbRakFLxCK4p',
-        //   instanceId: '_FxsvSTurDPdbRakFLxCK4p',
-        //   layout: {
-        //     default: { id: '_1Diikn4OcJ5Uz7p6zK0gTg', templateId: 'yp9tx9ke4m71ghm' }
-        //   }
-        // },
-        // {
-        //   groupId: '_5LVH2JpsXEHjnbcCPq7rGW',
-        //   instanceId: '_tvfsLgAGGQlONQbs4kFIRs',
-        //   layout: {
-        //     default: { id: '_1Diikn4OcJ5Uz7p6zK0gTW', templateId: 'y979zrlxovvopvt' }
-        //   }
-        // }
-      ]
+    sections: {
+      _dvhwaiuq2xvu9fd_id$001: []
     }
   }
 }
