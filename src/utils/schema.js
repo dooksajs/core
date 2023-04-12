59961495346dd537fb50d24aa1b8845fcd892c59
Thomas David <tom@mightysmile.com>
2023-04-12 12:52:41 +0200

    DS-878 Create schema validation

---
+++ src/utils/schema.js
@@ -0,0 +1,210 @@
+export default {
+  entry (context, item, name) {
+    const props = ['items', 'properties', 'patternProperties', 'type', 'name']
+    const idProps = ['defaultId', 'suffixId', 'prefixId']
+    const entry = { type: item.type }
+    const options = {}
+    let additionalProperties
+    const id = {}
+
+    let hasOptions = false
+    let hasId = false
+
+    if (name) {
+      entry.name = name
+    }
+
+    for (const key in item) {
+      if (Object.hasOwnProperty.call(item, key)) {
+        let value = item[key]
+
+        // prepare additionalProperties
+        if (key === 'properties') {
+          additionalProperties = Object.keys(value)
+        }
+
+        // collect options
+        if (idProps.includes(key)) {
+          const idKey = key.substring(-1, key.length - 2)
+
+          // add plugin context to function
+          if (typeof value === 'function') {
+            value = value.bind(context)
+          }
+
+          id[idKey] = value
+          hasId = true
+        } else if (!props.includes(key)) {
+          options[key] = value
+          hasOptions = true
+        }
+      }
+    }
+
+    if (hasOptions) {
+      // add allowed properties
+      if (options.additionalProperties === false) {
+        options.additionalProperties = additionalProperties
+      }
+
+      if (typeof options.default === 'function') {
+        options.default = options.default.bind(context)
+      }
+
+      entry.options = options
+    }
+
+    if (hasId) {
+      entry.id = id
+    }
+
+    return entry
+  },
+  create (context, items) {
+    const result = {
+      entry: this.entry(context, items)
+    }
+
+    if (items.properties || items.patternProperties) {
+      result.properties = []
+      result.patternProperties = []
+      result.entryProperties = items.properties
+      result.entryPatternProperties = items.patternProperties
+
+      for (const key in items.patternProperties) {
+        if (Object.hasOwnProperty.call(items.patternProperties, key)) {
+          const property = items.patternProperties[key]
+          const hasProperties = this.hasProperties(property)
+
+          if (hasProperties) {
+            result.patternProperties.push({
+              name: key,
+              type: property.type
+            })
+          } else {
+            const entry = this.entry(context, property, key)
+
+            result.patternProperties.push(entry)
+          }
+        }
+      }
+
+      for (const key in items.properties) {
+        if (Object.hasOwnProperty.call(items.properties, key)) {
+          const property = items.properties[key]
+          const hasProperties = this.hasProperties(property)
+
+          if (hasProperties) {
+            result.properties.push({
+              name: key,
+              type: property.type
+            })
+          } else {
+            const entry = this.entry(context, property, key)
+
+            result.properties.push(entry)
+          }
+        }
+      }
+    } else if (items.items) {
+      result.items = items.items
+    }
+
+    return result
+  },
+  hasProperties (entry) {
+    return !!(entry.items || entry.properties || entry.patternProperties)
+  },
+  processProperties (context, id, properties = [], options, relation, entries) {
+    const entryProperties = []
+    let hasRelation = false
+
+    for (let i = 0; i < properties.length; i++) {
+      const property = properties[i]
+      const name = property.name
+
+      if (options && options.required && options.required.includes(name)) {
+        property.required = true
+      }
+
+      const hasProps = this.hasProperties(property)
+
+      if (hasProps) {
+        if (property.type === 'object') {
+          const item = this.create(context, property)
+
+          this.object(context, id + '/' + name, item.entry, item.properties, item.patternProperties, item.entry.options, entries)
+        } else {
+          this.process(context, id + '/' + name, property, entries, true)
+        }
+      }
+
+      if (property.options && property.options.relation) {
+        hasRelation = true
+        relation.push([name, property.options.relation])
+      }
+
+      entryProperties.push(property)
+    }
+
+    return {
+      properties: entryProperties,
+      hasRelation
+    }
+  },
+  object (context, id, entry, properties, patternProperties, options, entries) {
+    const item = {
+      id,
+      entry
+    }
+
+    const relation = []
+    const entryProperties = this.processProperties(context, id, properties, options, relation, entries)
+    const entryPatternProperties = this.processProperties(context, id, patternProperties, options, relation, entries)
+
+    if (entryProperties.hasRelation || entryPatternProperties.hasRelation) {
+      item.relation = relation
+    }
+
+    if (entryProperties.properties.length) {
+      item.entry.properties = entryProperties.properties
+    }
+
+    if (entryPatternProperties.properties.length) {
+      item.entry.patternProperties = entryPatternProperties.properties
+    }
+
+    entries.push(item)
+  },
+  process (context, id, source, entries = [], head = false) {
+    const item = this.create(context, source)
+
+    if (!head) {
+      id = id + '/items'
+    }
+
+    if (item.properties && item.patternProperties) {
+      this.object(context, id, item.entry, item.properties, item.patternProperties, item.entry.options, entries)
+    } else if (item.items) {
+      this.process(context, id, item.items, entries)
+
+      const entry = { id, entry: item.entry }
+
+      if (item.entry.options && item.entry.options.relation) {
+        entry.relation = [item.entry.options.relation]
+      }
+
+      entries.push(entry)
+    } else {
+      const entry = { id, entry: item.entry }
+
+      if (item.entry.options && item.entry.options.relation) {
+        entry.relation = [item.entry.options.relation]
+      }
+
+      entries.push(entry)
+    }
+
+    return entries
+  }
+}
