f576cc2f6e849c5f684002a4b764448df00d7513
Thomas David <tom@mightysmile.com>
2023-04-12 12:55:15 +0200

    DS-884 Migrate plugins to use dsData

--- src/index.js
+++ src/index.js
@@ -4,79 +4,194 @@
 export default {
   name: 'dsTemplate',
   version: 1,
-  dependencies: [
-    {
-      name: 'dsUtilities',
-      version: 1
-    }
-  ],
   data: {
-    entry: {},
-    items: {},
-    components: {},
-    modifiers: {}
+    entry: {
+      default: {},
+      schema: {
+        type: 'collection',
+        items: {
+          type: 'string',
+          relation: 'dsTemplate/items'
+        }
+      }
+    },
+    items: {
+      default: {},
+      schema: {
+        type: 'collection',
+        items: {
+          type: 'object',
+          properties: {
+            events: {
+              type: 'object',
+              patternProperties: {
+                '^[0-9]+$': {
+                  type: 'object',
+                  patternProperties: {
+                    '^[0-9]+$': {
+                      type: 'object',
+                      properties: {
+                        click: {
+                          type: 'array',
+                          items: {
+                            type: 'string',
+                            relation: 'dsAction/sequenceActions'
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            },
+            layouts: {
+              type: 'array',
+              items: {
+                type: 'string',
+                relation: 'dsLayout/items'
+              }
+            },
+            content: {
+              type: 'object',
+              patternProperties: {
+                '^[0-9]+$': {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      value: {
+                        type: 'object'
+                      },
+                      type: {
+                        type: 'array',
+                        item: {
+                          type: 'string'
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   },
   /** @lends dsTemplate */
   methods: {
     create ({
-      id,
-      entry,
-      sectionId = this.$method('dsUtilities/generateId'),
-      instanceId,
-      groupId = this.$method('dsUtilities/generateId'),
-      defaultContent,
+      dsTemplateId,
+      dsTemplateEntryId,
+      dsWidgetPrefixId,
+      dsWidgetSectionId = this.$method('dsData/generateId'),
+      dsWidgetInstanceId = this.$method('dsData/generateId'),
+      dsWidgetGroupId = this.$method('dsData/generateId'),
+      dsWidgetMode = 'default',
+      dsWidgetContent,
       modifiers = {},
-      view = 'default',
-      head = false
+      head = true
     }) {
+      const entryId = this.$getDataValue({
+        name: 'dsTemplate/entry',
+        id: dsTemplateId
+      })
+
+      const item = this.$getDataValue({
+        name: 'dsTemplate/items',
+        id: entryId.value
+      })
       // get template
-      let item = this.items[entry] || this.items[this.entry[id]]
+      // const item = this.items[dsTemplateEntryId] || this.items[this.entry[dsTemplateId]]
 
-      if (!item) {
-        return new Promise(resolve => {
-          this._fetch(id)
-            .then(() => {
-              entry = this.entry[id]
-              item = this.items[entry]
-
-              const result = this._constructor(id, entry, item, sectionId, instanceId, groupId, defaultContent, modifiers, view, head)
-
-              resolve(result)
-            })
-        })
+      if (!item.isEmpty) {
+        return this._constructor(
+          dsTemplateId,
+          dsTemplateEntryId,
+          item.value,
+          dsWidgetSectionId,
+          dsWidgetPrefixId,
+          dsWidgetInstanceId,
+          dsWidgetGroupId,
+          dsWidgetContent,
+          modifiers,
+          dsWidgetMode,
+          head
+        )
       }
 
-      return this._constructor(id, entry, item, sectionId, instanceId, groupId, defaultContent, modifiers, view, head)
+      return new Promise(resolve => {
+        this._fetch(dsTemplateId)
+          .then(() => {
+            this.$setDataValue({
+              name: 'dsTemplates/entry',
+              source: dsTemplateId,
+              options: {
+                id: dsTemplateId
+              }
+            })
+
+            const result = this._constructor(
+              dsTemplateId,
+              dsTemplateEntryId,
+              item,
+              dsWidgetSectionId,
+              dsWidgetPrefixId,
+              dsWidgetInstanceId,
+              dsWidgetGroupId,
+              dsWidgetContent,
+              modifiers,
+              dsWidgetMode,
+              head
+            )
+
+            resolve(result)
+          })
+      })
     },
-    _constructor (id, entry, item, sectionId, instanceId, groupId, defaultContent, modifiers, view, head) {
-      let layoutEntry = ''
+    _constructor (
+      id,
+      entry,
+      item,
+      dsWidgetSectionId,
+      dsWidgetPrefixId,
+      instanceId,
+      groupId,
+      defaultContent,
+      modifiers,
+      view,
+      head
+    ) {
+      let dsLayoutEntryId = ''
       const result = {
         layoutEntry: '',
         widgets: {
           items: {},
-          content: {},
-          layout: {}
+          content: {}
         },
         content: {
           value: {},
           type: {}
         }
       }
+
       if (head) {
         const layoutId = item.layouts[0]
         const layout = {}
-        const itemId = sectionId + instanceId + '_' + view
         const i = 0
 
         if (item.content[i]) {
           const [contentRefs, content] = this._createContent(item.content[i], view, groupId, defaultContent, modifiers)
-          const modifierId = entry + layoutId
+          const instanceContent = this.$setDataValue({
+            name: 'dsWidget/instanceContent',
+            source: contentRefs,
+            options: {
+              id: instanceId,
+              suffixId: dsWidgetPrefixId
+            }
+          })
 
-          if (modifiers[modifierId]) {
-            result.widgets.layout = { [itemId]: modifiers[modifierId] }
-          }
-
-          result.widgets.content[itemId] = contentRefs
+          result.widgets.content[instanceContent.id] = contentRefs
           result.content = content
         }
 
@@ -89,45 +204,43 @@ export default {
           for (const key in events) {
             if (Object.hasOwnProperty.call(events, key)) {
               const event = events[key]
-              const eventId = instanceId + '_' + key.padStart(4, '0')
+              const eventId = instanceId + key.padStart(4, '0') + '_'
 
               for (const key in event) {
                 if (Object.hasOwnProperty.call(event, key)) {
                   const actions = event[key]
 
-                  result.events[eventId + '_' + key] = actions
+                  result.events[eventId + key] = actions
                 }
               }
             }
           }
         }
 
-        layoutEntry = layoutId
+        dsLayoutEntryId = layoutId
         layout[view] = { id: layoutId }
-
-        this.$method('dsWidget/setLoaded', { id: itemId, value: true })
       } else {
         for (let i = 0; i < item.layouts.length; i++) {
           const layoutId = item.layouts[i]
           const layout = {}
-          const instanceId = this.$method('dsUtilities/generateId')
-          const itemId = sectionId + instanceId + '_default'
+          const instanceId = this.$method('dsData/generateId')
+          const itemId = instanceId + 'default'
 
           // create content
           if (item.content && item.content[i]) {
             const [contentRefs, content] = this._createContent(item.content[i], view, groupId, defaultContent, modifiers)
 
-            const modifierId = entry + layoutId
-
-            if (modifiers[modifierId]) {
-              if (!result.widgets.layout) {
-                result.widgets.layout = {}
-              }
-
-              result.widgets.layout[sectionId + instanceId + '_default'] = modifiers[modifierId]
-            }
-
             result.widgets.content[itemId] = contentRefs
+
+            this.$setDataValue({
+              name: 'dsWidget/instanceContent',
+              payload: result.widgets.content,
+              options: {
+                id: instanceId,
+                suffixId: 'default'
+              }
+            })
+
             result.content.value = { ...result.content.value, ...content.value }
             result.content.type = { ...result.content.type, ...content.type }
           }
@@ -141,7 +254,7 @@ export default {
             for (const key in events) {
               if (Object.hasOwnProperty.call(events, key)) {
                 const event = events[key]
-                const eventId = instanceId + '_' + key.padStart(4, '0') + '_' + event.on
+                const eventId = instanceId + key.padStart(4, '0') + '_' + event.on
 
                 result.events[eventId] = event.action
               }
@@ -151,51 +264,79 @@ export default {
           // Is this set to default because of the depth?
           layout.default = { id: layoutId }
 
-          if (!result.widgets.sections[sectionId]) {
-            result.widgets.sections[sectionId] = [{ groupId, instanceId, layout }]
+          if (!result.widgets.sections[dsWidgetSectionId]) {
+            result.widgets.sections[dsWidgetSectionId] = [instanceId]
           } else {
-            result.widgets.sections[sectionId].push({ groupId, instanceId, layout })
+            result.widgets.sections[dsWidgetSectionId].push(instanceId)
           }
-
-          this.$method('dsWidget/setLoaded', { id: itemId, value: true })
         }
       }
 
       if (result.events) {
-        this.$method('dsEvent/set', result.events)
+        this.$setDataValue({
+          name: 'dsEvent/listeners',
+          source: result.events,
+          options: {
+            source: {
+              merge: true
+            }
+          }
+        })
       }
 
       // Set content
       if (result.content) {
         if (result.content.value) {
-          this.$method('dsContent/setValues', result.content.value)
+          this.$setDataValue({
+            name: 'dsContent/value',
+            source: result.content.value,
+            options: {
+              source: {
+                merge: true
+              }
+            }
+          })
         }
 
         if (result.content.type) {
-          this.$method('dsContent/setTypes', result.content.type)
+          this.$setDataValue({
+            name: 'dsContent/type',
+            source: result.content.type,
+            options: {
+              source: {
+                merge: true
+              }
+            }
+          })
         }
       }
 
       // Set widgets
-      if (result.widgets) {
-        if (result.widgets.sections) {
-          this.$method('dsWidget/setSection', result.widgets.sections)
-        }
-
-        if (result.widgets.layout) {
-          this.$method('dsWidget/setLayout', result.widgets.layout)
-        }
-
-        if (result.widgets.content) {
-          const dsPageId = this.$method('dsRouter/getCurrentId')
-
-          this.$method('dsWidget/setContent', { dsPageId, items: result.widgets.content })
-        }
+      if (result.widgets.sections) {
+        this.$setDataValue({
+          name: 'dsWidget/sections',
+          source: result.widgets.sections,
+          options: {
+            source: {
+              merge: true
+            }
+          }
+        })
       }
 
-      return [sectionId, layoutEntry, result]
+      return {
+        dsWidgetSectionId,
+        dsLayoutEntryId,
+        result
+      }
     },
-    _createContent (items, view, groupId, defaultContent = [], modifiers) {
+    _createContent (
+      items,
+      dsWidgetMode = 'default',
+      dsWidgetGroupId,
+      defaultContent = [],
+      modifiers
+    ) {
       const contentRefs = []
       const content = {
         value: {},
@@ -204,15 +345,18 @@ export default {
 
       for (let i = 0; i < items.length; i++) {
         const item = items[i]
-        const [type, isPermanent] = item.type
+        const [type, isTemporary] = item.type
         let id, exists
 
-        if (defaultContent.length && isPermanent) {
+        if (defaultContent.length && !isTemporary) {
           for (let i = 0; i < defaultContent.length; i++) {
             const contentId = defaultContent[i]
-            const contentType = this.$method('dsContent/getType', contentId)
+            const dsContentType = this.$getDataValue({
+              name: 'dsContent/type',
+              id: contentId
+            })
 
-            if (type === contentType[0]) {
+            if (type === dsContentType.value) {
               id = contentId
               defaultContent.splice(i, 1)
               exists = true
@@ -220,27 +364,23 @@ export default {
             }
           }
         } else {
-          id = this.$method('dsUtilities/generateId')
+          id = this.$method('dsData/generateId')
         }
 
         contentRefs.push(id)
 
         if (!exists) {
-          content.type[id] = [type, isPermanent]
+          content.type[id] = { name: type, isTemporary }
 
           if (type === 'section') {
-            const contentValue = {}
-
             for (let i = 0; i < item.value.length; i++) {
-              const [lang, entry] = item.value[i]
-              const [sectionId] = this.create({ entry, defaultContent, groupId, view, modifiers })
+              const [lang, dsTemplateEntryId] = item.value[i]
+              const [sectionId] = this.create({ dsTemplateEntryId, defaultContent, dsWidgetGroupId, dsWidgetMode, modifiers, head: false })
 
-              contentValue[lang] = sectionId
+              content.value[id + lang] = sectionId
             }
-
-            content.value[id] = contentValue
           } else {
-            content.value[id] = item.value
+            content.value[id + dsWidgetMode] = item.value
           }
         }
       }
@@ -252,7 +392,55 @@ export default {
         this.$action('dsDatabase/getOne', { collection: 'widgetTemplates', id },
           {
             onSuccess: (record) => {
-              this.set({ id: record.id, item: record })
+              // add templates
+              this.$setDataValue({
+                name: 'dsTemplate/entry',
+                source: record.templateEntry,
+                options: {
+                  id: record.templateEntry
+                }
+              })
+
+              this.$setDataValue({
+                name: 'dsTemplate/items',
+                source: record.template,
+                options: {
+                  source: {
+                    merge: true
+                  }
+                }
+              })
+
+              this.$setDataValue({
+                name: 'dsLayout/items',
+                source: record.layouts.items,
+                options: {
+                  source: {
+                    merge: true
+                  }
+                }
+              })
+
+              this.$setDataValue({
+                name: 'dsLayout/entry',
+                source: record.layouts.head,
+                options: {
+                  source: {
+                    merge: true
+                  }
+                }
+              })
+
+              this.$setDataValue({
+                name: 'dsComponent/items',
+                source: record.components,
+                options: {
+                  source: {
+                    merge: true
+                  }
+                }
+              })
+
               resolve(record)
             },
             onError: (error) => {
@@ -262,29 +450,6 @@ export default {
           }
         )
       })
-    },
-    set ({ id, item }) {
-      this.entry[id] = item.templateEntry
-
-      if (item.template) {
-        this.items = { ...this.items, ...item.template }
-      }
-
-      if (item.modifiers) {
-        this.modifiers[id] = item.modifiers
-      }
-
-      if (item.layouts) {
-        this.$method('dsLayout/setItems', item.layouts.items)
-
-        if (item.layouts.head) {
-          this.$method('dsLayout/setHead', item.layouts.head)
-        }
-      }
-
-      if (item.components) {
-        this.$method('dsComponent/set', item.components)
-      }
     }
   }
 }
