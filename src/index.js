7cd6c7e3c0a39997ecfe7fc2adfe2f17d17054a1
Thomas David <tom@mightysmile.com>
2023-04-12 12:54:30 +0200

    DS-884 Migrate plugins to use dsData

--- src/index.js
+++ src/index.js
@@ -6,154 +6,304 @@ export default {
   name: 'dsLayout',
   version: 1,
   data: {
-    metadata: {},
-    event: {},
-    items: {},
-    head: {},
-    components: {},
-    modifiers: {}
-  },
-  methods: {
-    getComponents ({ dsWidgetInstanceId, dsWidgetView = 'default' }) {
-      return this.components[dsWidgetView + dsWidgetInstanceId]
+    events: {
+      default: {},
+      schema: {
+        type: 'collection',
+        items: {
+          type: 'object'
+        }
+      }
     },
-    render ({ dsLayoutId, dsWidgetSectionId, dsWidgetInstanceId, dsWidgetView = 'default', dsViewId = 'appElement', dsWidgetPrefixId, language = 'default' }) {
-      let components = this.getComponents({ dsWidgetInstanceId, dsWidgetView })
+    items: {
+      default: {},
+      schema: {
+        type: 'collection',
+        items: {
+          type: 'array',
+          items: {
+            type: 'object',
+            required: ['componentId'],
+            properties: {
+              contentIndex: { type: 'number' },
+              parentIndex: { type: 'number' },
+              children: {
+                type: 'array',
+                items: { type: 'number' }
+              },
+              componentId: {
+                type: 'string',
+                relation: 'dsComponent/items'
+              }
+            }
+          }
+        }
+      }
+    },
+    entry: {
+      default: {},
+      schema: {
+        type: 'collection',
+        items: {
+          type: 'array',
+          items: { type: 'number' }
+        }
+      }
+    },
+    components: {
+      default: {},
+      schema: {
+        type: 'collection',
+        items: {
+          type: 'object'
+        }
+      }
+    }
+  },
+  /** @lends dsLayout */
+  methods: {
+    attach ({
+      dsLayoutId,
+      dsWidgetSectionId,
+      dsWidgetInstanceId,
+      dsWidgetMode,
+      dsViewId,
+      dsWidgetPrefixId
+    }) {
+      const language = this.$getDataValue({ name: 'dsMetadata/language' })
+      let components = this.$getDataValue({
+        name: 'dsLayout/components',
+        id: dsWidgetInstanceId,
+        prefixId: dsWidgetMode
+      })
 
-      if (!components) {
-        const items = this._getItem(dsLayoutId)
-        const head = this._getHead(dsLayoutId)
-        const events = this._getEvent(dsLayoutId)
+      if (!dsViewId) {
+        dsViewId = this.$getDataValue({
+          name: 'dsWidget/sectionView',
+          id: dsWidgetSectionId
+        })
+      }
 
-        components = this._create(dsLayoutId, items, head, items, events, dsWidgetSectionId, dsWidgetInstanceId, dsViewId, dsWidgetPrefixId, language, dsWidgetView)
+      if (components.isEmpty) {
+        const items = this.$getDataValue({
+          name: 'dsLayout/items',
+          id: dsLayoutId
+        })
+        const entry = this.$getDataValue({
+          name: 'dsLayout/entry',
+          id: dsLayoutId
+        })
+        const events = this.$getDataValue({
+          name: 'dsLayout/events',
+          id: dsLayoutId
+        })
+
+        components = this._create(
+          dsLayoutId,
+          items.value,
+          entry.value,
+          items.value,
+          events.value,
+          dsWidgetSectionId,
+          dsWidgetInstanceId,
+          dsViewId,
+          dsWidgetPrefixId,
+          language.value,
+          dsWidgetMode
+        )
 
         // ISSUE: [DS-810] This is unnecessary loop if _create function can attach components
         for (let i = 0; i < components.length; i++) {
           const component = components[i]
 
-          this._attachComponent(components, component, dsWidgetSectionId, dsWidgetInstanceId, dsWidgetView, dsViewId, dsWidgetPrefixId, language)
+          this._attachComponent(
+            components,
+            component,
+            dsWidgetSectionId,
+            dsWidgetInstanceId,
+            dsWidgetMode,
+            dsViewId,
+            dsWidgetPrefixId,
+            language.value
+          )
         }
 
-        this._setComponents(dsWidgetInstanceId, components, dsWidgetView)
+        // this._setComponents(dsWidgetInstanceId, dsWidgetPrefixId, dsWidgetMode, components)
       } else {
         for (let i = 0; i < components.length; i++) {
           const component = components[i]
 
-          this._attachComponent(components, component, dsWidgetSectionId, dsWidgetInstanceId, dsWidgetView, dsViewId, dsWidgetPrefixId, language)
+          this._attachComponent(
+            components,
+            component,
+            dsWidgetSectionId,
+            dsWidgetInstanceId,
+            dsWidgetMode,
+            dsViewId,
+            dsWidgetPrefixId,
+            language
+          )
         }
       }
     },
-    setHead (head) {
-      this.head = { ...this.head, ...head }
-    },
-    setModifiers (items) {
-      this.modifiers = { ...this.modifiers, ...items }
-    },
-    setItems (items) {
-      this.items = { ...this.items, ...items }
-    },
-    _attachComponent (components, component, dsWidgetSectionId, dsWidgetInstanceId, dsWidgetView, dsViewParentId, dsWidgetPrefixId, language) {
+    _attachComponent (
+      components,
+      component,
+      dsWidgetSectionId,
+      dsWidgetInstanceId,
+      dsWidgetMode,
+      dsViewParentId,
+      dsWidgetPrefixId,
+      language
+    ) {
       let dsViewId = dsViewParentId
 
-      if (component.viewId) {
-        dsViewParentId = !isNaN(component.parentIndex) ? components[component.parentIndex].viewId : dsViewParentId
+      if (component.dsViewId) {
+        dsViewParentId = Number.isInteger(component.parentIndex) ? components[component.parentIndex].dsViewId : dsViewParentId
 
-        dsViewId = component.viewId
+        dsViewId = component.dsViewId
 
         this.$method('dsView/append', {
-          dsViewId: component.viewId,
+          dsViewId: component.dsViewId,
           dsViewParentId
         })
       }
 
-      if (!isNaN(component.contentIndex)) {
-        const dsContentId = this.$method('dsWidget/getContentItem', {
-          dsWidgetSectionId,
-          dsWidgetInstanceId,
-          dsWidgetPrefixId,
-          dsViewId: dsViewParentId,
-          dsWidgetView,
-          contentIndex: component.contentIndex
-        })
-        const dsContentType = this.$method('dsContent/getType', dsContentId)
+      if (!Number.isInteger(component.contentIndex)) {
+        return
+      }
 
-        if (dsContentType[0] === 'section') {
-          const newSectionId = this.$method('dsContent/getValue', dsContentId)
-
-          // create a new widget and append it to this element item
-          this.$method('dsWidget/create', {
-            dsWidgetSectionId,
-            dsViewId: dsViewParentId,
-            dsWidgetPrefixId,
-            language,
-            dsWidgetView
-          })
-
-          this.$method('dsWidget/setSectionParentId', { dsWidgetSectionId: newSectionId, dsWidgetSectionParentId: dsWidgetSectionId })
-        } else {
-          // missing parentElement
-          this.$method('dsContent/attachView', { dsContentId, dsViewId })
+      const dsContentId = this.$getDataValue({
+        name: 'dsWidget/instanceContent',
+        id: dsWidgetInstanceId,
+        prefixId: dsWidgetPrefixId,
+        suffixId: dsWidgetMode,
+        options: {
+          position: component.contentIndex
         }
+      })
 
-        this.$method('dsWidget/attach', { type: 'content', id: dsContentId })
+      const dsContentType = this.$getDataValue({
+        name: 'dsContent/type',
+        id: dsContentId.value
+      })
+
+      if (dsContentType.name === 'section') {
+        const dsWidgetSectionId = this.$getDataValue({
+          name: 'dsContent/value',
+          id: dsContentId.value
+        })
+
+        // create a new widget and append it to this element item
+        this.$method('dsWidget/attachSection', {
+          dsWidgetSectionId,
+          dsViewId: dsViewParentId,
+          dsWidgetPrefixId,
+          dsWidgetMode
+        })
+      } else {
+        // missing parentElement
+        this.$method('dsView/updateValue', { dsViewId, dsContentId: dsContentId.value })
+
+        this.$addDataListener({
+          name: 'dsContent/value',
+          on: 'update',
+          id: dsContentId.value,
+          listener: () => {
+            this.$method('dsView/updateValue', { dsViewId, dsContentId: dsContentId.value })
+          }
+        })
+
+        this.$emit({
+          id: dsViewId,
+          on: 'dsContent/attachView',
+          payload: {
+            dsContentId: dsContentId.value,
+            dsViewId
+          }
+        })
       }
     },
-    _create (id, items, head, children, events, sectionId, instanceId, dsViewParentId, dsWidgetPrefixId, lang, view, components = [], componentChildren, currentIndex = 0) {
+    _create (
+      id,
+      items,
+      entry = [0],
+      children,
+      events = [],
+      dsWidgetSectionId,
+      dsWidgetInstanceId,
+      dsViewParentId,
+      dsWidgetPrefixId,
+      dsWidgetMode,
+      lang,
+      components = [],
+      componentChildren,
+      currentIndex = 0
+    ) {
       // components might make this variable redundant
       // position of components within the layout
       let fragments = []
 
       if (!components.length) {
         for (let i = 0; i < items.length; i++) {
-          components.push(instanceId + '_' + i.toString().padStart(4, '0'))
+          components.push(dsWidgetInstanceId + i.toString().padStart(4, '0'))
         }
 
         componentChildren = components
       }
 
-      for (let i = 0; i < head.length; i++) {
-        const item = children[head[i]]
+      for (let i = 0; i < entry.length; i++) {
+        const item = children[entry[i]]
         const fragment = {}
-        let dsViewId = componentChildren[head[i]]
+        let dsViewId = componentChildren[entry[i]]
 
         if (Object.prototype.hasOwnProperty.call(item, 'parentIndex')) {
           fragment.parentIndex = item.parentIndex
         }
 
         if (item.componentId) {
-          const modifierId = this.$method('dsWidget/getLayout', sectionId + instanceId + '_' + view)
-          const payload = {
-            dsComponentId: item.componentId
-          }
+          // const modifierId = this.$method('dsWidget/getLayout', sectionId + instanceId + '_' + view)
+          // const payload = {
+          //   dsComponentId: item.componentId
+          // }
 
-          if (this.modifiers[modifierId] && this.modifiers[modifierId][currentIndex]) {
-            payload.dsComponentModifierId = this.modifiers[modifierId][currentIndex]
-          }
+          // if (this.modifiers[modifierId] && this.modifiers[modifierId][currentIndex]) {
+          //   payload.dsComponentModifierId = this.modifiers[modifierId][currentIndex]
+          // }
 
-          const dsComponent = this.$method('dsComponent/get', payload)
+          const dsComponent = this.$getDataValue({
+            name: 'dsComponent/items',
+            id: item.componentId
+          })
 
-          if (dsComponent.textNode) {
+          if (dsComponent.value.id === 'text') {
             this.$method('dsView/createNode', dsViewId)
           } else {
             this.$method('dsView/createElement', {
               dsViewId,
-              dsWidgetSectionId: sectionId,
-              dsWidgetInstanceId: instanceId,
-              dsComponent
+              dsWidgetSectionId,
+              dsWidgetInstanceId,
+              dsComponent: dsComponent.value
             })
           }
-          // create parent component
 
           // add event listener to node
           if (events[currentIndex]) {
             const event = events[currentIndex]
 
             for (let i = 0; i < event.action.length; i++) {
-              this.$method('dsEvent/addListener', { id: dsViewId, name: event.on, dsActionId: event.action[i] })
+              this.$setDataValue({
+                name: 'dsEvent/listeners',
+                source: event.action[i],
+                options: {
+                  id: dsViewId,
+                  suffixId: event.on
+                }
+              })
             }
           }
 
-          fragment.viewId = dsViewId
+          fragment.dsViewId = dsViewId
         } else {
           dsViewId = dsViewParentId
         }
@@ -168,12 +318,12 @@ export default {
             sibling.head,
             sibling.children,
             events,
-            sectionId,
-            instanceId,
+            dsWidgetSectionId,
+            dsWidgetInstanceId,
             dsViewParentId,
             dsWidgetPrefixId,
+            dsWidgetMode,
             lang,
-            view,
             components,
             sibling.components,
             ++currentIndex
@@ -195,17 +345,8 @@ export default {
 
       return fragments
     },
-    _setComponents (id, item, view) {
-      this.components[view + id] = item
-    },
-    _getHead (id) {
-      return this.head[id] || [0]
-    },
-    _getEvent (id) {
-      return this.event[id] || []
-    },
-    _getItem (id) {
-      return this.items[id]
+    _getComponents (dsWidgetInstanceId, dsWidgetView = 'default') {
+      return this.components[dsWidgetView + dsWidgetInstanceId]
     },
     _getSibling (items, indexes, components) {
       const newItems = {
@@ -223,6 +364,15 @@ export default {
       }
 
       return newItems
+    },
+    _setComponents (dsWidgetInstanceId, dsWidgetPrefixId, dsWidgetMode, components) {
+      let id = dsWidgetMode + dsWidgetInstanceId
+
+      if (dsWidgetPrefixId) {
+        id = dsWidgetMode + dsWidgetPrefixId + dsWidgetInstanceId
+      }
+
+      this.components[id] = components
     }
   }
 }
