1fc6acf9fc7533ede3a117d9451268a948bbb28c
Thomas David <tom@mightysmile.com>
2023-04-12 12:54:42 +0200

    DS-884 Migrate plugins to use dsData

--- src/index.js
+++ src/index.js
@@ -6,37 +6,23 @@ export default {
   name: 'dsMetadata',
   version: 1,
   data: {
-    appId: '',
-    language: 'default',
-    theme: ''
-  },
-  methods: {
-    getLanguage () {
-      return this.language
+    appId: {
+      default: '',
+      schema: {
+        type: 'string'
+      }
     },
-    getTheme () {
-      return this.theme
+    language: {
+      default: 'default',
+      schema: {
+        type: 'string'
+      }
     },
-    /**
-     * Set the apps id
-     * @param {string} id - appId
-     */
-    setAppId (id) {
-      this.appId = id
-    },
-    /**
-     * Set the apps language
-     * @param {string} value - name of theme
-     */
-    setLanguage (value) {
-      this.language = value
-    },
-    /**
-     * Set the apps theme name
-     * @param {string} value - name of theme
-     */
-    setTheme (value) {
-      this.theme = value
+    theme: {
+      default: '',
+      schema: {
+        type: 'string'
+      }
     }
   }
 }
