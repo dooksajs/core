cf92b6b7bba3d695fde30707fc988ba8305110b7
Thomas David <tom@mightysmile.com>
2023-04-12 12:54:18 +0200

    DS-884 Migrate plugins to use dsData

--- src/index.js
+++ src/index.js
@@ -13,54 +13,49 @@
 export default {
   name: 'dsEvent',
   version: 1,
-  dependencies: [
-    {
-      name: 'dsAction',
-      version: 1
-    }
-  ],
   data: {
-    listeners: {}
+    listeners: {
+      default: {},
+      schema: {
+        type: 'collection',
+        items: {
+          type: 'array',
+          items: {
+            type: 'string',
+            relation: 'dsAction/sequenceActions'
+          }
+        }
+      }
+    }
   },
   /** @lends dsEvent */
   methods: {
     /**
-     * Add listener
-     * @param {Object} param
-     * @param {dsEventId} param.dsEventId - Event id is the listener reference item
-     * @param {dsEventOn} param.dsEventOn - Event fired "on" an event name
-     * @param {dsActionId} param.dsActionId - The action that runs on the event
-     */
-    addListener ({ dsEventId, dsEventOn, dsActionId }) {
-      const id = this._id(dsEventId, dsEventOn)
-
-      if (this.listeners[id]) {
-        this.listeners[id].push(dsActionId)
-      } else {
-        this.listeners[id] = [dsActionId]
-      }
-    },
-    /**
      * Emit event
      * @param {Object} param
-     * @param {dsEventId} param.dsEventId - Event id is the listener reference item
-     * @param {dsEventOn} param.dsEventOn - Event fired "on" an event name
+     * @param {dsEventId} param.id - Event id is the listener reference item
+     * @param {dsEventOn} param.on - Event fired "on" an event name
      * @param {Object.<string, any>} param.payload - The action that runs on the event
      */
-    emit ({ dsEventId, dsEventOn, payload }) {
-      const id = this._id(dsEventId, dsEventOn)
-      const listeners = this._getListener(id)
+    emit ({ id, on, payload }) {
+      const listeners = this.$getDataValue({
+        name: 'dsEvent/listeners',
+        id: id + '_' + on
+      })
 
-      for (let i = 0; i < listeners.length; i++) {
-        const dsActionId = listeners[i]
-
-        this.$method('dsAction/dispatch', { dsActionId, payload })
+      if (!listeners.isEmpty) {
+        for (let i = 0; i < listeners.value.length; i++) {
+          this.$method('dsAction/dispatch', {
+            dsActionId: listeners.value[i],
+            payload
+          })
+        }
       }
     },
     /**
      * Remove listener
      * @param {Object} param
-     * @param {dsEventIYou cannot open the zip file directly in vscode, itnId} param.dsActionId - The action that runs on the event
+     * @param {dsActionId} param.dsActionId - The action that runs on the event
      */
     removeListener ({ dsEventId, dsEventOn, dsActionId }) {
       const id = this._id(dsEventId, dsEventOn)
@@ -81,19 +76,6 @@ export default {
           this.listeners[id] = list
         }
       }
-    },
-    /**
-     * Set listeners
-     * @param {Object} listener
-     */
-    set (listener) {
-      this.listeners = { ...this.listeners, ...listener }
-    },
-    _getListener (dsEventId) {
-      return this.listeners[dsEventId] ?? []
-    },
-    _id (id, on) {
-      return id + '_' + on
     }
   }
 }
