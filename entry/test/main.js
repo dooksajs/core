39355eb9b3cc2fe88af6a29b1c0ab3bd2ed55504
Thomas David <tom@mightysmile.com>
2023-04-17 16:10:10 +0200

    Update test script

--- entry/test/main.js
+++ entry/test/main.js
@@ -3,27 +3,44 @@ import dsConfig from 'dsConfig'
 import buildTemplates from '../../utils/buildTemplates'
 import page from '../../data/index.js'
 
-const plugins = dsConfig.devDependencies ?? {}
+const plugins = dsConfig.devDependencies ?? { options: [], plugins: {} }
 const options = dsConfig.options ?? {}
 const setup = dsConfig.setup ?? {}
 const app = createApp(page, plugins, { ...options, setup })
 
 window.dsApp = app
-window.dsTemplates = {}
 
 // build templates
 if (dsConfig.templates) {
   const dsWidgetSectionId = page.widgets.entry[page.id][0]
 
   buildTemplates(app, dsConfig.templates)
-    .then(templates => {
-      for (let i = 0; i < templates.length; i++) {
-        const template = templates[i]
+    .then(instances => {
+      for (let i = 0; i < instances.length; i++) {
+        const instance = instances[i]
 
-        app.$method('dsWidget/insert', {
-          dsWidgetSectionId,
-          dsWidgetItem: template.item
-        })
+        for (let i = 0; i < instance.templates.length; i++) {
+          const template = instance.templates[i]
+          const instanceId = app.$method('dsWidget/insertInstance', {
+            dsWidgetSectionId
+          })
+
+          app.$setDataValue({
+            name: 'dsWidget/instanceTemplates',
+            source: template.id,
+            options: {
+              id: instanceId,
+              suffixId: template.mode
+            }
+          })
+        }
       }
+
+      const rootViewId = app.$getDataValue({ name: 'dsView/rootViewId' })
+
+      app.$method('dsWidget/attachSection', {
+        dsWidgetSectionId,
+        dsViewId: rootViewId.value
+      })
     })
 }
