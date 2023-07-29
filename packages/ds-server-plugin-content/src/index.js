8c587c742ac07db395d0019843865b612fde3eb0
Thomas David <tom@mightysmile.com>
2023-07-29 09:33:36 +0200

    Init content server plugin

---
+++ packages/ds-server-plugin-content/src/index.js
@@ -0,0 +1,65 @@
+import dsContent from '@dooksa/ds-plugin-content'
+
+/**
+ * DsPage plugin.
+ * @namespace dssContent
+ */
+export default {
+  name: 'dssContent',
+  version: 1,
+  dependencies: [
+    {
+      name: 'dssDatabase'
+    }
+  ],
+  data: {
+    ...dsContent.data
+  },
+  setup () {
+    this.$method('dssDatabase/model', {
+      name: 'content',
+      fields: [
+        {
+          name: 'id',
+          type: 'string',
+          options: {
+            primaryKey: true
+          }
+        },
+        {
+          name: 'groupId',
+          type: 'string',
+          options: {
+            allowNull: false
+          }
+        },
+        {
+          name: 'language',
+          type: 'string',
+          options: {
+            allowNull: false
+          }
+        },
+        {
+          name: 'type',
+          type: 'string',
+          options: {
+            allowNull: false
+          }
+        },
+        {
+          name: 'data',
+          type: 'json',
+          options: {
+            allowNull: false
+          }
+        }
+      ]
+    })
+  },
+  methods: {
+    async create (request, response) {
+
+    }
+  }
+}
