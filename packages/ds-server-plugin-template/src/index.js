39fdffb266d2734babf76a7d5a0ff01352a8e5c6
Thomas David <tom@mightysmile.com>
2023-07-29 09:31:48 +0200

    Init template server plugin

---
+++ packages/ds-server-plugin-template/src/index.js
@@ -0,0 +1,42 @@
+import dsTemplate from '@dooksa/ds-plugin-template'
+
+/**
+ * DsPage plugin.
+ * @namespace dssTemplate
+ */
+export default {
+  name: 'dssTemplate',
+  version: 1,
+  dependencies: [
+    {
+      name: 'dssDatabase'
+    }
+  ],
+  data: {
+    ...dsTemplate.data
+  },
+  setup () {
+    this.$method('dssDatabase/model', {
+      name: 'template',
+      fields: [
+        {
+          name: 'id',
+          type: 'string',
+          options: {
+            primaryKey: true
+          }
+        },
+        {
+          name: 'data',
+          type: 'json',
+          options: {
+            allowNull: false
+          }
+        }
+      ]
+    })
+  },
+  /** @lends dssTemplate */
+  methods: {
+  }
+}
