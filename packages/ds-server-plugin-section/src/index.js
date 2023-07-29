b06ba7268e54ff1d6f0f73cf44592a93d3092fb5
Thomas David <tom@mightysmile.com>
2023-07-29 09:32:08 +0200

    Init section server plugin

---
+++ packages/ds-server-plugin-section/src/index.js
@@ -0,0 +1,53 @@
+import dsSection from '@dooksa/ds-plugin-section'
+
+/**
+ * DsPage plugin.
+ * @namespace dssSection
+ */
+export default {
+  name: 'dssSection',
+  version: 1,
+  dependencies: [
+    {
+      name: 'dssDatabase'
+    }
+  ],
+  data: {
+    ...dsSection.data
+  },
+  setup () {
+    this.$method('dssDatabase/model', {
+      name: 'section',
+      fields: [
+        {
+          name: 'id',
+          type: 'string',
+          options: {
+            primaryKey: true
+          }
+        },
+        {
+          name: 'groupId',
+          type: 'string',
+          options: {
+            allowNull: false
+          }
+        },
+        {
+          name: 'mode',
+          type: 'string',
+          options: {
+            allowNull: false
+          }
+        },
+        {
+          name: 'data',
+          type: 'json',
+          options: {
+            allowNull: false
+          }
+        }
+      ]
+    })
+  }
+}
