06206a104c53956bdc3b8578ab5c465343272975
Thomas David <tom@mightysmile.com>
2023-07-29 09:33:50 +0200

    Init component server plugin

---
+++ packages/ds-server-plugin-component/src/index.js
@@ -0,0 +1,40 @@
+import dsComponent from '@dooksa/ds-plugin-component'
+
+/**
+ * DsPage plugin.
+ * @namespace dssComponent
+ */
+export default {
+  name: 'dssComponent',
+  version: 1,
+  dependencies: [
+    {
+      name: 'dssDatabase'
+    }
+  ],
+  data: {
+    ...dsComponent.data
+  },
+  setup () {
+    this.$method('dssDatabase/model', {
+      name: 'component',
+      fields: [
+        {
+          name: 'id',
+          type: 'string',
+          options: {
+            primaryKey: true
+          }
+        },
+        {
+          name: 'data',
+          type: 'json',
+          options: {
+            unique: true,
+            allowNull: false
+          }
+        }
+      ]
+    })
+  }
+}
