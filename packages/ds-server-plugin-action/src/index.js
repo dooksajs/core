9a554bc320188387724a2c1f3462e9faeb294cbe
Thomas David <tom@mightysmile.com>
2023-10-04 13:41:20 +0200

    Add action routes

--- packages/ds-server-plugin-action/src/index.js
+++ packages/ds-server-plugin-action/src/index.js
@@ -106,8 +106,95 @@ export default {
         through: 'actionRecipeSequences'
       }
     })
+
+    // route: add actions
+    this.$method('dssWebServer/route', {
+      path: '/action',
+      method: 'post',
+      handler: this._create.bind(this)
+    })
+
+    // route: update existing actions
+    this.$method('dssWebServer/route', {
+      path: '/action',
+      method: 'put',
+      handler: this._update.bind(this)
+    })
+
+    // route: get a list of action
+    this.$method('dssWebServer/route', {
+      path: '/action',
+      method: 'get',
+      handler: this._get.bind(this)
+    })
+
+    // route: delete action
+    this.$method('dssWebServer/route', {
+      path: '/action',
+      method: 'delete',
+      handler: this._delete.bind(this)
+    })
+
+    // route: add action sequences
+    this.$method('dssWebServer/route', {
+      path: '/action/sequence',
+      method: 'post',
+      handler: this._createSequence.bind(this)
+    })
+
+    // route: update existing actions
+    this.$method('dssWebServer/route', {
+      path: '/action/sequence',
+      method: 'put',
+      handler: this._updateSequence.bind(this)
+    })
+
+    // route: get a list of action
+    this.$method('dssWebServer/route', {
+      path: '/action/sequence',
+      method: 'get',
+      handler: this._getSequence.bind(this)
+    })
+
+    // route: delete action sequence
+    this.$method('dssWebServer/route', {
+      path: '/action/sequence',
+      method: 'delete',
+      handler: this._deleteSequence.bind(this)
+    })
+
+    // route: add action sequence entries
+    this.$method('dssWebServer/route', {
+      path: '/action/recipe',
+      method: 'post',
+      handler: this._createSequenceEntry.bind(this)
+    })
+
+    // route: update existing action sequence entries
+    this.$method('dssWebServer/route', {
+      path: '/action/recipe',
+      method: 'put',
+      handler: this._updateSequenceEntry.bind(this)
+    })
+
+    // route: get a list of action sequence entries
+    this.$method('dssWebServer/route', {
+      path: '/action/recipe',
+      method: 'get',
+      handler: this._getSequenceEntry.bind(this)
+    })
+
+    // route: delete action sequence entries
+    this.$method('dssWebServer/route', {
+      path: '/action/recipe',
+      method: 'delete',
+      handler: this._deleteSequenceEntry.bind(this)
+    })
   },
   /** @lends dssAction */
   methods: {
+    async _create (request, response) {
+
+    }
   }
 }
