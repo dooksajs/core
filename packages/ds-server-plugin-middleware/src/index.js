c3afa6b27593ff9b7b26644361bd081eb6f8b6ce
Thomas David <tom@mightysmile.com>
2023-10-12 08:44:47 +0200

    Add dsMiddleware plugin

---
+++ packages/ds-server-plugin-middleware/src/index.js
@@ -0,0 +1,63 @@
+/**
+ * Error handler for Dooksa plugins
+ * @namespace dsMiddleware
+ */
+export default {
+  name: 'dsMiddleware',
+  version: 1,
+  data: {
+    items: {
+      schema: {
+        type: 'collection',
+        items: {
+          type: 'function'
+        }
+      },
+      default: {}
+    }
+  },
+  setup () {
+    this.$setDataValue('dsMiddleware/items', {
+      source: this._checkBody,
+      options: {
+        id: 'request/checkBody'
+      }
+    })
+
+    this.$setDataValue('dsMiddleware/items', {
+      source: this._queryIsArray,
+      options: {
+        id: 'request/queryIsArray'
+      }
+    })
+  },
+  methods: {
+    _checkBody (request, response, next) {
+      if (Object.getPrototypeOf(request.body) !== Object.prototype || !Object.keys(request.body).length) {
+        return response.status(400).send({
+          message: 'Missing data'
+        })
+      }
+
+      next()
+    },
+    _queryIsArray (request, response, next) {
+      const query = request.query.id
+
+      if (!Array.isArray(query)) {
+        if (query) {
+          request.query.id = [query]
+        } else {
+          return response.status(400).send({
+            details: {
+              message: 'Query is undefined'
+            },
+            name: 'noQuery'
+          })
+        }
+      }
+
+      next()
+    }
+  }
+}
