267da44e5f6732717989b321dce3e6d0747595f3
Thomas David <tom@mightysmile.com>
2023-10-26 21:35:43 +0200

    Fix dsAction schema

--- packages/ds-plugin-action/src/index.js
+++ packages/ds-plugin-action/src/index.js
@@ -54,7 +54,7 @@ export default {
               children: {
                 type: 'array',
                 items: {
-                  type: 'string'
+                  type: 'number'
                 }
               }
             }
