b3b00ceb7d4220ed95de291ba4731de9cd856dcc
Thomas David <tom@mightysmile.com>
2023-10-31 09:30:51 +0100

    ds-server-page add get by id method

--- packages/ds-server-plugin-page/src/index.js
+++ packages/ds-server-plugin-page/src/index.js
@@ -16,7 +16,7 @@ export default {
     ...dsPage.data,
     templateStart: {
       private: true,
-      default: '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><link rel="icon" type="image/x-icon" href="/favicon.ico"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Dooksa</title><link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" as="style"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" media="print" onload=\'this.media="all"\'></head><body><div id="root"></div>'
+      default: '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><link rel="icon" type="image/x-icon" href="/favicon.ico"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Dooksa</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" media="print" onload=\'this.media="all"\'></head><body><div id="root"></div>'
     },
     templateEnd: {
       private: true,
@@ -28,24 +28,46 @@ export default {
 
     this.$setWebServerRoute('/*', {
       suffix: '',
-      handlers: [(request, response) => {
-        const page = this.$getDataValue('dsPage/items', { id: request.path })
-        response.set('Content-Type', 'text/html')
-
-        if (page.isEmpty) {
-          return response.status(404).send('404')
-        }
-
-        const data = '<script>' + page.item + '</script>'
-
-        response.send(Buffer.from(this.templateStart + data + this.templateEnd))
-      }]
+      handlers: [this._get.bind(this)]
     })
 
     this.$setWebServerRoute('/', {
       method: 'post',
+      suffix: '',
       middleware: ['dsUser/auth'],
-      handlers: [this.$setDatabaseValue]
+      handlers: [this._save.bind(this)]
     })
+  },
+  methods: {
+    _appendExpand: dsPage.methods._appendExpand.bind(this),
+    _getById: dsPage.methods._getById.bind(this),
+    _get (request, response) {
+      const pageData = this._getById(request.path)
+      response.set('Content-Type', 'text/html')
+
+      if (pageData.isEmpty) {
+        return response.status(404).send('404')
+      }
+
+      const data = '<script>window.dsData = ' + pageData.item + '</script>'
+
+      response.send(Buffer.from(this.templateStart + data + this.templateEnd))
+    },
+    _save (request, response) {
+      const setData = this.$setDatabaseValue({
+        items: request.body,
+        userId: request.userId
+      })
+
+      if (!setData.isValid) {
+        if (setData.snapshotError) {
+          return response.status(500).send(setData.snapshotError)
+        }
+
+        return response.status(400).send(setData.error.details)
+      }
+
+      response.status(201).send(setData)
+    }
   }
 }
