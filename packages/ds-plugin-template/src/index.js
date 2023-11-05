9eacc1a16a31c7588b37ee5ff3c16b9cd1021f16
Thomas David <tom@mightysmile.com>
2023-11-05 14:54:32 +0100

    Split dsTemplate into client and server

--- packages/ds-plugin-template/src/index.js
+++ packages/ds-plugin-template/src/index.js
@@ -1,5 +1,3 @@
-import { parseHTML, getNodeValue } from '@dooksa/parse-template'
-
 /**
  * @namespace dsTemplate
  */
@@ -256,85 +254,6 @@ export default {
       })
 
       return dsSectionId
-    },
-    parseHTML ({ html, actions }) {
-      const template = parseHTML(html, this.$componentGetters, this.$componentIgnoreAttr)
-
-      for (let i = 0; i < template.content.length; i++) {
-        const items = template.content[i]
-
-        for (let j = 0; j < items.length; j++) {
-          const node = items[j]
-          const nodeName = node.nodeName.toLowerCase()
-          const getters = this.$componentGetters[nodeName]
-          let value = {}
-
-          if (node.nodeName === '#text') {
-            value = getNodeValue(getters[0].type, node, getters[0].value)
-          } else {
-            // get node value
-            for (let i = 0; i < getters.length; i++) {
-              const getter = getters[i]
-              const result = getNodeValue(getter.type, node, getter.value)
-
-              value[result.key] = result.value
-            }
-          }
-
-          items[j] = {
-            item: value,
-            type: this.$component(nodeName).type
-          }
-        }
-      }
-
-      // add layouts
-      for (let i = 0; i < template.layout.length; i++) {
-        const layout = template.layout[i]
-        const layoutId = template.layoutId[i]
-
-        this.$setDataValue('dsLayout/items', layout, {
-          id: layoutId
-        })
-      }
-
-      // match action reference to events
-      if (actions) {
-        for (let i = 0; i < template.widgetEvent.length; i++) {
-          const events = template.widgetEvent[i]
-
-          for (const key in events) {
-            if (Object.hasOwn(events, key)) {
-              const event = events[key]
-
-              for (let i = 0; i < event.value.length; i++) {
-                const eventId = event.value[i]
-
-                if (actions[eventId]) {
-                  event.value[i] = actions[eventId]
-                }
-              }
-            }
-          }
-        }
-      }
-
-      this.$setDataValue('dsComponent/items', template.component, {
-        merge: true
-      })
-
-      const result = this.$setDataValue('dsTemplate/items', {
-        content: template.content,
-        layout: template.layout,
-        layoutId: template.layoutId,
-        widgetEvent: template.widgetEvent,
-        widgetSection: template.widgetSection,
-        section: template.section
-      }, {
-        id: template.id
-      })
-
-      return { id: result.id, mode: template.mode }
     }
   }
 }
