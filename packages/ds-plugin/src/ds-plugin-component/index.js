76a5f9b0da22b38740a7517d487484b8c7df5c9b
Thomas David <tom@mightysmile.com>
2023-11-11 17:51:49 +0100

    Add contentProperty to input components

--- packages/ds-plugin/src/ds-plugin-component/index.js
+++ packages/ds-plugin/src/ds-plugin-component/index.js
@@ -246,7 +246,8 @@ export default definePlugin({
         },
         {
           type: 'attribute',
-          name: 'placeholder'
+          name: 'placeholder',
+          contentProperty: 'placeholder'
         }],
         set: [{
           type: 'setter',
@@ -254,7 +255,8 @@ export default definePlugin({
         },
         {
           type: 'attribute',
-          name: 'placeholder'
+          name: 'placeholder',
+          contentProperty: 'placeholder'
         }]
       }
     },
