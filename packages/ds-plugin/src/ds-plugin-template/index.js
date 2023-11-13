f704f2a71a6ecaa413ceb7cd30852131d7dcadc8
Thomas David <tom@mightysmile.com>
2023-11-13 13:34:56 +0100

    dsTemplate add contentRef and sectionRefs

--- packages/ds-plugin/src/ds-plugin-template/index.js
+++ packages/ds-plugin/src/ds-plugin-template/index.js
@@ -164,7 +164,6 @@ export default definePlugin({
   methods: {
     create ({
       id,
-      events = {},
       mode = 'default',
       language,
       dsSectionId
@@ -181,6 +180,8 @@ export default definePlugin({
 
       const dsWidgetItems = []
       const dsWidgetGroupId = this.$method('dsData/generateId')
+      const actionRefs = []
+      const events = []
 
       this.$setDataValue('dsSection/templates', id, {
         id: dsSectionId,
@@ -192,8 +193,9 @@ export default definePlugin({
       })
 
       for (let i = 0; i < template.item.layoutId.length; i++) {
+        const contentRefs = template.item.contentRefs[i] || {}
         const contentItems = template.item.content[i]
-        const events = template.item.widgetEvent[i]
+        const event = template.item.widgetEvent[i]
         const widget = {
           id: this.$method('dsData/generateId'),
           content: [],
@@ -203,15 +205,14 @@ export default definePlugin({
         dsWidgetItems.push(widget)
 
         // add events to instance
-        if (Object.keys(events).length) {
-          this.$setDataValue('dsWidget/events', events, {
-            id: widget.id,
-            suffixId: mode
-          })
+        if (Object.keys(event).length) {
+          events.push([widget.id, event])
         }
 
         for (let j = 0; j < contentItems.length; j++) {
           const content = contentItems[j]
+          const contentRef = contentRefs[j]
+
           const dsContent = this.$setDataValue('dsContent/items', content.item, {
             suffixId: language,
             metadata: {
@@ -221,6 +222,14 @@ export default definePlugin({
 
           // remove language suffix to allow other language overrides
           const contentId = dsContent.noAffixId
+
+          if (contentRef) {
+            if (actionRefs[contentRef]) {
+              throw new Error('Action reference id must be unique: "' + contentRef + '"')
+            }
+
+            actionRefs[contentRef] = contentId
+          }
 
           widget.content.push(contentId)
         }
@@ -261,41 +270,47 @@ export default definePlugin({
       for (let i = 0; i < template.item.widgetSection.length; i++) {
         const sectionIndexes = template.item.widgetSection[i]
         const dsWidgetId = dsWidgetItems[i].id
+        const dsWidgetSection = []
+        const sectionRef = template.item.sectionRefs[i]
 
-        // get section within widget
-        if (sectionIndexes.length) {
-          const dsWidgetSection = []
-
-          for (let i = 0; i < sectionIndexes.length; i++) {
-            const index = sectionIndexes[i]
-            const templateSection = template.item.section[index]
-            const dsSection = []
-
-            for (let i = 0; i < templateSection.length; i++) {
-              const index = templateSection[i]
-              const dsWidgetId = dsWidgetItems[index].id
-              // include instance in current section
-              dsSection.push(dsWidgetId)
-              // mark instance as used
-              usedWidgets.push(dsWidgetId)
-            }
-
-            const section = this.$setDataValue('dsSection/items', dsSection, {
-              suffixId: mode
-            })
-
-            dsWidgetSection.push(section.noAffixId)
-
-            this.$setDataValue('dsSection/mode', mode, {
-              id: section.noAffixId
-            })
+        if (sectionRef) {
+          if (actionRefs[sectionRef]) {
+            throw new Error('Action reference id must be unique: "' + sectionRef + '"')
           }
 
-          this.$setDataValue('dsWidget/sections', dsWidgetSection, {
-            id: dsWidgetId,
+          actionRefs[sectionRef] = dsWidgetId
+        }
+
+        // get section within widget
+        for (let i = 0; i < sectionIndexes.length; i++) {
+          const index = sectionIndexes[i]
+          const templateSection = template.item.section[index]
+          const dsSection = []
+
+          for (let i = 0; i < templateSection.length; i++) {
+            const index = templateSection[i]
+            const dsWidgetId = dsWidgetItems[index].id
+            // include instance in current section
+            dsSection.push(dsWidgetId)
+            // mark instance as used
+            usedWidgets.push(dsWidgetId)
+          }
+
+          const section = this.$setDataValue('dsSection/items', dsSection, {
             suffixId: mode
           })
+
+          dsWidgetSection.push(section.noAffixId)
+
+          this.$setDataValue('dsSection/mode', mode, {
+            id: section.noAffixId
+          })
         }
+
+        this.$setDataValue('dsWidget/sections', dsWidgetSection, {
+          id: dsWidgetId,
+          suffixId: mode
+        })
 
         if (!usedWidgets.includes(dsWidgetId)) {
           rootSection.push(dsWidgetId)
@@ -307,7 +322,120 @@ export default definePlugin({
         suffixId: mode
       })
 
+      const newActions = this._updateActions(template.item.actions, actionRefs)
+
+      for (let i = 0; i < events.length; i++) {
+        const [widgetId, event] = events[i]
+
+        for (const key in event) {
+          if (Object.hasOwnProperty.call(event, key)) {
+            const item = event[key]
+
+            for (let i = 0; i < item.value.length; i++) {
+              const value = item.value[i]
+
+              // update event action item id
+              if (newActions[value]) {
+                item.value[i] = newActions[value]
+              }
+            }
+          }
+        }
+
+        this.$setDataValue('dsWidget/events', event, {
+          id: widgetId,
+          suffixId: mode
+        })
+      }
+
       return dsSectionId
+    },
+    _updateActions (actions, refs) {
+      const newActions = {}
+
+      for (let i = 0; i < actions.length; i++) {
+        const action = actions[i]
+        const newBlocks = {}
+        const newSequence = {}
+
+        for (let i = 0; i < action.blocks.length; i++) {
+          const id = action.blocks[i]
+          const block = this.$getDataValue('dsAction/blocks', { id })
+          const newBlock = this._replaceActionRef(Object.assign({}, block.item), refs)
+
+          newBlocks[id] = newBlock
+        }
+
+        for (let i = 0; i < action.sequences.length; i++) {
+          const id = action.sequences[i]
+          const sequences = this.$getDataValue('dsAction/sequences', { id })
+          const newSequences = sequences.item.slice()
+
+          for (let i = 0; i < newSequences.length; i++) {
+            let sequence = newSequences[i]
+            const block = newBlocks[sequence.id]
+
+            if (block) {
+              const blockId = objectHash(block)
+
+              // set new block
+              this.$setDataValue('dsAction/blocks', block, { id: blockId })
+
+              // create a shallow copy
+              sequence = Object.assign({}, sequence)
+
+              // update block id
+              sequence.id = blockId
+
+              // Add new object to sequence list
+              newSequences[i] = sequence
+            }
+          }
+
+          const sequenceId = objectHash(newSequence)
+
+          // store new sequence id change
+          newSequence[id] = sequenceId
+
+          this.$setDataValue('dsAction/sequences', newSequences, { id: sequenceId })
+        }
+
+        const id = action.items
+        const items = this.$getDataValue('dsAction/items', { id })
+
+        // copy action items
+        const newItems = items.item.slice()
+
+        for (let i = 0; i < newItems.length; i++) {
+          const sequenceId = newItems[i]
+
+          if (newSequence[sequenceId]) {
+            newItems[i] = newSequence[sequenceId]
+          }
+        }
+
+        const newItem = this.$setDataValue('dsAction/items', newItems)
+
+        newActions[id] = newItem.id
+      }
+
+      return newActions
+    },
+    _replaceActionRef (items, refs, parent, parentKey) {
+      for (const key in items) {
+        if (Object.hasOwnProperty.call(items, key)) {
+          const item = items[key]
+
+          if (refs[item]) {
+            parent[parentKey] = Object.assign({}, items)
+            parent[parentKey][key] = refs[item]
+          } else if (typeof item === 'object') {
+            this._replaceActionRef(item, refs, items, key)
+          }
+        }
+      }
+
+      return items
     }
   }
 })
