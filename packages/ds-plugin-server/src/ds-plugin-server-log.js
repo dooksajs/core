7a5a68ce3bf1a7c9cb297cdb6649f91a44ae75dc
Thomas David <tom@mightysmile.com>
2023-11-18 15:23:24 +0100

    dsLog - add common logging system

--- packages/ds-plugin-server/src/ds-plugin-server-log.js
+++ packages/ds-plugin-server/src/ds-plugin-server-log.js
@@ -1,4 +1,5 @@
 import { definePlugin } from '@dooksa/ds-app'
+import { template } from 'chalk-template'
 import chalk from 'chalk'
 
 /**
@@ -9,29 +10,70 @@ export default definePlugin({
   name: 'dsLog',
   version: 1,
   data: {
-    theme: {
-      private: true,
-      default: () => ({
-        primary: '#0d6efd',
-        secondary: '#6c757d',
-        success: '#198754',
-        info: '#0dcaf0',
-        danger: '#dc3545',
-        light: '#f8f9fa',
-        dark: '#212529'
-      })
+    message: {
+      schema: {
+        type: 'collection',
+        items: {
+          type: 'object'
+        }
+      }
+    },
+    errors: {
+      schema: {
+        type: 'collection',
+        items: {
+          type: 'object'
+        }
+      }
     }
-  },
-  setup ({ theme }) {
-    if (theme) {
-      this.theme = Object.assign(this.theme, theme)
-    }
-
-
   },
   methods: {
-    $log (type, message, store) {
+    $log (type, { message, cause, code, store }) {
+      const logType = '_log/' + type
 
+      if (!this[logType]) {
+        throw new Error('No log type: ' + type)
+      }
+
+      const now = new Date()
+      const hours = now.getHours()
+      const minutes = now.getMinutes()
+      const seconds = now.getSeconds()
+
+      this[logType](`${hours}:${minutes}:${seconds}`, message, store, code, cause)
+    },
+    '_log/message' (time, message) {
+      console.log(template(`{grey ${time}} {white Message:} ${message}`))
+    },
+    '_log/warn' (time, message, store, code = '400', cause) {
+      console.warn(template(`{yellow Warning:} ${message}`))
+
+      if (store) {
+        const error = this._error(message, code, cause)
+
+        this.$setDataValue('dsLog/errors', error)
+      }
+    },
+    '_log/error' (time, message, store, code = '500', cause) {
+      const error = this._error(message, code, cause)
+
+      console.error(template(`{red Error:} {grey ${time}} ${message}`))
+
+      if (store) {
+        this.$setDataValue('dsLog/errors', error)
+      }
+
+      throw error
+    },
+    _error (message, code, cause) {
+      const plainMessage = chalk.reset(template(`${message}`))
+
+      return new Error(plainMessage, {
+        cause: {
+          code,
+          details: cause
+        }
+      })
     }
   }
 })
