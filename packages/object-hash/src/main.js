1f2ef336efc29c6ca8b37c5a4a62555fdeb319ca
Thomas David <tom@mightysmile.com>
2023-11-23 12:02:51 +0100

    refactor: Simplify clone

--- packages/object-hash/src/main.js
+++ packages/object-hash/src/main.js
@@ -13,10 +13,10 @@ const TYPES = {
  * @param {Object} source - The original object used to create the hash
  * @returns {string} - xxhash 64 string
  */
-const objectHash = (source) => {
+function objectHash (source) {
   try {
-    if (!source) {
-      throw new Error('source is undefined')
+    if (source == null) {
+      throw new Error('Source is undefined')
     }
 
     const target = sortType(defaultType(source), source)
@@ -27,75 +27,52 @@ const objectHash = (source) => {
   }
 }
 
-const defaultType = (value) => {
+/**
+ * Sort object properties alphanumerically
+ * @private
+ * @param {Object|Array} target - Target value
+ * @param {Object|Array} source - Current value
+ * @returns {Object|Array} sorted value
+ */
+function sortType (target, source) {
+  for (const prop in source) {
+    if (Object.hasOwn(source, prop)) {
+      const nextSource = source[prop]
+
+      if (Array.isArray(nextSource)) {
+        target[prop] = sortType([], nextSource)
+      } else if (nextSource.constructor === Object) {
+        target[prop] = sortType({}, nextSource)
+      } else {
+        // update value
+        target[prop] = nextSource
+      }
+    }
+  }
+
+  let sortedTarget = {}
+
+  if (target.constructor === Object) {
+    const keys = Object.keys(target)
+
+    keys.sort()
+
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i]
+
+      sortedTarget[key] = target[key]
+    }
+  } else {
+    sortedTarget = target
+  }
+
+  return sortedTarget
+}
+
+function defaultType (value) {
   const name = value?.constructor.name
 
   return TYPES[name]()
-}
-
-/**
- * Sort source by data type
- * @private
- * @param {Object} target - Alphanumerically sorted object
- * @param {*} source - Current value
- * @returns
- */
-const sortType = (target, source) => {
-  if (source == null) {
-    throw new Error('objectHash: value cannot be undefined')
-  }
-
-  if (Array.isArray(source)) {
-    source = array(target, source)
-  } else if (typeof source === 'object') {
-    source = object(target, source)
-  } else if (typeof source === 'function') {
-    source = source.toString()
-  }
-
-  return source
-}
-
-/**
- * Traverse arrays values
- * @param {Object} target - Alphanumerically sorted object
- * @param {Array} source - Current nested array
- * @returns {Array}
- */
-const array = (target, source) => {
-  // make a copy
-  target = source.slice()
-
-  for (let i = 0; i < target.length; i++) {
-    const value = target[i]
-
-    const targetValue = defaultType(value)
-    target[i] = sortType(targetValue, value)
-  }
-
-  return target
-}
-
-/**
- * Sort object keys alphanumerically
- * @private
- * @param {Object} target - Alphanumerically sorted object
- * @param {Object} source - Current nested object
- * @returns {Object}
- */
-const object = (target, source) => {
-  const keys = Object.keys(source)
-
-  keys.sort()
-
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i]
-
-    target[key] = defaultType(source[key])
-    target[key] = sortType(target[key], source[key])
-  }
-
-  return target
 }
 
 export default objectHash
