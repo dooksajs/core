40a17cce0cbaf0d718784e0b8f0ca716cdb3b65a
Thomas David <tom@mightysmile.com>
2023-11-27 21:41:51 +0100

    fix: Build function only when function is requested

--- packages/object-hash/src/object-hash.js
+++ packages/object-hash/src/object-hash.js
@@ -1,13 +1,13 @@
 import { hash, sortObject } from '@dooksa/utils'
 
-hash.init()
-
 /**
  * Create xxhash hash from object
  * @param {Object} source - The original object used to create the hash
  * @returns {string} - xxhash 64 string
  */
 function objectHash (source) {
+  hash.init()
+
   const target = sortObject(source)
 
   return hash.update(target).digest('hex')
