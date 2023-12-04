594a9467a82d5dbd4f9cb197c694347ea6d932bd
Thomas David <tom@mightysmile.com>
2023-12-04 17:35:30 +0100

    fix: JSDocs

--- packages/ds-plugin/src/utils/DataResult.js
+++ packages/ds-plugin/src/utils/DataResult.js
@@ -1,11 +1,19 @@
 import { deepClone } from '@dooksa/utils'
 
+/**
+ * Data result
+ * @param {string} [collection=''] - Collection name
+ * @param {string} [id=''] - Document id
+ */
 function DataResult (collection = '', id = '') {
   this.id = id
   this.collection = collection
   this.isEmpty = false
   this.isCollectionEmpty = false
   this.isAffixEmpty = true
+  this.item = undefined
+  this.metadata = undefined
+  this.previous = undefined
 
   Object.defineProperty(this, 'noAffixId', {
     get () {
@@ -22,6 +30,10 @@ function DataResult (collection = '', id = '') {
   })
 }
 
+/**
+ * Deep copy result data
+ * @returns {*}
+ */
 DataResult.prototype.clone = function () {
   if (Array.isArray(this.item)) {
     return deepClone([], this.item)
