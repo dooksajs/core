76c0c859a4e72f5cd5dac2aaf92645cff4b42e5c
Thomas David <tom@mightysmile.com>
2023-12-27 13:50:43 +0100

    Put dev app in an IIFE

--- packages/ds-scripts/entry/dev/ds-app-client.js
+++ packages/ds-scripts/entry/dev/ds-app-client.js
@@ -1,78 +1,78 @@
 import dsAppClient from '@dooksa/ds-app-client'
 
-const eventSource = new window.EventSource('/_/esbuild')
+((data) => {
+  const eventSource = new window.EventSource('/_/esbuild')
 
-eventSource.addEventListener('rebuild-client', () => {
-  window.location.reload()
-})
+  eventSource.addEventListener('rebuild-client', () => {
+    window.location.reload()
+  })
 
-eventSource.addEventListener('rebuild-server', () => {
-  window.location.reload()
-})
+  eventSource.addEventListener('rebuild-server', () => {
+    window.location.reload()
+  })
 
-const data = __ds__ // eslint-disable-line
-
-dsAppClient.start({
-  isDev: true
-}, {
-  onSuccess: app => {
-    if (data.isEmpty) {
-      return
-    }
-
-    // set data
-    for (let i = 0; i < data.item.length; i++) {
-      const item = data.item[i]
-
-      // need to check if any data requires an async plugin
-      app.$setDataValue(item.collection, item.item, {
-        id: item.id,
-        metadata: item.metadata
-      })
-    }
-
-    const page = app.$getDataValue('dsPage/items', {
-      id: window.location.pathname
-    })
-
-    if (!page.isEmpty) {
-      // Render page
-      for (let i = 0; i < page.item.length; i++) {
-        app.$method('dsSection/append', { id: page.item[i] })
+  dsAppClient.start({
+    isDev: true
+  }, {
+    onSuccess: app => {
+      if (data.isEmpty) {
+        return
       }
 
-      return
-    }
+      // set data
+      for (let i = 0; i < data.item.length; i++) {
+        const item = data.item[i]
 
-    if (data.templates) {
-      const currentPath = app.$method('dsRouter/currentPath')
-
-      for (let i = 0; i < data.templates.length; i++) {
-        const templateId = data.templates[i]
-        app.$action('dsTemplate/create', { id: templateId }, {
-          onSuccess: (result) => {
-            app.$setDataValue('dsPage/items', result.dsSectionId, {
-              id: currentPath,
-              update: {
-                method: 'push'
-              }
-            })
-
-            const page = app.$getDataValue('dsPage/items', {
-              id: window.location.pathname
-            })
-
-            if (!page.isEmpty) {
-              for (let i = 0; i < page.item.length; i++) {
-                app.$method('dsSection/append', { id: page.item[i] })
-              }
-            }
-          }
+        // need to check if any data requires an async plugin
+        app.$setDataValue(item.collection, item.item, {
+          id: item.id,
+          metadata: item.metadata
         })
       }
+
+      const page = app.$getDataValue('dsPage/items', {
+        id: window.location.pathname
+      })
+
+      if (!page.isEmpty) {
+      // Render page
+        for (let i = 0; i < page.item.length; i++) {
+          app.$method('dsSection/append', { id: page.item[i] })
+        }
+
+        return
+      }
+
+      if (data.templates) {
+        const currentPath = app.$method('dsRouter/currentPath')
+
+        for (let i = 0; i < data.templates.length; i++) {
+          const templateId = data.templates[i]
+          app.$action('dsTemplate/create', { id: templateId }, {
+            onSuccess: (result) => {
+              app.$setDataValue('dsPage/items', result.dsSectionId, {
+                id: currentPath,
+                update: {
+                  method: 'push'
+                }
+              })
+
+              const page = app.$getDataValue('dsPage/items', {
+                id: window.location.pathname
+              })
+
+              if (!page.isEmpty) {
+                for (let i = 0; i < page.item.length; i++) {
+                  app.$method('dsSection/append', { id: page.item[i] })
+                }
+              }
+            }
+          })
+        }
+      }
+    },
+    onError: (e) => {
+      console.error(e)
     }
-  },
-  onError: (e) => {
-    console.error(e)
-  }
-})
+  })
+})(__ds__)
