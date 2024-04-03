caf9f51b07a23f5d9d524efdc432edbfeba331d5
Thomas David <tom@mightysmile.com>
2024-04-03 15:43:56 +0200

    fix: Decrement handle strings

--- packages/ds-plugin/src/ds-plugin-operator.js
+++ packages/ds-plugin/src/ds-plugin-operator.js
@@ -137,9 +137,20 @@ export default definePlugin({
      * Decrement operator
      * @private
      * @param {number[]} v
-     * @returns {number}
+     * @returns {number|string}
      */
-    '_operator/--': v => --v[0],
+    '_operator/--': v => {
+      let num = v[0]
+
+      if (v instanceof String) {
+        num = parseInt(v[0])
+        num--
+
+        return num.toString()
+      }
+
+      return --num
+    },
     /**
      * Negation operator
      * @private
