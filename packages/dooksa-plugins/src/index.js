b63a14c3d99b9e44b548db639713a5171426da1d
Thomas David <tom@mightysmile.com>
2024-05-02 12:41:39 +0200

    feature: Export plugin functions

--- packages/dooksa-plugins/src/index.js
+++ packages/dooksa-plugins/src/index.js
@@ -1,11 +1,47 @@
+import action from './action.js'
 import data from './data.js'
 import operator from './operator.js'
 import list from './list.js'
 import metadata from './metadata.js'
+import event from './event.js'
+import fetch from './fetch.js'
+import view from './view.js'
+import viewModal from './viewModal.js'
+import component from './component.js'
+import layout from './layout.js'
+import content from './content.js'
+import token from './token.js'
+import query from './query.js'
+import template from './template.js'
+import router from './router.js'
+
+export * from './action.js'
+export * from './data.js'
+export * from './fetch.js'
+export * from './operator.js'
+export * from './token.js'
+export * from './list.js'
+export * from './query.js'
+export * from './template.js'
+export * from './router.js'
+export * from './view.js'
+export * from './viewModal.js'
 
 export {
+  action,
+  component,
+  content,
   data,
-  operator,
+  event,
+  fetch,
+  layout,
   list,
-  metadata
+  metadata,
+  operator,
+  router,
+  template,
+  token,
+  query,
+  view,
+  viewModal
 }
