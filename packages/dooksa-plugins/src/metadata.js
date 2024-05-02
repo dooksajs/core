42ba08722060d98ed756c64b0ee5587a30560143
Thomas David <tom@mightysmile.com>
2024-05-02 12:38:41 +0200

    refactor: dsMetadata

--- packages/dooksa-plugins/src/metadata.js
+++ packages/dooksa-plugins/src/metadata.js
@@ -1,245 +1,31 @@
-import createPlugin from '@dooksa/create-plugin'
+import { createPlugin } from '@dooksa/create-plugin'
 
-/**
- * Two or one values to run an operator on
- * @typedef {(string[]|number[])} OperatorValues
- * @example
- * [1, 2]
- * ['hello', 'world]
- * [0]
- */
-
-export default createPlugin('dsOperator', ({ defineActions, defineActionSchema }) => {
-  const operators = {
-    /**
-     * Equality
-     * @private
-     * @param {OperatorValues} v
-     * @returns {boolean}
-     */
-    '_operator/==': v => v[0] === v[1],
-    /**
-     * Inequality
-     * @private
-     * @param {OperatorValues} v
-     * @returns {boolean}
-     */
-    '_operator/!=': v => v[0] !== v[1],
-    /**
-     * Greater than
-     * @private
-     * @param {OperatorValues} v
-     * @returns {boolean}
-     */
-    '_operator/>': v => v[0] > v[1],
-    /**
-     * Greater than or equal operator
-     * @private
-     * @param {OperatorValues} v
-     * @returns {boolean}
-     */
-    '_operator/>=': v => v[0] >= v[1],
-    /**
-     * Less than
-     * @private
-     * @param {OperatorValues} v
-     * @returns {boolean}
-     */
-    '_operator/<': v => v[0] < v[1],
-    /**
-     * Less than or equal operator
-     * @private
-     * @param {OperatorValues} v
-     * @returns {boolean}
-     */
-    '_operator/<=': v => v[0] <= v[1],
-    /**
-     * Logical NOT
-     * @private
-     * @param {OperatorValues} v
-     * @returns {boolean}
-     */
-    '_operator/!': v => !v[0],
-    /**
-     * Remainder operator
-     * @private
-     * @param {number[]} v
-     * @returns {number}
-     */
-    '_operator/%': v => v[0] % v[1],
-    /**
-     * Increment operator
-     * @private
-     * @param {number[]} v
-     * @returns {number|string}
-     */
-    '_operator/++': v => {
-      let num = v[0]
-
-      if (v instanceof String) {
-        num = parseInt(v[0])
-        num++
-
-        return num.toString()
+export default createPlugin('metadata', ({ defineData, defineSetup }, { $setDataValue }) => {
+  defineData({
+    language: {
+      default: () => 'en',
+      schema: {
+        type: 'string'
       }
-
-      return ++num
     },
-    /**
-     * Decrement operator
-     * @private
-     * @param {number[]} v
-     * @returns {number|string}
-     */
-    '_operator/--': v => {
-      let num = v[0]
-
-      if (v instanceof String) {
-        num = parseInt(v[0])
-        num--
-
-        return num.toString()
+    availableLanguages: {
+      default: () => ['en'],
+      schema: {
+        type: 'array',
+        items: {
+          type: 'string'
+        }
       }
-
-      return --num
-    },
-    /**
-     * Negation operator
-     * @private
-     * @param {number[]} v
-     * @returns {number}
-     */
-    '_operator/-': v => v[0] - v[1],
-    /**
-     * Plus operator
-     * @private
-     * @param {number[]} v
-     * @returns {number}
-     */
-    '_operator/+': v => v[0] + v[1],
-    /**
-     * Multiplication operator
-     * @private
-     * @param {number[]} v
-     * @returns {number}
-     */
-    '_operator/*': v => v[0] * v[1],
-    /**
-     * Exponentiation operator
-     * @private
-     * @param {number[]} v
-     * @returns {number}
-     */
-    '_operator/**': v => v[0] ** v[1],
-    /**
-     * Boolean value
-     * @private
-     * @param {OperatorValues} v
-     * @returns {boolean}
-     */
-    '_operator/!!': v => Boolean(v[0]),
-    /**
-     * Check if value is within an string or array
-     * @private
-     * @param {(string|Array)} v
-     * @return {boolean}
-     */
-    '_operator/~': v => v[0].includes(v[1])
-  }
-
-  defineActionSchema({
-    compare: {
-      name: 'Compare',
-      description: 'Compare two or more values',
-      schema: [
-        {
-          type: 'array',
-          items: {
-            anyOf: [
-              {
-                type: 'string'
-              },
-              {
-                type: 'number'
-              }
-            ]
-          }
-        }
-      ]
-    },
-    eval: {
-      name: 'Evaluate',
-      description: 'Evaluate two values',
-      schema: [
-        {
-          type: 'object',
-          properties: {
-            name: {
-              type: 'string'
-            },
-            values: {
-              type: 'array',
-              items: {
-                oneOf: [
-                  {
-                    type: 'string'
-                  },
-                  {
-                    type: 'number'
-                  }
-                ]
-              }
-            }
-          }
-        }
-      ]
     }
   })
 
-  defineActions({
-    /**
-     * Compare two or more values
-     * @param {*[]} values - Contains two values or more values which are compared
-     * @example
-     * const andValues = ['1', '&&', 1]
-     */
-    compare (values) {
-      let result = false
+  defineSetup(({ defaultLanguage = '', availableLanguages = [] }) => {
+    if (defaultLanguage) {
+      $setDataValue('metadata/language', defaultLanguage)
+    }
 
-      for (let i = 0; i < values.length; i++) {
-        const value = values[i]
-
-        if (value === '&&') {
-          if ((values[i - 1] && values[i + 1])) {
-            result = true
-          } else {
-            break
-          }
-        }
-
-        if (value === '||') {
-          if ((values[i - 1] || values[i + 1])) {
-            result = true
-          } else {
-            break
-          }
-        }
-      }
-
-      return result
-    },
-    /**
-     * Evaluate two values
-     * @param {Object} eval - The Object containing the data to evaluate two values
-     * @param {string} eval.name - Operator name
-     * @param {OperatorValues} eval.values - Contains two values to be evaluated
-     */
-    eval ({ name, values }) {
-      if (operators[name]) {
-        return operators[name](values)
-      } else {
-        throw new Error('No operator found: ' + name)
-      }
+    if (availableLanguages.length) {
+      $setDataValue('metadata/availableLanguages', availableLanguages)
     }
   })
 })
