b750ee4f20e687451b37f266b818955fd60e5e30
Thomas David <tom@mightysmile.com>
2024-05-07 07:51:19 +0200

    fix (dooksa-client): Init client

---
+++ packages/dooksa-client/src/index.js
@@ -0,0 +1,38 @@
+import {
+  createApp,
+  action,
+  component,
+  content,
+  data,
+  event,
+  fetch,
+  layout,
+  list,
+  metadata,
+  operator,
+  router,
+  template,
+  token,
+  query,
+  view
+} from 'dooksa'
+
+createApp({
+  plugins: [
+    action,
+    component,
+    content,
+    data,
+    event,
+    fetch,
+    layout,
+    list,
+    metadata,
+    operator,
+    router,
+    template,
+    token,
+    query,
+    view
+  ]
+})
