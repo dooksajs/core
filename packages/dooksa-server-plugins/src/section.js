04ab9c2ede086e3c10820492f5f920b60e0de403
Thomas David <tom@mightysmile.com>
2024-05-10 13:18:32 +0200

    refactor: Section server

---
+++ packages/dooksa-server-plugins/src/section.js
@@ -0,0 +1,30 @@
+import { createPlugin } from '@dooksa/create'
+import { section } from '@dooksa/plugins'
+import { $setRoute } from './http.js'
+import { $deleteDatabaseValue, $getDatabaseValue, $seedDatabase } from './database.js'
+
+export default createPlugin({
+  name: 'section',
+  data: { ...section.data },
+  setup () {
+    $seedDatabase('section-items')
+
+    // route: get a list of section
+    $setRoute('/section', {
+      method: 'get',
+      middleware: ['request/queryIsArray'],
+      handlers: [
+        $getDatabaseValue(['section/items'])
+      ]
+    })
+
+    // route: delete section
+    $setRoute('/section', {
+      method: 'delete',
+      middleware: ['user/auth', 'request/queryIsArray'],
+      handlers: [
+        $deleteDatabaseValue(['section/items'])
+      ]
+    })
+  }
+})
