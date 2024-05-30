2d21484e92cffd8c41279b87d22331bfc7e417eb
Thomas David <tom@mightysmile.com>
2024-05-30 18:40:37 +0200

    feature (content-server): Manage backend content

---
+++ packages/app-server-plugins/src/content.js
@@ -0,0 +1,32 @@
+import createPlugin from '@dooksa/create-plugin'
+import { content } from '@dooksa/plugins'
+import { $deleteDatabaseValue, $getDatabaseValue, $seedDatabase } from './database.js'
+import { $setRoute } from './http.js'
+
+const contentServer = createPlugin({
+  name: 'content',
+  models: content.models,
+  setup () {
+    $seedDatabase('content-items')
+
+    // route: get a list of component
+    $setRoute('/content', {
+      method: 'get',
+      middleware: ['request/queryIsArray'],
+      handlers: [
+        $getDatabaseValue(['content/items'])
+      ]
+    })
+
+    // route: delete component
+    $setRoute('/content', {
+      method: 'delete',
+      middleware: ['user/auth', 'request/queryIsArray'],
+      handlers: [
+        $deleteDatabaseValue(['content/items'])
+      ]
+    })
+  }
+})
+
+export default contentServer
