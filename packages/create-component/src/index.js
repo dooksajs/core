8abba417849dfa83bcf273b4bc73201552b61896
Thomas David <tom@mightysmile.com>
2024-05-30 18:18:17 +0200

    refactor (create-component): Remove any code related to component plugin

--- packages/create-component/src/index.js
+++ packages/create-component/src/index.js
@@ -1,12 +1,37 @@
+import { objectHash } from '@dooksa/utils'
+
 /**
- * @typedef {Object.<string, ComponentOptionItem>} ComponentOption
+ * @typedef {Object} Component
+ * @property {string} id - Component id
+ * @property {string} [hash] - Component hash
+ * @property {Function} [component] - Lazy load component
+ * @property {boolean} [isLoaded] - Marks if web component is loaded
+ * @property {Array<Component|ComponentInstance|string>} [children] - Child components
+ * @property {Array<Component|ComponentInstance|string>} [allowedChildren] - Permitted nested components
+ * @property {Function} [initialize] - Constructor function to create a component instance
+ * @property {string} [tag] - Element tag name
+ * @property {Object[]} [content] - Define content structure
+ * @property {string} content[].name - Name of content property
+ * @property {string} content[].get - Getter name
+ * @property {string} content[].set - Setter name
+ * @property {ComponentProperty[]} [properties]
+ * @property {ComponentOption} [options]
+ * @property {ComponentOption[]} [extendedOptions]
+ * @property {Object[]} [styles]
+ * @property {string} styles[].name - Style property name
+ * @property {'unit'|'box-shadow'|'font-family'|'rgba'|'units'|'number'} styles[].type - Style property name
+ */
+
+/**
+ * @typedef {Object.<string, (Object.<string,ComponentOptionItem>|ComponentOptionItem)>} ComponentOption
  */
 
 /**
  * @typedef {Object} ComponentOptionItem
- * @property {string} attribute - Name of attribute
- * @property {boolean} [replace] - How to modify attribute
- * @property {Object.<string, (string|string[])>} [values] - Attribute values
+ * @property {string} name - Name of attribute/setter
+ * @property {boolean} [join] - Join option value with existing component value
+ * @property {Object.<string, string>} [values] - Attribute values
+ * @property {string} [value] - Attribute value
  */
 
 /**
@@ -17,97 +42,77 @@
  */
 
 /**
- * @typedef {Object} ComponentChild
+ * @typedef {Object} ComponentInstance
  * @property {string} id
- * @property {ComponentProperty[]} properties
- * @property {Array<ComponentChild|string>} children
+ * @property {string} hash
+ * @property {ComponentProperty[]} [properties]
+ * @property {Array<Component|ComponentInstance|string>} [children]
  */
 
 /**
- * @template C
- * @template {string|function} G
- * @template {string|function} S
- * @param {Object} component
- * @param {string} component.id - Component id
- * @param {Function} [component.component] - Lazy load component
- * @param {Array<ComponentChild|string>} [component.children] - Child components
- * @param {Object} [component.allowedChildren] - Permitted nested components
- * @param {C} [component.constructor] - Constructor function to create a component instance
- * @param {string} [component.tag] - Element tag name
- * @param {Object[]} [component.content] - Define content structure
- * @param {string} component.content[].name - Name of content property
- * @param {G} component.content[].get - Getter name
- * @param {S} component.content[].set - Setter name
- * @param {ComponentProperty[]} [component.properties]
- * @param {ComponentOption} [component.options]
- * @param {ComponentOption[]} [component.extendedOptions]
- * @param {Object[]} [component.styles]
- * @param {string} component.styles[].name - Style property name
- * @param {'unit'|'box-shadow'|'font-family'|'rgba'|'units'|'number'} component.styles[].type - Style property name
+ * @param {Object} data
+ * @param {string} data.id - Component id
+ * @param {Function} [data.component] - Lazy load component
+ * @param {Array<Component|ComponentInstance|string>} [data.children] - Child components
+ * @param {Array<Component|ComponentInstance|string>} [data.allowedChildren] - Permitted nested components
+ * @param {Function} [data.initialize] - Constructor function to create a component instance
+ * @param {string} [data.tag] - Element tag name
+ * @param {Object[]} [data.content] - Define content structure
+ * @param {string} data.content[].name - Name of content property
+ * @param {string} data.content[].get - Getter name
+ * @param {string} data.content[].set - Setter name
+ * @param {ComponentProperty[]} [data.properties]
+ * @param {ComponentOption} [data.options]
+ * @param {ComponentOption[]} [data.extendedOptions]
+ * @param {Object[]} [data.styles]
+ * @param {string} data.styles[].name - Style property name
+ * @param {'unit'|'box-shadow'|'font-family'|'rgba'|'units'|'number'} data.styles[].type - Style property name
+ * @returns {Component}
  */
 function createComponent (data) {
-  return new Component(data)
-}
+  /**
+   * @type {Component}
+   */
+  const result = data
 
-/**
- * @constructor
- * @param {Object} component
- */
-function Component ({
-  id,
-  name,
-  component,
-  children,
-  allowedChildren,
-  initialize,
-  tag,
-  content,
-  properties = [],
-  options,
-  styles
-}) {
-  this.id = id
-  this.name = name
-  this.component = component
-  this.children = children,
-  this.allowedChildren = allowedChildren
-  this.initialize = initialize
-  this.tag = tag
-  this.content = content
-  this.properties = properties
-  this.options = options
-  this.styles = styles
-  this.componentIsLoaded = !component
+  result.isLoaded = !(typeof data.component === 'function')
+  result.hash = objectHash(result)
+
+  return result
 }
 
 /**
  * Create a modified component
+ * @param {Component} component
  * @param {Object.<string, string>} options
- * @param {ComponentChild} [children]
+ * @returns {ComponentInstance}
  */
-Component.prototype.modify = function (options, children = this.children) {
-  const properties = this.properties.slice()
+function modifyComponent (component, options) {
+  const properties = component.properties ? component.properties.slice() : []
 
   for (const key in options) {
     if (Object.hasOwnProperty.call(options, key)) {
-      const option = this.options[key]
+      const componentOption = component.options[key]
       const value = options[key]
       let newPropertyValue = value
 
-      if (!option) {
+      if (!componentOption) {
         throw new Error('Component option does not exist "' + value +'"')
       }
 
-      if (option.values) {
-        if (option.values[value] == null) {
+      if (componentOption.values) {
+        if (componentOption.values[value] == null) {
           throw new Error('Component modifier value has an unexpected value "' + value + '"')
         }
 
-        newPropertyValue = option.values[value]
+        newPropertyValue = componentOption.values[value]
       }
 
+      /**
+       * @type {ComponentProperty}
+       */
       const newProperty = {
-        name: option.name,
+        name: componentOption.name,
         value: newPropertyValue
       }
 
@@ -116,10 +121,10 @@ Component.prototype.modify = function (options, children = this.children) {
       for (let i = 0; i < properties.length; i++) {
         const property = properties[i]
 
-        if (property.name === option.name) {
+        if (property.name === componentOption.name) {
           hasProperty = true
 
-          if (property && option.join) {
+          if (property && componentOption.join) {
             newProperty.value = property.value + newProperty.value
           }
 
@@ -133,137 +138,25 @@ Component.prototype.modify = function (options, children = this.children) {
     }
   }
 
+  if (component.children) {
+    return {
+      id: component.id,
+      hash: component.hash,
+      properties,
+      children: component.children
+    }
+  }
+
   return {
-    id: this.id,
-    modifiedProperties: properties,
-    create: () => {
-      return this.create(null, properties, children)
-    }
+    id: component.id,
+    hash: component.hash,
+    properties
   }
 }
 
-Component.prototype.create = function (options, properties = this.properties, children = this.children) {
-  if (options) {
-    properties = this.modify(options).modifiedProperties
-  }
-
-  // lazy load web component
-  if (typeof this.component === 'function' && !this.componentIsLoaded) {
-    return new Promise((resolve, reject) => {
-      this.component()
-        .then(() => {
-          const element = document.createElement(this.tag)
-
-          this.componentIsLoaded = true
-
-          setProperties(element, properties)
-          appendChildren(element, children)
-
-          resolve(element)
-        })
-        .catch(error => reject(error))
-    })
-  }
-
-  let node
-
-  // Custom element constructor
-  if (typeof this.initialize === 'function') {
-    node = this.initialize()
-  } else {
-    // create element
-    node = document.createElement(this.tag)
-  }
-
-  setProperties(node, properties)
-  appendChildren(node, children)
-
-  return node
-}
-
-Component.prototype.getContent = function (node) {
-  if (!this.content) {
-    return
-  }
-
-  const content = {}
-
-  for (let i = 0; i < this.content.length; i++) {
-    const content = this.content[i]
-
-    content[node.name] = node[content.get]
-  }
-
-  return content
-}
-
-Component.prototype.setContent = function (node, values) {
-  if (!this.content) {
-    return
-  }
-
-  for (let i = 0; i < this.content.length; i++) {
-    const content = this.content[i]
-
-    if (content.set) {
-      node[content.set] = values[content.name]
-    }
-  }
-}
-
-function appendChildren (element, children = []) {
-  if (!children.length) {
-    return
-  }
-
-  const nodeList = []
-  let hasPromise = false
-
-  for (let i = 0; i < children.length; i++) {
-    const childNode = children[i].create()
-
-    if (childNode instanceof Promise) {
-      hasPromise = true
-    }
-
-    nodeList.push(childNode)
-  }
-
-  if (hasPromise) {
-    Promise.all(nodeList)
-      .then((nodes) => {
-        for (let i = 0; i < nodes.length; i++) {
-          const node = nodes[i]
-
-          element.appendChild(node)
-        }
-      })
-  } else {
-    for (let i = 0; i < nodeList.length; i++) {
-      const node = nodeList[i]
-
-      element.appendChild(node)
-    }
-  }
-}
-
-
-/**
- * Set properties to element
- * @param {HTMLElement} element - view node id
- * @param {Object[]} properties
- * @private
- */
-function setProperties (element, properties = []) {
-  for (let i = 0; i < properties.length; i++) {
-    const { name, value } = properties[i]
-
-    if (element[name] != null) {
-      element[name] = value
-    } else {
-      element.setAttribute(name, value)
-    }
-  }
+export {
+  modifyComponent,
+  createComponent
 }
 
 export default createComponent
