799bfcbea7f23d2f29ff929165006978fbc96a0f
Thomas David <tom@mightysmile.com>
2024-05-30 18:41:00 +0200

    fix (app-server) Remove unused plugins

--- packages/app-server-plugins/src/index.js
+++ packages/app-server-plugins/src/index.js
@@ -1,42 +1,33 @@
 import $http from './http.js'
 import action from './action.js'
+import content from './content.js'
 import component from './component.js'
 import database from './database.js'
+import metadata from './metadata.js'
 import middleware from './middleware.js'
 import development from './development.js'
 import event from './event.js'
-import layout from './layout.js'
 import page from './page.js'
-import section from './section.js'
-import template from './template.js'
-import templateBuild from './template-build.js'
 import theme from './theme.js'
 import user from './user.js'
-import widget from './widget.js'
 
 export * from './database.js'
 export * from './http.js'
-export * from './component.js'
 export * from './middleware.js'
-export * from './template-build.js'
 export * from './page.js'
-export * from './template.js'
 export * from './theme.js'
 
 export {
   $http,
-  database,
-  middleware,
   action,
+  database,
+  metadata,
+  middleware,
   user,
-  layout,
+  content,
   component,
   development,
   event,
   page,
-  section,
-  template,
-  templateBuild,
-  theme,
-  widget
+  theme
 }
