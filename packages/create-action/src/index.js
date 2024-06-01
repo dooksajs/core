4f4067eec4aba664e6f8928fae62efe3511d105c
Thomas David <tom@mightysmile.com>
2024-06-01 08:17:18 +0200

    feature (create-action): Parse actions

---
+++ packages/create-action/src/index.js
@@ -0,0 +1,313 @@
+import parseAction from './parse-action.js'
+import availableMethods from './available-methods.js'
+
+/**
+ * @typedef {Object} Action
+ * @property {ActionDispatch} [action_dispatch]
+ * @property {DeleteDataValue} [delete_dataValue]
+ * @property {ComponentAppend} [component_append]
+ * @property {ComponentAppend} [component_appendTemplate]
+ * @property {EvalCondition} [eval_condition]
+ * @property {GetActionValue} [get_actionValue]
+ * @property {GetBlockValue} [get_blockValue]
+ * @property {Action|string[]|string} [get_contextValue]
+ * @property {Action|string[]|string} [get_payloadValue]
+ * @property {Action|string[]|string} [get_sequenceValue]
+ * @property {GetDataValue} [get_dataValue]
+ * @property {SetActionValue} [set_actionValue]
+ * @property {SetDataValue} [set_dataValue]
+ * @property {'$null'} [data_generateId]
+ * @property {DataFind} [data_find]
+ * @property {FetchGetAll} [fetch_getAll]
+ * @property {FetchGetById} [fetch_getById]
+ * @property {ListFilter} [list_filter]
+ * @property {ListForEach} [list_forEach]
+ * @property {ListPush} [list_push]
+ * @property {ListSort} [list_sort]
+ * @property {ListSplice} [list_splice]
+ * @property {OperatorCompare[]} [operator_compare]
+ * @property {OperatorEval} [operator_eval]
+ * @property {QueryFetch} [query_fetch]
+ * @property {QueryFilter} [query_filter]
+ * @property {'$null'} [route_currentId]
+ * @property {'$null'} [route_currentPath]
+ * @property {RouteNavigate} [route_navigate]
+ * @property {BootstrapModalCreate} [bootstrap_modalCreate]
+ * @property {BootstrapModalShow} [bootstrap_modalShow]
+ */
+
+/**
+ * @typedef {Object} ComponentAppend
+ * @property {Action|string} nodeId
+ * @property {Action|string} id
+ */
+
+/**
+ * @typedef {Object} ActionDispatch
+ * @property {Action|string} id
+ * @property {Object} [context]
+ * @property {Action|string} [context.contentId]
+ * @property {Action|string} [context.componentId]
+ * @property {Array<Action|string>} [context.components]
+ * @property {Action} [payload]
+ */
+
+/**
+ * @typedef {'action/block'|
+ * 'action/items'|
+ * 'action/sequences'|
+ * 'action/values'|
+ * 'content/items'|
+ * 'component/items'|
+ * 'component/children'|
+ * 'component/content'|
+ * 'content/items'|
+ * 'event/listeners'|
+ * 'event/handlers'|
+ * 'layout/items'|
+ * 'layout/eventListeners'|
+ * 'metadata/currentLanguage'|
+ * 'metadata/currentLanguages'|
+ * 'page/id'|
+ * 'page/events'|
+ * 'page/items'|
+ * 'query/items'|
+ * 'query/where'|
+ * 'query/sort'} DataCollectionName
+ */
+
+/**
+ * @typedef {Object} DeleteDataValue
+ * @property {Action|DataCollectionName} name
+ * @property {Action|string} id
+ * @property {Action|boolean} [cascade]
+ * @property {Action|boolean} [listeners]
+ */
+
+/**
+ * @typedef {'=='|
+ * '!='|
+ * '>'|
+ * '>='|
+ * '<'|
+ * '<='|
+ * '!'|
+ * '%'|
+ * '++'|
+ * '--'|
+ * '-'|
+ * '+'|
+ * '*'|
+ * '**'|
+ * '!!'|
+ * '~'} Operator
+ */
+
+/**
+ * @typedef {Object} EvalConditionFrom
+ * @property {Operator} op
+ * @property {Action|string|number} from
+ * @property {Action|string|number} to
+ */
+
+/**
+ * @typedef {Object} EvalConditionAndOr
+ * @property {'&&'|'||'} andOr
+ */
+
+/**
+ * @typedef {Object} EvalCondition
+ * @property {Array<EvalConditionFrom|EvalConditionAndOr>} if
+ * @property {number[]} then
+ * @property {number[]} else
+ */
+
+/**
+ * @typedef {Object} GetActionValue
+ * @property {Action|string} id
+ * @property {Action|string[]|string} [query]
+ */
+
+/**
+ * @typedef {Object} GetBlockValue
+ * @property {Action|string} value
+ * @property {Action|string[]|string} query
+ */
+
+/**
+ * @typedef {Object} GetDataValue
+ * @property {Action|DataCollectionName} name
+ * @property {Object} [query]
+ * @property {Action|string} [query.id]
+ * @property {Action|string} [query.prefixId]
+ * @property {Action|string} [query.suffixId]
+ * @property {Object} [query.options]
+ * @property {Action|boolean} [query.options.expand]
+ * @property {Object.<string,Action|boolean>} [query.options.expandExclude]
+ * @property {Action|boolean} [query.options.expandClone]
+ * @property {Action|string[]|string|number[]|number} [query.options.position]
+ * @property {Action|boolean} [query.options.clone]
+ */
+
+/**
+ * @typedef {Object} SetActionValue
+ * @property {Action|string} id
+ * @property {Action|string} [prefixId]
+ * @property {Action|string} [suffixId]
+ * @property {Action|string|number|boolean} value
+ */
+
+/**
+ * @typedef {Object} SetDataValueOptions
+ * @property {Action|string} id
+ * @property {Action|boolean} stopPropagation
+ * @property {Action|boolean} merge
+ * @property {Object} update
+ * @property {Action[]|string[]|number[]} update.position
+ * @property {'push'|'pull'|'pop'|'shift'|'unshift'|'splice'} update.method
+ */
+
+/**
+ * @typedef {Object} SetDataValue
+ * @property {Action|DataCollectionName} name
+ * @property {Action|Action[]|string|number|boolean} value
+ * @property {SetDataValueOptions} [options]
+ */
+
+/**
+ * @typedef {Object} DataFind
+ * @property {Action|DataCollectionName} name
+ * @property {Object[]} where
+ * @property {Action|string} where[].name
+ * @property {Operator} where[].op
+ * @property {Action|string|number} where[].value
+ * @property {Object[]} where[].and
+ * @property {Action|string} where[].and[].name
+ * @property {Operator} where[].and[].op
+ * @property {Action|string|number} where[].and[].value
+ * @property {Object[]} where[].or
+ * @property {Action|string} where[].or[].name
+ * @property {Operator} where[].or[].op
+ * @property {Action|string|number} where[].or[].value
+ */
+
+/**
+ * @typedef {Object} FetchGetAll
+ * @property {Action|string} collection
+ * @property {Action|boolean} expand
+ * @property {Action|number} limit
+ * @property {Action|number} page
+ * @property {Action|number} perPage
+ * @property {Action|boolean} sync
+ * @property {Action|string} where
+ */
+
+/**
+ * @typedef {Object} FetchGetById
+ * @property {Action|string} collection
+ * @property {Action|string[]} id
+ * @property {Action|boolean} expand
+ * @property {Action|boolean} sync
+ */
+
+/**
+ * @typedef {Object} ListFilter
+ * @property {Action|Action[]|string[]|number[]} items
+ * @property {Object[]} options
+ * @property {Action|string} options[].name
+ * @property {Action|string|number|boolean} options[].value
+ */
+
+/**
+ * @typedef {Object} ListForEach
+ * @property {Action} items
+ * @property {Action|string} actionId
+ * @property {Action} [context]
+ * @property {Action|boolean} [async]
+ */
+
+/**
+ * @typedef {Object} ListPush
+ * @property {Action|Action[]|string[]|number[]} target
+ * @property {Action|string|number} source
+ */
+
+/**
+ * @typedef {Object} ListSort
+ * @property {Action|Action[]|string[]|number[]} items
+ * @property {'ascending'|'descending'} type
+ */
+
+/**
+ * @typedef {Object} ListSplice
+ * @property {Action|Action[]|string[]|number[]} target
+ * @property {Action|string|number|boolean} source
+ * @property {Action|number} [start]
+ * @property {Action|number} [number]
+ */
+
+/**
+ * @typedef {Object} OperatorCompare
+ * @property {Action|string|number|boolean} value_1
+ * @property {Action|string|number|boolean} value_2
+ * @property {Operator} op
+ */
+
+/**
+ * @typedef {Object} OperatorEval
+ * @property {Action|string} name
+ * @property {Array<Action|string|number>} values
+ */
+
+/**
+ * @typedef {Object} QueryFilter
+ * @property {Action|string} id
+ * @property {Action|string} componentId
+ */
+
+/**
+ * @typedef {Object} QueryFetch
+ * @property {Action|string} id
+ */
+
+/**
+ * @typedef {Object} RouteNavigate
+ * @property {Action|string} to
+ */
+
+/**
+ * @typedef {Object} BootstrapModalCreate
+ * @property {Action|string} componentId
+ * @property {Object} options
+ * @property {Action|boolean|'static'} options.backdrop
+ * @property {Action|boolean} focus
+ * @property {Action|boolean} keyboard
+ */
+
+/**
+ * @typedef {Object} BootstrapModalShow
+ * @property {Action|string} componentId
+ */
+
+/**
+ * @param {string} id
+ * @param {Action} data
+ * @param {Object.<string, boolean>} [availableActions=availableMethods] - Allowed actions
+ */
+function createAction (id, data, availableActions = availableMethods) {
+  const action = parseAction(data, availableActions)
+
+  return {
+    id,
+    blocks: action.blocks,
+    sequences: action.sequence,
+    sequenceId: action.sequenceId
+  }
+}
+
+export {
+  createAction,
+  availableMethods
+}
+
+export default createAction
---
+++ packages/create-action/src/parse-action.js
@@ -0,0 +1,212 @@
+import { objectHash } from '@dooksa/utils'
+
+/**
+ * @typedef {Object} ActionSequence - The sequence that will construct the final action
+ * @property {string} id - Reference id to an action item
+ * @property {Array.<string>} path - A list of keys related to the target object that is used to place the returned action value
+ */
+
+/**
+ * @typedef {Object} Action
+ * @property {Object.<string, ActionBlock>} action.items - Collection of actions
+ * @property {ActionSequence[]} action.sequence - The sequence that will construct the final action
+ */
+
+/**
+ * @typedef {Object} ActionBlock
+ * @property {string} ActionBlock._$a - Action name
+ * @property {(Object|Array|string|number)} ActionBlock._$arg - Action parameters
+ */
+
+/**
+ * Convert template action to dsAction data
+ * @param {Object} source
+ * @param {Object} availableMethods - List of active methods
+ * @returns {Action} - Data that can be used to set dsAction/items and dsAction/sequence
+ */
+export default (source, availableMethods) => {
+  const actions = convertToActions(source, availableMethods)
+
+  if (!actions.length) {
+    return
+  }
+
+  const blocks = {}
+  const sequences = []
+  let children = []
+  let depth = actions[0].path.length
+
+  for (let i = 0; i < actions.length; i++) {
+    const action = actions[i]
+    const id = objectHash(action.source)
+    const item = {
+      path: action.path,
+      id
+    }
+    let node = source
+
+    if (depth > action.path.length) {
+      let split = false
+
+      // check if child values belong to current action
+      for (let i = 0; i < children.length; i++) {
+        const childAction = actions[children[i]]
+
+        // check if previous action belongs to the same parent action
+        if (action.path.lastIndexOf('_$arg') !== childAction.path.lastIndexOf('_$arg')) {
+          // check if actions belong to the same lineage
+          for (let k = 0; k < action.path.length; k++) {
+            const a = action.path[k]
+            const b = childAction.path[k]
+
+            if (a !== b) {
+              children.splice(i, 1)
+              break
+            }
+          }
+
+          split = true
+          break
+        }
+      }
+
+      if (split && children.length) {
+        item.children = children.slice()
+        children = []
+      }
+    }
+
+    // exclude own key
+    const keyLength = action.path.length - 1
+
+    for (let i = 0; i < keyLength; i++) {
+      const key = action.path[i]
+
+      node = node[key]
+    }
+
+    children.push(i)
+    depth = action.path.length
+
+    if (keyLength > -1) {
+      node[action.path[keyLength]] = {
+        _$val: i
+      }
+    }
+
+    sequences.push(item)
+
+    blocks[id] = action.source
+  }
+
+  // update last sequence children
+  const lastIndex = sequences.length - 1
+  sequences[lastIndex].children = getLastSequenceChildren(sequences, lastIndex)
+
+  const sequenceId = objectHash(sequences)
+
+  return { blocks, sequences, sequenceId }
+}
+
+/**
+ * Extract actions from action templates
+ * @private
+ * @param {Object} source - Action template
+ * @param {Object} availableMethods - List of active methods
+ * @param {Object} [node={ path: [] }]
+ * @param {string[]} [node.path] - List of keys to assign to parent object
+ * @param {Object[]} actions - Dooksa actions
+ * @param {number} [lastNode] - Last key in object
+ * @param {boolean} [isHead] - Indicates that the current possible is the root element
+ * @returns {Object[]|Object}
+ */
+function convertToActions (
+  source,
+  availableMethods,
+  node = { path: [] },
+  actions = [],
+  lastNode = 0,
+  isHead = true
+) {
+  const keys = Object.keys(source)
+
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i]
+    const value = source[key]
+    let isAction = false
+
+    // store current node split ⅄
+    if (keys.length > 1) {
+      lastNode = node.path.length
+    }
+
+    if (availableMethods[key]) {
+      isAction = true
+      source._$a = key
+
+      if (value != null && value !== '$null') {
+        if (Array.isArray(value)) {
+          source._$arg = value.slice()
+        } else if (typeof value === 'object') {
+          source._$arg = Object.assign({}, value)
+        } else {
+          source._$arg = value
+        }
+      }
+
+      delete source[key]
+
+      actions.unshift({
+        path: node.path.slice(),
+        source
+      })
+    }
+
+    if (typeof value === 'object') {
+      if (isAction) {
+        key = '_$arg'
+      }
+
+      node.path.push(key)
+
+      const result = convertToActions(
+        value,
+        availableMethods,
+        node,
+        actions,
+        lastNode,
+        false
+      )
+
+      // clear keys up until last working node
+      node.path = result.node.path.slice(0, lastNode)
+    }
+  }
+
+  if (isHead) {
+    return actions
+  }
+
+  return { actions, node }
+}
+
+/**
+ * Get the last sequence children "data"
+ * @param {ActionSequence} sequences - List of sequences
+ * @param {number} length - Last index of sequence
+ * @returns {Array}
+ */
+function getLastSequenceChildren (sequences, length) {
+  const children = []
+
+  for (let i = 0; i < length; i++) {
+    const sequence = sequences[i]
+
+    // check for nested functions and ignore
+    if (sequence.path.indexOf('_$arg') === sequence.path.lastIndexOf('_$arg')) {
+      children.push(i)
+    }
+  }
+
+  return children
+}
