fb14f1930d7efd7a57267caea024999e96e37dd9
Thomas David <tom@mightysmile.com>
2024-06-02 22:34:44 +0200

    Remove unused template and schema

--- packages/schema/__test__/validate.js
+++
@@ -1,9 +0,0 @@
-import { getSchemaValidator } from '../src/index.js'
-
-const validate = getSchemaValidator()
-
-if (validate.name === 'validate20') {
-  console.log('Pass')
-} else {
-  throw new Error('Schema failed')
-}
--- packages/templates/src/index.js
+++
@@ -1,1 +0,0 @@
-export const templatePath = import.meta.dirname
