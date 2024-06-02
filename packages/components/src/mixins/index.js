210615e530053bdc43376a799056f93922aeef85
Thomas David <tom@mightysmile.com>
2024-06-02 22:22:54 +0200

    feature (components): Export mixin

---
+++ packages/components/src/mixins/index.js
@@ -0,0 +1,17 @@
+import background from './background.js'
+import position from './position.js'
+import spacing from './spacing.js'
+import level from './level.js'
+import translate from './translate.js'
+import inset from './inset.js'
+import button from './button.js'
+
+export {
+  level,
+  button,
+  background,
+  inset,
+  spacing,
+  position,
+  translate
+}
