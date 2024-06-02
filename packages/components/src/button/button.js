70b2db0df3dd46bc562c714f5949e196b57f1029
Thomas David <tom@mightysmile.com>
2024-06-02 22:23:39 +0200

    fix (components): Use button mixin

--- packages/components/src/button/button.js
+++ packages/components/src/button/button.js
@@ -1,9 +1,14 @@
 import { createComponent, modifyComponent } from '@dooksa/create-component'
+import button from '../mixins/button.js'
 import text from '../text/text.js'
 import icon from '../icon/icon.js'
 
-const btnText = modifyComponent(text, { text: 'Button' })
-const btnIcon = modifyComponent(icon, { icon: 'material-symbols:info-outline' })
+const btnText = modifyComponent(text, {
+  options: { text: 'Button' }
+})
+const btnIcon = modifyComponent(icon, {
+  options: { icon: 'material-symbols:info-outline' }
+})
 
 export default createComponent({
   id: 'button',
@@ -18,98 +23,5 @@ export default createComponent({
     { name: 'className',
       value: 'btn'
     }
-  ],
-  options: {
-    type: {
-      name: 'type',
-      values: {
-        button: 'button',
-        reset: 'rest',
-        submit: 'submit'
-      }
-    },
-    variant: {
-      name: 'className',
-      join: true,
-      values: {
-        primary: 'btn-primary',
-        secondary: 'btn-secondary',
-        success: 'btn-success',
-        warning: 'btn-warning',
-        danger: 'btn-danger',
-        info: 'btn-info',
-        light: 'btn-light',
-        dark: 'btn-dark',
-        link: 'btn-link',
-        outlinePrimary: 'btn-outline-primary',
-        outlineSecondary: 'btn-outline-secondary',
-        outlineSuccess: 'btn-outline-success',
-        outlineDanger: 'btn-outline-danger',
-        outlineWarning: 'btn-outline-warning',
-        outlineInfo: 'btn-outline-info',
-        outlineLight: 'btn-outline-light',
-        outlineDark: 'btn-outline-dark'
-      }
-    },
-    close: {
-      name: 'className',
-      values: {
-        close: 'btn-close'
-      }
-    }
-  },
-  styles: [
-    {
-      name: 'btn-padding-x',
-      type: 'unit'
-    },
-    {
-      name: 'btn-font-family',
-      type: 'font-family'
-    },
-    {
-      name: 'btn-font-weight',
-      type: 'unit'
-    },
-    {
-      name: 'btn-line-height',
-      type: 'number'
-    },
-    {
-      name: 'btn-color',
-      type: 'rgba'
-    },
-    {
-      name: 'btn-bg',
-      type: 'rgba'
-    },
-    {
-      name: 'btn-border-width',
-      type: 'unit'
-    },
-    {
-      name: 'btn-border-color',
-      type: 'rgba'
-    },
-    {
-      name: 'btn-border-radius',
-      type: 'unit'
-    },
-    {
-      name: 'btn-hover-border-color',
-      type: 'rgba'
-    },
-    {
-      name: 'btn-box-shadow',
-      type: 'box-shadow'
-    },
-    {
-      name: 'btn-disabled-opacity',
-      type: 'unit'
-    },
-    {
-      name: 'btn-focus-box-shadow',
-      type: 'box-shadow'
-    }
   ]
-})
+}, [button])
