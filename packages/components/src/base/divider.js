143a8af1ed99c6f5545d60791dd091e33e984f08
Thomas David <tom@mightysmile.com>
2024-06-04 13:21:13 +0200

    fix (components): Add displayFlex to div

--- packages/components/src/base/divider.js
+++ packages/components/src/base/divider.js
@@ -1,7 +1,7 @@
 import { createComponent } from '@dooksa/create-component'
-import { background, position, spacing, level, inset, translate, button } from '../mixins/index.js'
+import { background, position, spacing, level, inset, translate, button, displayFlex } from '../mixins/index.js'
 
 export default createComponent({
   id: 'divider',
   tag: 'div'
-}, [background, position, spacing, level, inset, translate, button])
+}, [background, position, spacing, level, inset, translate, button, displayFlex])
