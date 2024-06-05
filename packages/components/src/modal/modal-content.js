eaa994853f8067df4ed25227095fc42e337cf884
Thomas David <tom@mightysmile.com>
2024-06-05 21:57:12 +0200

    feature (components): Modal content extender

--- packages/components/src/modal/modal-content.js
+++ packages/components/src/modal/modal-content.js
@@ -1,9 +1,10 @@
 import modalHeader from './modal-header.js'
 import modalBody from './modal-body.js'
 import modalFooter from './modal-footer.js'
-import createComponent from '@dooksa/create-component'
+import { createComponent, extendComponent } from '@dooksa/create-component'
+import { shadowMixin } from '../mixins/index.js'
 
-export default createComponent({
+const modalContent = createComponent({
   id: 'modal-content',
   tag: 'div',
   children: [modalHeader, modalBody, modalFooter],
@@ -14,4 +15,28 @@ export default createComponent({
       value: 'modal-content'
     }
   ]
-})
+}, [shadowMixin])
+
+/**
+ * @typedef {import('@dooksa/create-component').ComponentExtend} ComponentExtend
+ * @typedef {import('../mixins/shadow.js').ShadowMixin} ShadowMixin
+ */
+
+/**
+ * @typedef {Object} ComponentExtendModalContent
+ * @property {ShadowMixin} options
+ */
+
+/**
+ * @param {ComponentExtend|ComponentExtendModalContent} options
+ */
+function extendModalComponent (options) {
+  return extendComponent(modalContent, options)
+}
+
+export {
+  modalContent,
+  extendModalComponent
+}
+
+export default modalContent
