f8fb8775b422b24bfa3590bc5d0579840faa6a0a
Thomas David <tom@mightysmile.com>
2024-06-05 19:52:31 +0200

    fix (components): Display mixin makes use of new extend data pattern

--- packages/components/src/mixins/display.js
+++ packages/components/src/mixins/display.js
@@ -1,122 +1,161 @@
 import { createMixin } from '@dooksa/create-component'
 
+/**
+ * @typedef {import('@dooksa/create-component').BreakpointAlwaysXXL} BreakpointAlwaysXXL
+ */
+
+/**
+ * @typedef {Object} DisplayMixin
+ * @property {BreakpointAlwaysXXL} [displayBlock]
+ * @property {BreakpointAlwaysXXL} [displayFlex]
+ * @property {BreakpointAlwaysXXL} [displayGrid]
+ * @property {BreakpointAlwaysXXL} [displayInlineBlock]
+ * @property {BreakpointAlwaysXXL} [displayInlineFlex]
+ * @property {BreakpointAlwaysXXL} [displayInlineGrid]
+ * @property {BreakpointAlwaysXXL} [displayTable]
+ * @property {BreakpointAlwaysXXL} [displayTableCell]
+ * @property {BreakpointAlwaysXXL} [displayTableRow]
+ */
+
 export default createMixin({
   metadata: {
     id: 'display'
   },
   data: {
     options: {
-      display: {
+      displayFlex: {
         name: 'className',
-        computedValues: {
-          /**
-           * Flex
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          flex (breakpoint) {
-            if (breakpoint) {
-              return 'd-' + breakpoint + '-flex'
-            }
-
-            return 'd-flex'
-          },
-          /**
-           * Inline flex
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          inlineFlex (breakpoint) {
-            if (breakpoint) {
-              return 'd-' + breakpoint + '-inline-flex'
-            }
-
-            return 'd-inline-flex'
-          },
-          /**
-           * Block
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          block (breakpoint) {
-            if (breakpoint) {
-              return 'd-' + breakpoint + '-block'
-            }
-
-            return 'd-block'
-          },
-          /**
-           * Inline block
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          inlineBlock (breakpoint) {
-            if (breakpoint) {
-              return 'd-' + breakpoint + '-inline'
-            }
-
-            return 'd-inline'
-          },
-          /**
-           * Grid
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          grid (breakpoint) {
-            if (breakpoint) {
-              return 'd-' + breakpoint + '-grid'
-            }
-
-            return 'd-grid'
-          },
-          /**
-           * Inline grid
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          inlineGrid (breakpoint) {
-            if (breakpoint) {
-              return 'd-' + breakpoint + '-inline-grid'
-            }
-
-            return 'd-inline-grid'
-          },
-          /**
-           * Table
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          table (breakpoint) {
-            if (breakpoint) {
-              return 'd-' + breakpoint + '-table'
-            }
-
-            return 'd-table'
-          },
-          /**
-           * Table cell
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          tableCell (breakpoint) {
-            if (breakpoint) {
-              return 'd-' + breakpoint + '-table-cell'
-            }
-
-            return 'd-table-cell'
-          },
-          /**
-           * Table row
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          tableRow (breakpoint) {
-            if (breakpoint) {
-              return 'd-' + breakpoint + '-table-row'
-            }
-
-            return 'd-table-row'
+        /**
+         * Flex
+         * @param {BreakpointAlwaysXXL} [breakpoint]
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'd-' + breakpoint + '-flex'
           }
+
+          return 'd-flex'
+        }
+      },
+      displayInlineFlex: {
+        name: 'className',
+        /**
+         * Inline flex
+         * @param {BreakpointAlwaysXXL} [breakpoint]
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'd-' + breakpoint + '-inline-flex'
+          }
+
+          return 'd-inline-flex'
+        }
+      },
+      displayBlock: {
+        name: 'className',
+        /**
+         * Block
+         * @param {BreakpointAlwaysXXL} [breakpoint]
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'd-' + breakpoint + '-block'
+          }
+
+          return 'd-block'
+        }
+      },
+      displayInlineBlock: {
+        name: 'className',
+        /**
+         * Inline block
+         * @param {BreakpointAlwaysXXL} [breakpoint]
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'd-' + breakpoint + '-inline'
+          }
+
+          return 'd-inline'
+        }
+      },
+      displayGrid: {
+        name: 'className',
+        /**
+         * Grid
+         * @param {BreakpointAlwaysXXL} [breakpoint]
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'd-' + breakpoint + '-grid'
+          }
+
+          return 'd-grid'
+        }
+      },
+      displayInlineGrid: {
+        name: 'className',
+        /**
+         * Inline grid
+         * @param {BreakpointAlwaysXXL} [breakpoint]
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'd-' + breakpoint + '-inline-grid'
+          }
+
+          return 'd-inline-grid'
+        }
+      },
+      displayTable: {
+        name: 'className',
+        /**
+         * Table
+         * @param {BreakpointAlwaysXXL} [breakpoint]
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'd-' + breakpoint + '-table'
+          }
+
+          return 'd-table'
+        }
+      },
+      displayTableCell: {
+        name: 'className',
+        /**
+         * Table cell
+         * @param {BreakpointAlwaysXXL} [breakpoint]
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'd-' + breakpoint + '-table-cell'
+          }
+
+          return 'd-table-cell'
+        }
+      },
+      displayTableRow: {
+        name: 'className',
+        /**
+         * Table row
+         * @param {BreakpointAlwaysXXL} [breakpoint]
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'd-' + breakpoint + '-table-row'
+          }
+
+          return 'd-table-row'
         }
       }
     }
