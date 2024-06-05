3d7cd42c92f3ed283bed3febe2c0b455c323dbe6
Thomas David <tom@mightysmile.com>
2024-06-05 22:00:46 +0200

    fix (components): Flatten rounded mixin options

--- packages/components/src/mixins/rounded.js
+++ packages/components/src/mixins/rounded.js
@@ -1,74 +1,97 @@
 import { createMixin } from '@dooksa/create-component'
 
+/**
+ * @typedef {'0'|'1'|'2'|'3'|'4'|'5'|'circle'|'pill'|'regular'} Rounded
+ */
+
+/**
+ * @typedef {Object} RoundedMixin
+ * @property {Rounded} [rounded]
+ * @property {Rounded} [roundedTop]
+ * @property {Rounded} [roundedBottom]
+ * @property {Rounded} [roundedEnd]
+ * @property {Rounded} [roundedStart]
+ */
+
 export default createMixin({
   metadata: {
-    id: 'shadow'
+    id: 'rounded'
   },
   data: {
     options: {
       rounded: {
         name: 'className',
-        computedValues: {
-          /**
-           * Rounded
-           * @param {number|'circle'|'pill'} strength
-           * @returns {string}
-           */
-          all (strength) {
-            if (strength) {
-              return 'rounded-' + strength
-            }
-
-            return 'rounded'
-          },
-          /**
-           * Rounded top
-           * @param {number|'circle'|'pill'} strength
-           * @returns {string}
-           */
-          top (strength) {
-            if (strength) {
-              return 'rounded-top' + strength
-            }
-
-            return 'rounded-top'
-          },
-          /**
-           * Rounded end
-           * @param {number|'circle'|'pill'} strength
-           * @returns {string}
-           */
-          end (strength) {
-            if (strength) {
-              return 'rounded-end' + strength
-            }
-
-            return 'rounded-end'
-          },
-          /**
-           * Rounded bottom
-           * @param {number|'circle'|'pill'} strength
-           * @returns {string}
-           */
-          bottom (strength) {
-            if (strength) {
-              return 'rounded-bottom' + strength
-            }
-
-            return 'rounded-bottom'
-          },
-          /**
-           * Rounded start
-           * @param {number|'circle'|'pill'} strength
-           * @returns {string}
-           */
-          start (strength) {
-            if (strength) {
-              return 'rounded-start' + strength
-            }
-
-            return 'rounded-start'
+        /**
+         * Rounded
+         * @param {Rounded} strength
+         * @returns {string}
+         */
+        computedValue (strength) {
+          if (strength !== 'regular') {
+            return 'rounded-' + strength
           }
+
+          return 'rounded'
+        }
+      },
+      roundedTop: {
+        name: 'className',
+        /**
+         * Rounded top
+         * @param {Rounded} strength
+         * @returns {string}
+         */
+        computedValue (strength) {
+          if (strength !== 'regular') {
+            return 'rounded-top' + strength
+          }
+
+          return 'rounded-top'
+        }
+      },
+      roundedEnd: {
+        name: 'className',
+        /**
+         * Rounded end
+         * @param {Rounded} strength
+         * @returns {string}
+         */
+        computedValue (strength) {
+          if (strength !== 'regular') {
+            return 'rounded-end' + strength
+          }
+
+          return 'rounded-end'
+        }
+      },
+      roundedBottom: {
+        name: 'className',
+        /**
+         * Rounded bottom
+         * @param {Rounded} strength
+         * @returns {string}
+         */
+        computedValue (strength) {
+          if (strength !== 'regular') {
+            return 'rounded-bottom' + strength
+          }
+
+          return 'rounded-bottom'
+        }
+      },
+      roundedStart: {
+        name: 'className',
+        /**
+         * Rounded start
+         * @param {Rounded} strength
+         * @returns {string}
+         */
+        computedValue (strength) {
+          if (strength !== 'regular') {
+            return 'rounded-start' + strength
+          }
+
+          return 'rounded-start'
         }
       }
     }
