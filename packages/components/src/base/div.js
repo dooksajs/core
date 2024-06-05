e75af1d5b932429e9ce63131b52cbb70742bd8b0
Thomas David <tom@mightysmile.com>
2024-06-05 21:55:20 +0200

    feature (components): DIv extender

---
+++ packages/components/src/base/div.js
@@ -0,0 +1,39 @@
+import { createComponent, extendComponent } from '@dooksa/create-component'
+import { backgroundMixin, buttonMixin, displayMixin, flexMixin, insetMixin, zIndexMixin, positionMixin, spacingMixin, translateMixin } from '../mixins/index.js'
+
+const div = createComponent({
+  id: 'div',
+  tag: 'div'
+}, [backgroundMixin, positionMixin, spacingMixin, zIndexMixin, insetMixin, translateMixin, buttonMixin, displayMixin, flexMixin])
+
+/**
+ * @typedef {import('@dooksa/create-component').ComponentExtend} ComponentExtend
+ * @typedef {import('../mixins/flex.js').FlexMixin} FlexMixin
+ * @typedef {import('../mixins/background.js').BackgroundMixin} BackgroundMixin
+ * @typedef {import('../mixins/position.js').PositionMixin} PositionMixin
+ * @typedef {import('../mixins/spacing.js').SpacingMixin} SpacingMixin
+ * @typedef {import('../mixins/z-index.js').ZIndexMixin} ZIndexMixin
+ * @typedef {import('../mixins/inset.js').InsetMixin} InsetMixin
+ * @typedef {import('../mixins/translate.js').TranslateMixin} TranslateMixin
+ * @typedef {import('../mixins/button.js').ButtonMixin} ButtonMixin
+ * @typedef {import('../mixins/display.js').DisplayMixin} DisplayMixin
+ */
+
+/**
+ * @typedef {Object} ComponentExtendDiv
+ * @property {BackgroundMixin|FlexMixin|PositionMixin|SpacingMixin|ZIndexMixin|InsetMixin|TranslateMixin|ButtonMixin|DisplayMixin} options
+ */
+
+/**
+ * @param {ComponentExtendDiv} options
+ */
+function extendDivComponent (options) {
+  return extendComponent(div, options)
+}
+
+export {
+  div,
+  extendDivComponent
+}
+
+export default div
--- packages/components/src/base/divider.js
+++
@@ -1,7 +0,0 @@
-import { createComponent } from '@dooksa/create-component'
-import { backgroundMixin, buttonMixin, displayMixin, flexMixin, insetMixin, levelMixin, positionMixin, spacingMixin, translateMixin } from '../mixins/index.js'
-
-export default createComponent({
-  id: 'divider',
-  tag: 'div'
-}, [backgroundMixin, positionMixin, spacingMixin, levelMixin, insetMixin, translateMixin, buttonMixin, displayMixin, flexMixin])
