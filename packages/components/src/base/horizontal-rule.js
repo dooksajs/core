0591bf9b5b7bba806645a9d5434429a7afeaab70
Thomas David <tom@mightysmile.com>
2024-06-05 22:03:39 +0200

    feature (components): Hr extender

--- packages/components/src/base/horizontal-rule.js
+++
@@ -1,7 +0,0 @@
-import { createComponent } from '@dooksa/create-component'
-import { spacingMixin, displayMixin, flexMixin } from '../mixins/index.js'
-
-export default createComponent({
-  id: 'horizontal-rule',
-  tag: 'hr'
-}, [spacingMixin, flexMixin, displayMixin])
---
+++ packages/components/src/base/hr.js
@@ -0,0 +1,33 @@
+import { createComponent, extendComponent } from '@dooksa/create-component'
+import { spacingMixin, displayMixin, flexMixin } from '../mixins/index.js'
+
+const hr = createComponent({
+  id: 'hr',
+  tag: 'hr'
+}, [spacingMixin, flexMixin, displayMixin])
+
+/**
+ * @typedef {import('@dooksa/create-component').ComponentExtend} ComponentExtend
+ * @typedef {import('../mixins/flex.js').FlexMixin} FlexMixin
+ * @typedef {import('../mixins/spacing.js').SpacingMixin} SpacingMixin
+ * @typedef {import('../mixins/display.js').DisplayMixin} DisplayMixin
+ */
+
+/**
+ * @typedef {Object} ComponentExtendHr
+ * @property {FlexMixin|SpacingMixin|DisplayMixin} options
+ */
+
+/**
+ * @param {ComponentExtend|ComponentExtendHr} options
+ */
+function extendHrComponent (options) {
+  return extendComponent(hr, options)
+}
+
+export {
+  hr,
+  extendHrComponent
+}
+
+export default hr
