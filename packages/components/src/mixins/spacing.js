6e3070d1d8c065dc59e2ae9c234dbc950c95e42f
Thomas David <tom@mightysmile.com>
2024-06-05 19:53:42 +0200

    fix (components): Spacing mixin makes use of new extend data pattern

--- packages/components/src/mixins/spacing.js
+++ packages/components/src/mixins/spacing.js
@@ -1,32 +1,63 @@
 import { createMixin } from '@dooksa/create-component'
 
+/**
+ * @typedef {'end'|'start'|'top'|'bottom'|'xAxis'|'yAxis'|'none'} SpacingDirection
+ */
+
+/**
+ * @typedef {Object} MarginMixin
+ * @property {'0'|'1'|'2'|'3'|'4'|'5'|'auto'} strength
+ * @property {SpacingDirection} [direction]
+ */
+
+/**
+ * @typedef {Object} PaddingMixin
+ * @property {'0'|'1'|'2'|'3'|'4'|'5'} strength
+ * @property {SpacingDirection} [direction]
+ */
+
+/**
+ * @typedef {Object} SpacingMixin
+ * @property {MarginMixin} [margin]
+ * @property {PaddingMixin} [padding]
+ */
+
+const directions = {
+  end: 'e',
+  start: 's',
+  top: 't',
+  bottom: 'b',
+  xAxis: 'x',
+  yAxis: 'y',
+  none: ''
+}
+
 export default createMixin({
   metadata: {
     id: 'spacing'
   },
   data: {
     options: {
-      spacing: {
+      margin: {
         name: 'className',
-        computedValues: {
-          /**
-           * Margin
-           * @param {number|'auto'} strength
-           * @param {'e'|'s'|'t'|'b'|'x'|'y'|''} [direction='']
-           * @returns {string}
-           */
-          margin (strength, direction = '') {
-            return 'm' + direction + '-' + strength
-          },
-          /**
-           * Padding
-           * @param {number} strength
-           * @param {'e'|'s'|'t'|'b'|'x'|'y'|''} [direction='']
-           * @returns {string}
-           */
-          padding (strength, direction = '') {
-            return 'p' + direction + '-' + strength
-          }
+        /**
+         * Margin
+         * @param {MarginMixin} param
+         * @returns {string}
+         */
+        computedValue ({ strength, direction = 'none' }) {
+          return 'm' + directions[direction] + '-' + strength
+        }
+      },
+      padding: {
+        name: 'className',
+        /**
+         * Padding
+         * @param {PaddingMixin} param
+         * @returns {string}
+         */
+        computedValue ({ strength, direction = 'none' }) {
+          return 'p' + directions[direction] + '-' + strength
         }
       }
     },
