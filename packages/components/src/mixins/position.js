f2a0170fb5f5fcb0c6e1be4ae47e48d89200ddd1
Thomas David <tom@mightysmile.com>
2024-06-05 19:53:19 +0200

    fix (components): Add JSdocs

--- packages/components/src/mixins/position.js
+++ packages/components/src/mixins/position.js
@@ -1,5 +1,10 @@
 import { createMixin } from '@dooksa/create-component'
 
+/**
+ * @typedef {Object} PositionMixin
+ * @property {'relative'|'static'|'absolute'|'sticky'|'fixed'} [position]
+ */
+
 export default createMixin({
   metadata: {
     id: 'position'
