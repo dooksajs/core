1714f7b4dc15b9bed7055021f3c24831e64c238b
Thomas David <tom@mightysmile.com>
2024-06-05 19:53:58 +0200

    fix (components): Flex mixin makes use of new extend data pattern

--- packages/components/src/mixins/flex.js
+++ packages/components/src/mixins/flex.js
@@ -1,262 +1,305 @@
 import { createMixin } from '@dooksa/create-component'
 
+/**
+ * @typedef {import('@dooksa/create-component').BreakpointAlwaysXXL} BreakpointAlwaysXXL
+ * @typedef {import('@dooksa/create-component').BreakpointSmXXL} BreakpointSmXXL
+ */
+
+/**
+ * @typedef {Object} JustifyContentMixin
+ * @property {'start'|'end'|'center'|'between'|'around'|'evenly'} position
+ * @property {BreakpointSmXXL} [breakpoint]
+ */
+
+/**
+ * @typedef {Object} AlignContentMixin
+ * @property {'start'|'end'|'center'|'between'|'around'|'stretch'} position
+ * @property {BreakpointSmXXL} [breakpoint]
+ */
+
+/**
+ * @typedef {Object} AlignItemsSelfMixin
+ * @property {'start'|'end'|'center'|'baseline'|'stretch'} position
+ * @property {BreakpointSmXXL} [breakpoint]
+ */
+
+/**
+ * @typedef {Object} FlexSizeMixin
+ * @property {'0'|'1'} size
+ * @property {BreakpointSmXXL} [breakpoint]
+ */
+
+/**
+ * @typedef {Object} FlexOrderMixin
+ * @property {'0'|'1'|'2'|'3'|'4'|'5'|'first'|'last'} position
+ * @property {BreakpointSmXXL} [breakpoint]
+ */
+
+/**
+ * @typedef {Object} FlexMixin
+ * @property {JustifyContentMixin} [justifyContent]
+ * @property {BreakpointAlwaysXXL} [flexDirectionRow]
+ * @property {BreakpointAlwaysXXL} [flexDirectionRowReverse]
+ * @property {BreakpointAlwaysXXL} [flexDirectionColumn]
+ * @property {BreakpointAlwaysXXL} [flexDirectionColumnReverse]
+ * @property {AlignContentMixin} [alignContent]
+ * @property {AlignItemsSelfMixin} [alignItems]
+ * @property {AlignItemsSelfMixin} [alignSelf]
+ * @property {BreakpointAlwaysXXL} [flexFill]
+ * @property {FlexSizeMixin} [flexGrow]
+ * @property {FlexSizeMixin} [flexShrink]
+ * @property {BreakpointAlwaysXXL} [flexWrap]
+ * @property {BreakpointAlwaysXXL} [flexWrapReverse]
+ * @property {BreakpointAlwaysXXL} [flexNoWrap]
+ * @property {BreakpointAlwaysXXL} [flexNoWrapReverse]
+ * @property {FlexOrderMixin} [flexOrder]
+ */
+
+
 export default createMixin({
   metadata: {
     id: 'flex'
   },
   data: {
     options: {
-      flex: {
+      flexDirectionRow: {
         name: 'className',
-        computedValues: {
-          /**
-           * Flex
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          block (breakpoint) {
-            if (breakpoint) {
-              return 'd-' + breakpoint + '-flex'
-            }
-
-            return 'd-flex'
-          },
-          /**
-           * Inline flex
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          inline (breakpoint) {
-            if (breakpoint) {
-              return 'd-inline-' + breakpoint + '-flex'
-            }
-
-            return 'd-inline-flex'
+        /**
+         * Flex direction row
+         * @param {BreakpointAlwaysXXL} breakpoint
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'flex-' + breakpoint + '-row'
           }
+
+          return 'flex-row'
         }
       },
-      flexDirection: {
+      flexDirectionRowReverse: {
         name: 'className',
-        computedValues: {
-          /**
-           * Flex direction row
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          row (breakpoint) {
-            if (breakpoint) {
-              return 'flex-' + breakpoint + '-row'
-            }
-
-            return 'flex-row'
-          },
-          /**
-           * Flex direction row reverse
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          rowReverse (breakpoint) {
-            if (breakpoint) {
-              return 'flex-' + breakpoint + '-row-reverse'
-            }
-
-            return 'flex-row-reverse'
-          },
-          /**
-           * Flex direction column
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          column (breakpoint) {
-            if (breakpoint) {
-              return 'flex-' + breakpoint + '-column'
-            }
-
-            return 'flex-column'
-          },
-          /**
-           * Flex direction column reverse
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          columnReverse (breakpoint) {
-            if (breakpoint) {
-              return 'flex-' + breakpoint + '-column-reverse'
-            }
-
-            return 'flex-column-reverse'
+        /**
+         * Flex direction row reverse
+         * @param {BreakpointAlwaysXXL} breakpoint
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'flex-' + breakpoint + '-row-reverse'
           }
+
+          return 'flex-row-reverse'
         }
       },
-      flexAlign: {
+      flexDirectionColumn: {
         name: 'className',
-        computedValues: {
-          /**
-           * Justify content
-           * @param {'start'|'end'|'center'|'between'|'around'|'evenly'} position
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          justifyContent (position, breakpoint) {
-            if (breakpoint) {
-              return 'justify-content-' + breakpoint + '-' + position
-            }
-
-            return 'justify-content-' + position
-          },
-          /**
-           * Align items
-           * @param {'start'|'end'|'center'|'between'|'around'|'stretch'} position
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          alignContent (position, breakpoint) {
-            if (breakpoint) {
-              return 'align-content-' + breakpoint + '-' + position
-            }
-
-            return 'align-content-' + position
-          },
-          /**
-           * Align items
-           * @param {'start'|'end'|'center'|'baseline'|'stretch'} position
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          alignItems (position, breakpoint) {
-            if (breakpoint) {
-              return 'align-items-' + breakpoint + '-' + position
-            }
-
-            return 'align-items-' + position
-          },
-          /**
-           * Align self
-           * @param {'start'|'end'|'center'|'baseline'|'stretch'} position
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          alignSelf (position, breakpoint) {
-            if (breakpoint) {
-              return 'align-self-' + breakpoint + '-' + position
-            }
-
-            return 'align-self-' + position
+        /**
+         * Flex direction column
+         * @param {BreakpointAlwaysXXL} breakpoint
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'flex-' + breakpoint + '-column'
           }
+
+          return 'flex-column'
+        }
+      },
+      flexDirectionColumnReverse: {
+        name: 'className',
+        /**
+         * Flex direction column reverse
+         * @param {BreakpointAlwaysXXL} breakpoint
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'flex-' + breakpoint + '-column-reverse'
+          }
+
+          return 'flex-column-reverse'
+        }
+      },
+      justifyContent: {
+        name: 'className',
+        /**
+         * Justify content
+         * @param {JustifyContentMixin} param
+         * @returns {string}
+         */
+        computedValue ({ position, breakpoint }) {
+          if (breakpoint) {
+            return 'justify-content-' + breakpoint + '-' + position
+          }
+
+          return 'justify-content-' + position
+        }
+      },
+      alignContent: {
+        name: 'className',
+        /**
+         * Align items
+         * @param {AlignContentMixin} param
+         * @returns {string}
+         */
+        computedValue ({ position, breakpoint }) {
+          if (breakpoint) {
+            return 'align-content-' + breakpoint + '-' + position
+          }
+
+          return 'align-content-' + position
+        }
+      },
+      alignItems: {
+        name: 'className',
+        /**
+         * Align items
+         * @param {AlignItemsSelfMixin} param
+         * @returns {string}
+         */
+        computedValue ({ position, breakpoint }) {
+          if (breakpoint) {
+            return 'align-content-' + breakpoint + '-' + position
+          }
+
+          return 'align-content-' + position
+        }
+      },
+      alignSelf: {
+        name: 'className',
+        /**
+         * Align self
+         * @param {AlignItemsSelfMixin} param
+         * @returns {string}
+         */
+        computedValue ({ position, breakpoint }) {
+          if (breakpoint) {
+            return 'align-self-' + breakpoint + '-' + position
+          }
+
+          return 'align-self-' + position
         }
       },
       flexFill: {
         name: 'className',
-        computedValues: {
-          /**
-           * Fill
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          fill (breakpoint) {
-            if (breakpoint) {
-              return 'flex-' + breakpoint + '-fill'
-            }
-
-            return 'flex-fill'
-          },
-          /**
-           * Grow
-           * @param {'0'|'1'} size
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          grow (size, breakpoint) {
-            if (breakpoint) {
-              return 'flex-' + breakpoint + '-grow-' + size
-            }
-
-            return 'flex-grow-' + size
-          },
-          /**
-           * Grow
-           * @param {'0'|'1'} size
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          shrink (size, breakpoint) {
-            if (breakpoint) {
-              return 'flex-' + breakpoint + '-shrink-' + size
-            }
-
-            return 'flex-shrink-' + size
+        /**
+         * Fill
+         * @param {BreakpointAlwaysXXL} breakpoint
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'flex-' + breakpoint + '-fill'
           }
+
+          return 'flex-fill'
+        }
+      },
+      flexGrow: {
+        name: 'className',
+        /**
+         * Flex grow
+         * @param {FlexSizeMixin} param
+         * @returns {string}
+         */
+        computedValue ({ size, breakpoint }) {
+          if (breakpoint) {
+            return 'flex-' + breakpoint + '-grow-' + size
+          }
+
+          return 'flex-grow-' + size
+        }
+      },
+      flexShrink: {
+        name: 'className',
+        /**
+         * Flex shrink
+         * @param {FlexSizeMixin} param
+         * @returns {string}
+         */
+        computedValue ({ size, breakpoint }) {
+          if (breakpoint) {
+            return 'flex-' + breakpoint + '-shrink-' + size
+          }
+
+          return 'flex-shrink-' + size
         }
       },
       flexWrap: {
         name: 'className',
-        computedValues: {
-          /**
-           * Wrap
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          wrap (breakpoint) {
-            if (breakpoint) {
-              return 'flex-' + breakpoint + '-wrap'
-            }
-
-            return 'flex-warp'
-          },
-          /**
-           * Wrap reverse
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          wrapReverse (breakpoint) {
-            if (breakpoint) {
-              return 'flex-' + breakpoint + '-wrap-reverse'
-            }
-
-            return 'flex-wrap-reverse'
-          },
-          /**
-           * No wrap
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          nowrap (breakpoint) {
-            if (breakpoint) {
-              return 'flex-' + breakpoint + '-nowrap'
-            }
-
-            return 'flex-nowrap'
-          },
-          /**
-           * No wrap reverse
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          nowrapReverse (breakpoint) {
-            if (breakpoint) {
-              return 'flex-' + breakpoint + '-nowrap-reverse'
-            }
-
-            return 'flex-nowrap-reverse'
+        /**
+         * Wrap
+         * @param {BreakpointAlwaysXXL} breakpoint
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'flex-' + breakpoint + '-wrap'
           }
+
+          return 'flex-warp'
         }
       },
-      order: {
+      flexWrapReverse: {
         name: 'className',
-        computedValues: {
-          /**
-           * Wrap
-           * @param {number|'first'|'last'} position
-           * @param {'sm'|'md'|'lg'|'xl'|'xxl'} [breakpoint]
-           * @returns {string}
-           */
-          value (position, breakpoint) {
-            if (breakpoint) {
-              return 'order-' + breakpoint + '-' + position
-            }
-
-            return 'order-' + position
+        /**
+         * Wrap reverse
+         * @param {BreakpointAlwaysXXL} breakpoint
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'flex-' + breakpoint + '-wrap-reverse'
           }
+
+          return 'flex-wrap-reverse'
+        }
+      },
+      flexNoWrap: {
+        name: 'className',
+        /**
+         * No wrap
+         * @param {BreakpointAlwaysXXL} breakpoint
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'flex-' + breakpoint + '-nowrap'
+          }
+
+          return 'flex-nowrap'
+        }
+      },
+      flexNoWrapReverse: {
+        name: 'className',
+        /**
+         * No wrap reverse
+         * @param {BreakpointAlwaysXXL} breakpoint
+         * @returns {string}
+         */
+        computedValue (breakpoint) {
+          if (breakpoint !== 'always') {
+            return 'flex-' + breakpoint + '-nowrap-reverse'
+          }
+
+          return 'flex-nowrap-reverse'
+        }
+      },
+      flexOrder: {
+        name: 'className',
+        /**
+         * Order
+         * @param {FlexOrderMixin} param
+         * @returns {string}
+         */
+        computedValue ({ position, breakpoint }) {
+          if (breakpoint) {
+            return 'order-' + breakpoint + '-' + position
+          }
+
+          return 'order-' + position
         }
       }
     }
