a8bd050dbd4294629f9d7355035c3af409f1b328
Thomas David <tom@mightysmile.com>
2024-06-06 13:51:32 +0200

    feature (components): Add mixins to modal body

--- packages/components/src/modal/modal-body.js
+++ packages/components/src/modal/modal-body.js
@@ -1,4 +1,5 @@
 import { createComponent, extendComponent } from '@dooksa/create-component'
+import { displayMixin, flexMixin, gapMixin, spacingMixin } from '../mixins/index.js'
 
 const modalBody = createComponent({
   id: 'modal-body',
@@ -10,17 +11,24 @@ const modalBody = createComponent({
       value: 'modal-body'
     }
   ]
-})
-
+}, [displayMixin, flexMixin, spacingMixin, gapMixin])
 
 /**
  * @typedef {import('@dooksa/create-component').ComponentExtend} ComponentExtend
+ * @typedef {import('../mixins/display.js').DisplayMixin} DisplayMixin
+ * @typedef {import('../mixins/spacing.js').SpacingMixin} SpacingMixin
+ * @typedef {import('../mixins/gap.js').GapMixin} GapMixin
+ * @typedef {import('../mixins/flex.js').FlexMixin} FlexMixin
  */
 
 /**
- * @param {ComponentExtend} options
+ * @typedef {Object} ExtendModalBody
+ * @property {DisplayMixin|SpacingMixin|GapMixin|FlexMixin} options
  */
 
+/**
+ * @param {ComponentExtend|ExtendModalBody} options
+ */
 function extendModalBody (options) {
   return extendComponent(modalBody, options)
 }
