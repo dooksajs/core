095f4d3d98f0b14dc478ee55bfc4e7466fb17acc
Thomas David <tom@mightysmile.com>
2024-06-06 11:16:18 +0200

    fix (eslint): Add consistent new lines to Object properties and Array values

--- eslint.config.js
+++ eslint.config.js
@@ -2,59 +2,76 @@ import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default [
   {
-    plugins: {
-      '@stylistic/js': stylisticJs
-    },
+    plugins: { '@stylistic/js': stylisticJs },
     rules: {
-      '@stylistic/js/indent': ['error', 2, {
-        SwitchCase: 1,
-        VariableDeclarator: 'first',
-        outerIIFEBody: 'off',
-        MemberExpression: 1,
-        FunctionExpression: { body: 1, parameters: 1 },
-        CallExpression: { arguments: 1 },
-        ArrayExpression: 1,
-        ObjectExpression: 1,
-        ImportDeclaration: 1,
-        flatTernaryExpressions: true,
-        ignoreComments: false
-      }],
+      '@stylistic/js/indent': [
+        'error', 2, {
+          SwitchCase: 1,
+          VariableDeclarator: 'first',
+          outerIIFEBody: 'off',
+          MemberExpression: 1,
+          FunctionExpression: {
+            body: 1,
+            parameters: 1
+          },
+          CallExpression: { arguments: 1 },
+          ArrayExpression: 1,
+          ObjectExpression: 1,
+          ImportDeclaration: 1,
+          flatTernaryExpressions: true,
+          ignoreComments: false
+        }
+      ],
       '@stylistic/js/space-before-function-paren': ['error', 'always'],
       '@stylistic/js/function-call-spacing': ['error', 'never'],
       '@stylistic/js/implicit-arrow-linebreak': ['error', 'beside'],
       '@stylistic/js/eol-last': ['error', 'always'],
-      '@stylistic/js/brace-style': ['error', '1tbs', {
-        allowSingleLine: false
-      }],
+      '@stylistic/js/brace-style': [
+        'error', '1tbs', { allowSingleLine: false }
+      ],
       '@stylistic/js/semi': ['error', 'never'],
-      '@stylistic/js/quotes': ['error', 'single', {
-        avoidEscape: true,
-        allowTemplateLiterals: true
-      }],
+      '@stylistic/js/quotes': [
+        'error', 'single', {
+          avoidEscape: true,
+          allowTemplateLiterals: true
+        }
+      ],
       '@stylistic/js/comma-dangle': ['error', 'never'],
-      '@stylistic/js/comma-spacing': ['error', {
-        before: false,
-        after: true
-      }],
+      '@stylistic/js/comma-spacing': [
+        'error', {
+          before: false,
+          after: true
+        }
+      ],
       '@stylistic/js/object-curly-spacing': ['error', 'always'],
+      '@stylistic/js/object-curly-newline': ['error', { 'consistent': true }],
+      '@stylistic/js/object-property-newline': ['error', { 'allowAllPropertiesOnSameLine': false }],
       '@stylistic/js/array-bracket-spacing': ['error', 'never'],
+      '@stylistic/js/array-bracket-newline': ['error', 'consistent'],
+      '@stylistic/js/array-element-newline': ['error', 'consistent'],
       '@stylistic/js/computed-property-spacing': ['error', 'never'],
       '@stylistic/js/no-mixed-operators': [
         'error',
         {
           'groups': [
-            ['+', '-', '*', '/', '%', '**'],
-            ['&', '|', '^', '~', '<<', '>>', '>>>'],
-            ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+            [
+              '+', '-', '*', '/', '%', '**'
+            ],
+            [
+              '&', '|', '^', '~', '<<', '>>', '>>>'
+            ],
+            [
+              '==', '!=', '===', '!==', '>', '>=', '<', '<='
+            ],
             ['&&', '||'],
             ['in', 'instanceof']
           ],
           'allowSamePrecedence': false
         }
       ],
-      '@stylistic/js/key-spacing': ['error', {
-        mode: 'strict'
-      }],
+      '@stylistic/js/key-spacing': [
+        'error', { mode: 'strict' }
+      ],
       '@stylistic/js/no-trailing-spaces': 'error',
       '@stylistic/js/no-multi-spaces': 'error',
       '@stylistic/js/no-confusing-arrow': 'error'
