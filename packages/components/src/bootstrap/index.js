bd784e67dadf85c884db86d6f091ca136476743c
Thomas David <tom@mightysmile.com>
2024-06-06 13:48:32 +0200

    feature (components): Modal section edit item

--- packages/components/src/bootstrap/index.js
+++ packages/components/src/bootstrap/index.js
@@ -2,9 +2,11 @@ import editSectionInner from './edit-section-inner.js'
 import editSectionOuter from './edit-section-outer.js'
 import addComponentButton from './add-component-button.js'
 import modalSectionEdit from './modal-section-edit.js'
+import modalSectionEditItem from './modal-section-edit-item.js'
 
 export {
   modalSectionEdit,
+  modalSectionEditItem,
   addComponentButton,
   editSectionInner,
   editSectionOuter
