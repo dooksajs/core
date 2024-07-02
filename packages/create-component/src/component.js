17a47dc5b0c81c8726ea14f6c2c72e4a57f92019
Thomas David <tom@mightysmile.com>
2024-07-02 09:00:58 +0200

    fix (create-component): Move JSDocs into index

---
+++ packages/create-component/src/component.js
@@ -0,0 +1,228 @@
+/**
+ * @typedef {import('./index.js').ComponentData} ComponentData
+ * @typedef {import('./index.js').ComponentDataValues} ComponentDataValues
+ * @typedef {import('./index.js').ComponentMixinMetadata} ComponentMixinMetadata
+ * @typedef {import('./index.js').Component} Component
+ * @typedef {import('./index.js').ComponentOption} ComponentOption
+ * @typedef {import('./index.js').ComponentProperty} ComponentProperty
+ * @typedef {import('./index.js').ComponentExtend} ComponentExtend
+ */
+
+/**
+* @param {ComponentData & ComponentDataValues} data
+* @param {Object[]} [mixins]
+* @param {ComponentMixinMetadata} mixins[].metadata
+* @param {ComponentDataValues} mixins[].data
+* @returns {Component}
+*/
+function createComponent (data, mixins = []) {
+  /**
+  * @type {Component}
+  */
+  const result = {}
+
+  if (mixins.length) {
+    result.mixins = []
+
+    for (let i = 0; i < mixins.length; i++) {
+      const { metadata, data } = mixins[i]
+
+      result.mixins.push(metadata)
+
+      // merge mixin
+      if (data.options) {
+        const options = result.options || {}
+
+        mergeProperties(options, data.options)
+
+        result.options = options
+      }
+
+      if (data.styles) {
+        const styles = result.styles || []
+
+        result.styles = styles.concat(data.styles)
+      }
+
+      if (data.events) {
+        const events = result.events || []
+
+        result.events = events.concat(data.events)
+      }
+
+      if (data.eventTypes) {
+        const eventTypes = result.eventTypes || {}
+
+        result.eventTypes = Object.assign(eventTypes, data.eventTypes)
+      }
+    }
+  }
+
+  // merge component
+  mergeProperties(result, data)
+
+  result.isLoaded = !(typeof data.component === 'function')
+  result.type = result.type || 'element'
+
+  return result
+}
+
+function mergeProperties (target, source) {
+  for (const key in source) {
+    if (Object.hasOwnProperty.call(source, key)) {
+      const t = target[key]
+      const s = source[key]
+
+      if (t) {
+        if (Array.isArray(t)) {
+          target[key] = t.concat(s)
+        } else {
+          target[key] = Object.assign(t, s)
+        }
+      } else {
+        target[key] = s
+      }
+    }
+  }
+
+  return target
+}
+
+/**
+* Convert component options to properties
+* @param {ComponentOption} options
+* @param {ComponentOption} templateOptions
+* @param {ComponentProperty[]} properties
+* @returns {ComponentProperty[]}
+*/
+function componentOptions (options, templateOptions, properties = []) {
+  properties = properties.slice()
+  let replacedPropertyValue = ''
+
+  for (const key in options) {
+    if (Object.hasOwnProperty.call(options, key)) {
+      const templateOption = templateOptions[key]
+      const propertyName = templateOption.name
+
+      if (replacedPropertyValue === propertyName) {
+        continue
+      }
+
+      const item = options[key]
+      let newPropertyValue = item
+
+      if (!templateOption) {
+        throw new Error('Component option does not exist "' + item +'"')
+      }
+
+      if (templateOption.value) {
+        const newValue = templateOption.value
+
+        if (newValue == null) {
+          throw new Error('Component modifier value has an unexpected value "' + item + '"')
+        }
+
+        newPropertyValue = newValue
+      } else if (templateOption.values) {
+        const newValue = templateOption.values[item]
+
+        if (newValue == null) {
+          throw new Error('Component modifier value has an unexpected value "' + item + '"')
+        }
+
+        newPropertyValue = newValue
+      } else if (templateOption.computedValue) {
+        const computeValue = templateOption.computedValue
+
+        if (typeof computeValue !== 'function') {
+          throw new Error('Component modifier value has an unexpected value "' + item + '"')
+        }
+
+        newPropertyValue = computeValue(item)
+      }
+
+      /** @type {ComponentProperty} */
+      const newProperty = {
+        name: propertyName,
+        value: newPropertyValue
+      }
+
+      let hasProperty = false
+
+      for (let i = 0; i < properties.length; i++) {
+        const property = properties[i]
+
+        if (property.name === propertyName) {
+          hasProperty = true
+
+          if (property) {
+            if (templateOption.replace) {
+              replacedPropertyValue = propertyName
+              newProperty.value = newPropertyValue
+              properties[i] = newProperty
+              break
+            } else if (templateOption.toggle) {
+              // remove value
+              if (!item) {
+                newProperty.value = property.value.replace(newProperty.value, '')
+                properties[i] = newProperty
+                break
+              }
+            }
+          }
+
+          newProperty.value = property.value + ' ' + newProperty.value
+
+          properties[i] = newProperty
+        }
+      }
+
+      if (!hasProperty) {
+        properties.push(newProperty)
+      }
+    }
+  }
+
+  return properties
+}
+
+/**
+* Create a modified component
+* @param {Component} component
+* @param {ComponentExtend} extend
+*/
+function extendComponent (component, { metadata, options, children, events }) {
+  const result = Object.assign({}, component)
+  let properties = component.properties
+
+  if (metadata) {
+    result.id = metadata.id
+    result.parentId = component.id
+  }
+
+  if (options) {
+    properties = componentOptions(options, component.options, component.properties)
+  }
+
+  if (properties) {
+    result.properties = properties
+  }
+
+  if (events) {
+    result.events = events
+  }
+
+  const componentChildren = children || component.children
+
+  if (componentChildren) {
+    result.children = componentChildren
+  }
+
+  return result
+}
+
+export {
+  componentOptions,
+  extendComponent,
+  createComponent
+}
--- packages/create-component/src/index.js
+++ packages/create-component/src/index.js
@@ -1,9 +1,12 @@
-/** @typedef {'sm'|'md'|'lg'|'always'} BreakpointAlwaysLg */
-/** @typedef {'sm'|'md'|'lg'|'xl'|'xxl'} BreakpointSmXXL */
-/** @typedef {'sm'|'md'|'lg'|'xl'|'xxl'|'always'} BreakpointAlwaysXXL */
-/** @typedef {'10'|'25'|'50'|'75'} Opacity */
-/** @typedef {'0'|'1'|'2'|'3'|'4'|'5'} Spacer */
+import { componentOptions, createComponent, extendComponent } from './component.js'
+import createMixin from './mixin.js'
+
 /**
+ * @typedef {'sm'|'md'|'lg'|'always'} BreakpointAlwaysLg
+ * @typedef {'sm'|'md'|'lg'|'xl'|'xxl'} BreakpointSmXXL
+ * @typedef {'sm'|'md'|'lg'|'xl'|'xxl'|'always'} BreakpointAlwaysXXL
+ * @typedef {'10'|'25'|'50'|'75'} Opacity
+ * @typedef {'0'|'1'|'2'|'3'|'4'|'5'} Spacer
  * @typedef {'primary'|
  * 'primarySubtle'|
  * 'secondary'|
@@ -25,9 +28,6 @@
  * 'bodyTertiary'|
  * 'black'|
  * 'white'} ColorExtra
- */
-
-/**
  * @typedef {'primary'|
  * 'secondary'|
  * 'success'|
@@ -131,231 +131,9 @@
  * @property {string} [tag] - Element tag name
  */
 
-/**
- * @param {ComponentData & ComponentDataValues} data
- * @param {Object[]} [mixins]
- * @param {ComponentMixinMetadata} mixins[].metadata
- * @param {ComponentDataValues} mixins[].data
- * @returns {Component}
- */
-function createComponent (data, mixins = []) {
-  /**
-   * @type {Component}
-   */
-  const result = {}
-
-  if (mixins.length) {
-    result.mixins = []
-
-    for (let i = 0; i < mixins.length; i++) {
-      const { metadata, data } = mixins[i]
-
-      result.mixins.push(metadata)
-
-      // merge mixin
-      if (data.options) {
-        const options = result.options || {}
-
-        mergeProperties(options, data.options)
-
-        result.options = options
-      }
-
-      if (data.styles) {
-        const styles = result.styles || []
-
-        result.styles = styles.concat(data.styles)
-      }
-
-      if (data.events) {
-        const events = result.events || []
-
-        result.events = events.concat(data.events)
-      }
-
-      if (data.eventTypes) {
-        const eventTypes = result.eventTypes || {}
-
-        result.eventTypes = Object.assign(eventTypes, data.eventTypes)
-      }
-    }
-  }
-
-  // merge component
-  mergeProperties(result, data)
-
-  result.isLoaded = !(typeof data.component === 'function')
-  result.type = result.type || 'element'
-
-  return result
-}
-
-function mergeProperties (target, source) {
-  for (const key in source) {
-    if (Object.hasOwnProperty.call(source, key)) {
-      const t = target[key]
-      const s = source[key]
-
-      if (t) {
-        if (Array.isArray(t)) {
-          target[key] = t.concat(s)
-        } else {
-          target[key] = Object.assign(t, s)
-        }
-      } else {
-        target[key] = s
-      }
-    }
-  }
-
-  return target
-}
-
-/**
- * @param {Object} mixin
- * @param {ComponentMixinMetadata} mixin.metadata
- * @param {ComponentDataValues} mixin.data
- */
-function createMixin (mixin) {
-  return mixin
-}
-
-/**
- * Convert component options to properties
- * @param {ComponentOption} options
- * @param {ComponentOption} templateOptions
- * @param {ComponentProperty[]} properties
- * @returns {ComponentProperty[]}
- */
-function componentOptions (options, templateOptions, properties = []) {
-  properties = properties.slice()
-  let replacedPropertyValue = ''
-
-  for (const key in options) {
-    if (Object.hasOwnProperty.call(options, key)) {
-      const templateOption = templateOptions[key]
-      const propertyName = templateOption.name
-
-      if (replacedPropertyValue === propertyName) {
-        continue
-      }
-
-      const item = options[key]
-      let newPropertyValue = item
-
-      if (!templateOption) {
-        throw new Error('Component option does not exist "' + item +'"')
-      }
-
-      if (templateOption.value) {
-        const newValue = templateOption.value
-
-        if (newValue == null) {
-          throw new Error('Component modifier value has an unexpected value "' + item + '"')
-        }
-
-        newPropertyValue = newValue
-      } else if (templateOption.values) {
-        const newValue = templateOption.values[item]
-
-        if (newValue == null) {
-          throw new Error('Component modifier value has an unexpected value "' + item + '"')
-        }
-
-        newPropertyValue = newValue
-      } else if (templateOption.computedValue) {
-        const computeValue = templateOption.computedValue
-
-        if (typeof computeValue !== 'function') {
-          throw new Error('Component modifier value has an unexpected value "' + item + '"')
-        }
-
-        newPropertyValue = computeValue(item)
-      }
-
-      /** @type {ComponentProperty} */
-      const newProperty = {
-        name: propertyName,
-        value: newPropertyValue
-      }
-
-      let hasProperty = false
-
-      for (let i = 0; i < properties.length; i++) {
-        const property = properties[i]
-
-        if (property.name === propertyName) {
-          hasProperty = true
-
-          if (property) {
-            if (templateOption.replace) {
-              replacedPropertyValue = propertyName
-              newProperty.value = newPropertyValue
-              properties[i] = newProperty
-              break
-            } else if (templateOption.toggle) {
-              // remove value
-              if (!item) {
-                newProperty.value = property.value.replace(newProperty.value, '')
-                properties[i] = newProperty
-                break
-              }
-            }
-          }
-
-          newProperty.value = property.value + ' ' + newProperty.value
-
-          properties[i] = newProperty
-        }
-      }
-
-      if (!hasProperty) {
-        properties.push(newProperty)
-      }
-    }
-  }
-
-  return properties
-}
-
-/**
- * Create a modified component
- * @param {Component} component
- * @param {ComponentExtend} extend
- */
-function extendComponent (component, { metadata, options, children, events }) {
-  const result = Object.assign({}, component)
-  let properties = component.properties
-
-  if (metadata) {
-    result.id = metadata.id
-    result.parentId = component.id
-  }
-
-  if (options) {
-    properties = componentOptions(options, component.options, component.properties)
-  }
-
-  if (properties) {
-    result.properties = properties
-  }
-
-  if (events) {
-    result.events = events
-  }
-
-  const componentChildren = children || component.children
-
-  if (componentChildren) {
-    result.children = componentChildren
-  }
-
-  return result
-}
-
 export {
   componentOptions,
-  extendComponent,
   createComponent,
-  createMixin
+  createMixin,
+  extendComponent
 }
