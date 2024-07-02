16a5e47007356cc564d6cd9610a153dcff88f9b2
Thomas David <tom@mightysmile.com>
2024-07-02 08:50:05 +0200

    fix (app-client): Imports

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,13 +1,79 @@
-export * from './button/index.js'
-export * from './button-group/index.js'
-export * from './icon/index.js'
-export * from './modal/index.js'
-export * from './text/index.js'
-export * from './container/index.js'
-export * from './card/index.js'
+import { anchor, div, h1, h2, h5, hr, img, span } from './base/index.js'
+import { buttonGroup } from './button-group/button-group.js'
+import { button } from './button/button.js'
+import { card, cardBody, cardHeader, cardImg, cardText, cardTitle } from './card/index.js'
+import { container } from './container/container.js'
+import { formCheck, inputCheckbox, inputCheckboxButton, inputColor, inputFile, inputText, label } from './form/index.js'
+import { icon } from './icon/icon.js'
+import { listGroup, listGroupAction, listGroupItem, listGroupItemAction, listGroupItemActionLink } from './list-group/index.js'
+import { modal, modalBody, modalContent, modalDialog, modalFooter, modalHeader, modalTitle } from './modal/index.js'
+import { nav, navItem, navLinkAnchor, navLinkButton, tabContent, tabPane } from './nav/index.js'
+import { text } from './text/text.js'
+import { accordion, accordionBody, accordionButton, accordionCollapse, accordionHeader, accordionItem } from './accordion/index.js'
+
+export * from './accordion/index.js'
 export * from './base/index.js'
 export * from './bootstrap/index.js'
+export * from './button-group/index.js'
+export * from './button/index.js'
+export * from './card/index.js'
+export * from './container/index.js'
 export * from './form/index.js'
-export * from './accordion/index.js'
-export * from './nav/index.js'
+export * from './icon/index.js'
 export * from './list-group/index.js'
+export * from './modal/index.js'
+export * from './nav/index.js'
+export * from './text/index.js'
+
+export default [
+  accordion,
+  accordionBody,
+  accordionButton,
+  accordionCollapse,
+  accordionHeader,
+  accordionItem,
+  button,
+  buttonGroup,
+  card,
+  cardBody,
+  cardHeader,
+  cardImg,
+  cardText,
+  cardTitle,
+  icon,
+  modal,
+  modalBody,
+  modalContent,
+  modalDialog,
+  modalFooter,
+  modalHeader,
+  modalTitle,
+  text,
+  container,
+  h1,
+  h2,
+  h5,
+  hr,
+  div,
+  anchor,
+  img,
+  span,
+  formCheck,
+  inputCheckbox,
+  inputCheckboxButton,
+  inputColor,
+  inputFile,
+  inputText,
+  label,
+  listGroup,
+  listGroupAction,
+  listGroupItem,
+  listGroupItemAction,
+  listGroupItemActionLink,
+  nav,
+  navItem,
+  navLinkAnchor,
+  navLinkButton,
+  tabContent,
+  tabPane
+]
