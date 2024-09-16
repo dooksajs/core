44410f68d9a3282ea8e854300aee22d388d4a923
Thomas David <tom@mightysmile.com>
2024-09-16 15:23:32 +0200

    feature (app-client): Use new component packages

--- packages/app-client/src/index.js
+++ packages/app-client/src/index.js
@@ -1,7 +1,10 @@
-import components from '@dooksa/components'
+import componentBase from '@dooksa/component-base'
+import componentExtra from '@dooksa/component-extra'
+import componentBootstrap from '@dooksa/component-bootstrap'
 import { createAppClient } from '@dooksa/create-app'
 import plugins from '@dooksa/plugins'
 
+const components = [].concat(componentBase, componentExtra, componentBootstrap)
 export default createAppClient({
   plugins,
   components
