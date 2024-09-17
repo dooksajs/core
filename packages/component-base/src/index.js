7508ab8578d9334684bf4d4059190b2575f28776
Thomas David <tom@mightysmile.com>
2024-09-17 18:18:00 +0200

    fix (component-base): Exports

--- packages/component-base/src/index.js
+++ packages/component-base/src/index.js
@@ -1,26 +1,27 @@
+import { extendH1, h1 } from './content/h1.js'
+import { extendH2, h2 } from './content/h2.js'
+import { extendH3, h3 } from './content/h3.js'
+import { extendH4, h4 } from './content/h4.js'
+import { extendH5, h5 } from './content/h5.js'
+import { extendH6, h6 } from './content/h6.js'
+import { extendImg, img } from './content/img.js'
+import { extendParagraph, paragraph } from './content/paragraph.js'
+import { extendText, text } from './content/text.js'
+import { extendFormCheck, formCheck } from './form/form-check.js'
+import { extendForm, form } from './form/form.js'
+import { extendInputCheckboxButton, inputCheckboxButton } from './form/input-checkbox-button.js'
+import { extendInputCheckbox, inputCheckbox } from './form/input-checkbox.js'
+import { extendInputColor, inputColor } from './form/input-color.js'
+import { extendInputFile, inputFile } from './form/input-file.js'
+import { extendInputText, inputText } from './form/input-text.js'
+import { extendLabel, label } from './form/label.js'
+import { extendOption, option } from './form/select-option.js'
+import { extendSelect, select } from './form/select.js'
 import { div, extendDiv } from './generic/div.js'
 import { extendHr, hr } from './generic/hr.js'
 import { extendSpan, span } from './generic/span.js'
 import { anchor, extendAnchor } from './interactive/anchor.js'
 import { button, extendButton } from './interactive/button.js'
-import { h1, extendH1 } from './content/h1.js'
-import { h2, extendH2 } from './content/h2.js'
-import { h3, extendH3 } from './content/h3.js'
-import { h4, extendH4 } from './content/h4.js'
-import { h5, extendH5 } from './content/h5.js'
-import { h6, extendH6 } from './content/h6.js'
-import { img, extendImg } from './content/img.js'
-import { text, extendText } from './content/text.js'
-import { formCheck, extendFormCheck } from './form/form-check.js'
-import { inputCheckbox, extendInputCheckbox } from './form/input-checkbox.js'
-import { inputCheckboxButton, extendInputCheckboxButton } from './form/input-checkbox-button.js'
-import { inputColor, extendInputColor } from './form/input-color.js'
-import { inputFile, extendInputFile } from './form/input-file.js'
-import { inputText, extendInputText } from './form/input-text.js'
-import { label, extendLabel } from './form/label.js'
-import { form, extendForm } from './form/form.js'
-import { select, extendSelect } from './form/select.js'
-import { selectOption, extendSelectOption } from './form/select-option.js'
 
 export {
   extendAnchor,
@@ -42,33 +43,33 @@ export {
   extendInputFile,
   extendInputText,
   extendLabel,
-  extendSelect,
-  extendSelectOption,
-  extendText,
-  extendSpan
+  extendParagraph, extendSelect,
+  extendOption, extendSpan, extendText
 }
 
 export default [
-  div,
-  hr,
   anchor,
-  span,
+  button,
+  div,
+  form,
+  formCheck,
   h1,
   h2,
   h3,
   h4,
   h5,
   h6,
+  hr,
   img,
-  button,
-  text,
-  formCheck,
   inputCheckbox,
   inputCheckboxButton,
   inputColor,
   inputFile,
   inputText,
   label,
-  form,
-  select
+  paragraph,
+  select,
+  option,
+  span,
+  text
 ]
