d1d7b10b1f027936d3b5776220cc98011169e1ee
Thomas David <tom@mightysmile.com>
2024-11-05 12:29:55 +0100

    feature (component-mixins): Common shared data

---
+++ packages/component-mixins/src/styles/utils.js
@@ -0,0 +1,18 @@
+export const directions = {
+  end: 'e',
+  start: 's',
+  top: 't',
+  bottom: 'b',
+  xAxis: 'x',
+  yAxis: 'y',
+  none: ''
+}
+
+export const breakpoints = {
+  sm: 'sm',
+  md: 'md',
+  lg: 'lg',
+  xl: 'xl',
+  xxl: 'xxl',
+  always: ''
+}
