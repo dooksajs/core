4c237b5da9a8c655a38e423e2b6b604854fc71f9
Thomas David <tom@mightysmile.com>
2024-11-12 07:41:47 +0100

    feature (component-base): Add  types for new components

--- packages/component-base/src/types.js
+++ packages/component-base/src/types.js
@@ -1,59 +1,59 @@
 /**
- * @import {ExtendDetails} from './interactive/details.js'
- * @import {ExtendSummary} from './interactive/summary.js'
- * @import {ExtendButton} from './interactive/button.js'
  * @import {ExtendAnchor} from './interactive/anchor.js'
+ * @import {ExtendButton} from './interactive/button.js'
+ * @import {ExtendDetails} from './interactive/details.js'
+ * @import {ExtendDiv} from './generic/div.js'
+ * @import {ExtendForm} from './form/form.js'
+ * @import {ExtendFormCheck} from './form/form-check.js'
  * @import {ExtendH1} from './content/h1.js'
  * @import {ExtendH2} from './content/h2.js'
  * @import {ExtendH3} from './content/h3.js'
  * @import {ExtendH4} from './content/h4.js'
  * @import {ExtendH5} from './content/h5.js'
  * @import {ExtendH6} from './content/h6.js'
- * @import {ExtendSmall} from './content/small.js'
+ * @import {ExtendHr} from './generic/hr.js'
  * @import {ExtendImg} from './content/img.js'
- * @import {ExtendParagraph} from './content/paragraph.js'
- * @import {ExtendText} from './content/text.js'
- * @import {ExtendFormCheck} from './form/form-check.js'
- * @import {ExtendForm} from './form/form.js'
- * @import {ExtendInputCheckboxButton} from './form/input-checkbox-button.js'
  * @import {ExtendInputCheckbox} from './form/input-checkbox.js'
+ * @import {ExtendInputCheckboxButton} from './form/input-checkbox-button.js'
  * @import {ExtendInputColor} from './form/input-color.js'
  * @import {ExtendInputFile} from './form/input-file.js'
  * @import {ExtendInputText} from './form/input-text.js'
  * @import {ExtendLabel} from './form/label.js'
  * @import {ExtendOption} from './form/option.js'
+ * @import {ExtendParagraph} from './content/paragraph.js'
  * @import {ExtendSelect} from './form/select.js'
- * @import {ExtendDiv} from './generic/div.js'
- * @import {ExtendHr} from './generic/hr.js'
+ * @import {ExtendSmall} from './content/small.js'
  * @import {ExtendSpan} from './generic/span.js'
+ * @import {ExtendSummary} from './interactive/summary.js'
+ * @import {ExtendText} from './content/text.js'
  */
 
 /**
- * @typedef {ExtendSmall} ComponentExtendSmall
- * @typedef {ExtendButton} ComponentExtendButton
  * @typedef {ExtendAnchor} ComponentExtendAnchor
+ * @typedef {ExtendButton} ComponentExtendButton
  * @typedef {ExtendDetails} ComponentExtendDetails
- * @typedef {ExtendSummary} ComponentExtendSummary
+ * @typedef {ExtendDiv} ComponentExtendDiv
+ * @typedef {ExtendForm} ComponentExtendForm
+ * @typedef {ExtendFormCheck} ComponentExtendFormCheck
  * @typedef {ExtendH1} ComponentExtendH1
  * @typedef {ExtendH2} ComponentExtendH2
  * @typedef {ExtendH3} ComponentExtendH3
  * @typedef {ExtendH4} ComponentExtendH4
  * @typedef {ExtendH5} ComponentExtendH5
  * @typedef {ExtendH6} ComponentExtendH6
+ * @typedef {ExtendHr} ComponentExtendHr
  * @typedef {ExtendImg} ComponentExtendImg
- * @typedef {ExtendParagraph} ComponentExtendParagraph
- * @typedef {ExtendText} ComponentExtendText
- * @typedef {ExtendFormCheck} ComponentExtendFormCheck
- * @typedef {ExtendForm} ComponentExtendForm
- * @typedef {ExtendInputCheckboxButton} ComponentExtendInputCheckboxButton
  * @typedef {ExtendInputCheckbox} ComponentExtendInputCheckbox
+ * @typedef {ExtendInputCheckboxButton} ComponentExtendInputCheckboxButton
  * @typedef {ExtendInputColor} ComponentExtendInputColor
  * @typedef {ExtendInputFile} ComponentExtendInputFile
  * @typedef {ExtendInputText} ComponentExtendInputText
  * @typedef {ExtendLabel} ComponentExtendLabel
  * @typedef {ExtendOption} ComponentExtendOption
+ * @typedef {ExtendParagraph} ComponentExtendParagraph
  * @typedef {ExtendSelect} ComponentExtendSelect
- * @typedef {ExtendDiv} ComponentExtendDiv
- * @typedef {ExtendHr} ComponentExtendHr
+ * @typedef {ExtendSmall} ComponentExtendSmall
  * @typedef {ExtendSpan} ComponentExtendSpan
+ * @typedef {ExtendSummary} ComponentExtendSummary
+ * @typedef {ExtendText} ComponentExtendText
  */
