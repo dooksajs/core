2a9f05e75a16331bc6eff12501373b3a2aa64511
Thomas David <tom@mightysmile.com>
2024-11-14 12:24:20 +0100

    fix (actions) Simplify export import

--- packages/actions/src/index.js
+++ packages/actions/src/index.js
@@ -1,18 +1,134 @@
-import onCreateChangeText from './on-create-change-text.js'
-import onCreateChangeImage from './on-create-change-image.js'
-import editImg from './edit-img.js'
-import onCreateLabel from './on-create-label.js'
-import onCreateLabelText from './on-create-label-text.js'
-import onCreateSetOptionId from './on-create-set-option-id.js'
-export * from './select-edit/index.js'
-export * from './accordion/index.js'
-export * from './icon/index.js'
+// import {} from './select-edit/index.js'
+import {
+  accordionGenerateName,
+  accordionSetDetailsName
+} from './accordion/index.js'
+import {
+  inputId,
+  labelHtmlFor,
+  optionText,
+  optionValue
+} from './form/index.js'
+import {
+  variableContextId,
+  variableContextRootId,
+  variableContextParentId
+} from './variable/index.js'
+import {
+  actionInputAddObjectProperty,
+  actionInputArrayItem,
+  actionInputArrayItems,
+  actionInputData,
+  actionInputDataActionList,
+  actionInputDataActionOption,
+  actionInputDataActionOptions,
+  actionInputDataType,
+  actionInputNamePrefix,
+  actionInputNamePrefixAction,
+  actionInputNamePrefixActionValue,
+  actionInputObjectProperties,
+  actionInputObjectProperty,
+  actionInputObjectPropertyData,
+  actionInputObjectPropertyName,
+  actionInputObjectPropertySchema,
+  actionInputObjectPropertyValue,
+  actionInputUpdateNamePrefixActionValue,
+  actionInputUpdateObjectPropertyName,
+  actionInputUpdateObjectPropertyTitle,
+  actionInputValue,
+  actionInputValueActionMethod
+} from './action-input/index.js'
+import {
+  actionCardBody,
+  actionCardIcon,
+  actionCardDescription,
+  actionCardTitle,
+  actionCardPluginIcon,
+  actionCardBodyLabelIcon,
+  actionCardBodyLabelText
+} from './action-card/index.js'
+import {
+  actionAddBlockSetContext,
+  actionAddBlockCollapse,
+  actionAddBlockInsertAction,
+  actionAddBlockBody,
+  actionAddBlockFetchActions,
+  actionAddBlockListItem,
+  actionAddBlockListItemDescription,
+  actionAddBlockListItemIcon,
+  actionAddBlockListItemTitle
+} from './action-add-block/index.js'
+import {
+  dataCollectionLabel,
+  dataCollectionOption,
+  dataCollectionOptionItem,
+  dataDocumentOption,
+  dataDocumentOptionItem,
+  dataDocumentSelectMethodLabel,
+  dataDocumentSetSelect,
+  dataDocumentValue,
+  dataSetValueByIdUpdateFieldset
+} from './data/index.js'
+import {
+  iconRender
+} from './icon/index.js'
 
-export {
-  editImg,
-  onCreateChangeText,
-  onCreateChangeImage,
-  onCreateLabel,
-  onCreateLabelText,
-  onCreateSetOptionId
-}
+export default [
+  accordionGenerateName,
+  accordionSetDetailsName,
+  inputId,
+  labelHtmlFor,
+  optionText,
+  optionValue,
+  variableContextId,
+  variableContextRootId,
+  variableContextParentId,
+  actionInputAddObjectProperty,
+  actionInputData,
+  actionInputArrayItem,
+  actionInputArrayItems,
+  actionInputObjectPropertyName,
+  actionInputDataActionList,
+  actionInputDataActionOption,
+  actionInputDataActionOptions,
+  actionInputDataType,
+  actionInputNamePrefix,
+  actionInputNamePrefixAction,
+  actionInputNamePrefixActionValue,
+  actionInputObjectProperties,
+  actionInputObjectPropertyValue,
+  actionInputObjectPropertyData,
+  actionInputObjectPropertySchema,
+  actionInputObjectProperty,
+  actionInputValue,
+  actionInputValueActionMethod,
+  actionInputUpdateNamePrefixActionValue,
+  actionInputUpdateObjectPropertyName,
+  actionInputUpdateObjectPropertyTitle,
+  actionCardBody,
+  actionCardIcon,
+  actionCardDescription,
+  actionCardTitle,
+  actionCardPluginIcon,
+  actionCardBodyLabelIcon,
+  actionCardBodyLabelText,
+  actionAddBlockSetContext,
+  actionAddBlockCollapse,
+  actionAddBlockInsertAction,
+  actionAddBlockBody,
+  actionAddBlockFetchActions,
+  actionAddBlockListItem,
+  actionAddBlockListItemDescription,
+  actionAddBlockListItemIcon,
+  actionAddBlockListItemTitle,
+  dataCollectionLabel,
+  dataCollectionOption,
+  dataCollectionOptionItem,
+  dataDocumentOption,
+  dataDocumentOptionItem,
+  dataDocumentSelectMethodLabel,
+  dataDocumentSetSelect,
+  dataDocumentValue,
+  dataSetValueByIdUpdateFieldset,
+  iconRender
+]
--- packages/app-server/src/index.js
+++ packages/app-server/src/index.js
@@ -1,6 +1,6 @@
 import { createAppServer } from '@dooksa/create-app'
 import clientPlugins, { data } from '@dooksa/plugins'
-import { editImg, selectEditInner, selectEditInnerIterate, selectEditOuter, selectEditOuterIterate, onClickAccordionButton, onCollapseHideAccordionButton, onCollapseShowAccordionButton, onCreateAccordion, onCreateAccordionButton, onCreateAccordionCollapse, onCreateChangeImage, onCreateChangeText, labelHtmlFor, onCreateLabelText, onCreateSetOptionId, selectEditAddComponent, selectEditModal, selectEditModalItems, actionEditorAddButton, actionEditorAddButtonCondition, actionEditorAddButtonIterate, inputId, setActionInputId, actionEditorBlockSelectorToggle, actionEditorBlockSelectorAdd, actionEditorBlockSelectorRemove, inputCheckboxContentId, actionEditorAddButtonOff } from '@dooksa/actions'
+import actions from '@dooksa/actions'
 import {
   $http,
   action,
@@ -11,8 +11,7 @@ import {
   middleware,
   page,
   theme,
-  user,
-  content
+  user
 } from '@dooksa/plugins-server'
 
 export default createAppServer({
@@ -23,7 +22,6 @@ export default createAppServer({
     metadata,
     user,
     database,
-    content,
     action,
     component,
     event,
@@ -31,35 +29,5 @@ export default createAppServer({
     theme
   ],
   clientPlugins,
-  actions: [
-    actionEditorAddButton,
-    actionEditorAddButtonCondition,
-    actionEditorAddButtonIterate,
-    actionEditorAddButtonOff,
-    editImg,
-    onCreateChangeImage,
-    onCreateChangeText,
-    selectEditAddComponent,
-    selectEditOuter,
-    selectEditOuterIterate,
-    selectEditInner,
-    selectEditInnerIterate,
-    selectEditModal,
-    selectEditModalItems,
-    labelHtmlFor,
-    inputId,
-    setActionInputId,
-    onCreateLabelText,
-    onCreateAccordionButton,
-    onCreateAccordionCollapse,
-    onCreateSetOptionId,
-    onClickAccordionButton,
-    onCreateAccordion,
-    onCollapseHideAccordionButton,
-    onCollapseShowAccordionButton,
-    actionEditorBlockSelectorToggle,
-    actionEditorBlockSelectorAdd,
-    actionEditorBlockSelectorRemove,
-    inputCheckboxContentId
-  ]
+  actions
 })
