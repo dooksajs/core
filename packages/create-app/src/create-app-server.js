8609edeca3a3962ec4c27a1f5e506edb5459a4d6
Thomas David <tom@mightysmile.com>
2024-11-24 14:02:47 +0100

    feat(create-app-server): handle defaults

--- packages/create-app/src/create-app-server.js
+++ packages/create-app/src/create-app-server.js
@@ -1,23 +1,46 @@
 import appendPlugin from './append-plugin.js'
-import { data } from '@dooksa/plugins/client'
-import {
-  $http,
-  middleware,
-  action,
-  metadata,
-  database,
-  component,
-  event,
-  page,
-  theme,
-  user
-} from '@dooksa/plugins/server'
+import defaultClientPlugins, { data } from '@dooksa/plugins/client'
+import defaultServerPlugins, { httpStart } from '@dooksa/plugins/server'
+import defaultActions from '@dooksa/actions'
 
-/** @import {Plugin, ActiveAction} from '../../create-plugin/src/index.js' */
+/**
+ * @import {Plugin} from '../../create-plugin/src/index.js'
+ * @import {Action} from '@dooksa/create-action/types'
+ */
 
+/**
+ * @param {Action[]} appActionData
+ */
 function appendAction (appActionData) {
+  /**
+   * @param {Action} action
+   */
   return (action) => {
     appActionData.push(action)
+  }
+}
+
+/**
+ * Append client plugins
+ * @param {Object} appMetadata
+ * @param {Array} appMetadata.actions
+ * @param {Array} appMetadata.plugins
+ */
+function appendClientPlugin (appMetadata) {
+  /**
+   * @param {Plugin} plugin
+   */
+  return function (plugin) {
+    if (plugin.metadata) {
+      appMetadata.plugins.push({
+        name: plugin.name,
+        metadata: plugin.metadata
+      })
+    }
+
+    if (plugin.actions) {
+      appMetadata.actions = appMetadata.actions.concat(plugin.actions)
+    }
   }
 }
 
@@ -43,21 +66,58 @@ function initialize (appSetup, appActionData, appDataModels, appMetadata) {
 
     appSetup = []
 
-    return $http.httpStart(options.http)
+    return httpStart(options.http)
   }
 }
 
 /**
+ * @typedef {Object} ServerPlugins
+ * @property {Plugin} [data]
+ * @property {Plugin} [middleware]
+ * @property {Plugin} [http]
+ * @property {Plugin} [metadata]
+ * @property {Plugin} [user]
+ * @property {Plugin} [database]
+ * @property {Plugin} [action]
+ * @property {Plugin} [component]
+ * @property {Plugin} [event]
+ * @property {Plugin} [page]
+ * @property {Plugin} [theme]
+ */
+
+/**
+ * @typedef {Object} ClientPlugins
+ * @property {Plugin} [data]
+ * @property {Plugin} [metadata]
+ * @property {Plugin} [fetch]
+ * @property {Plugin} [operator]
+ * @property {Plugin} [action]
+ * @property {Plugin} [variable]
+ * @property {Plugin} [component]
+ * @property {Plugin} [regex]
+ * @property {Plugin} [editor]
+ * @property {Plugin} [list]
+ * @property {Plugin} [event]
+ * @property {Plugin} [token]
+ * @property {Plugin} [icon]
+ * @property {Plugin} [query]
+ * @property {Plugin} [route]
+ * @property {Plugin} [form]
+ * @property {Plugin} [string]
+ * @property {Plugin} [page]
+ */
+
+/**
  * Create Dooksa app
  * @param {Object} data
- * @param {Plugin[]} [data.serverPlugins=[]]
- * @param {Plugin[]} [data.clientPlugins=[]]
- * @param {ActiveAction[]} [data.actions=[]]
+ * @param {ServerPlugins} [data.serverPlugins={}] - Overwrite core server plugins
+ * @param {ClientPlugins} [data.clientPlugins={}] - Overwrite core client plugins
+ * @param {Object.<string, Action>} [data.actions={}] - Overwrite core client plugins
  */
 export default function createAppServer ({
-  serverPlugins = [],
-  clientPlugins = [],
-  actions = []
+  clientPlugins = {},
+  serverPlugins = {},
+  actions = {}
 } = {}) {
   const appPlugins = []
   const appSetup = []
@@ -73,50 +133,50 @@ export default function createAppServer ({
   }
   const usePlugin = appendPlugin(appPlugins, appSetup, appDataModels)
   const useAction = appendAction(appActionData)
+  const useClientPlugin = appendClientPlugin(appMetadata)
 
-  // add required server-side plugins
+  // use data
   usePlugin(data)
-  usePlugin(middleware)
-  usePlugin($http)
-  usePlugin(metadata)
-  usePlugin(user)
-  usePlugin(database)
-  usePlugin(action)
-  usePlugin(component)
-  usePlugin(event)
-  usePlugin(page)
-  usePlugin(theme)
 
-  // add additional server-side plugins
-  for (let i = 0; i < serverPlugins.length; i++) {
-    const plugin = serverPlugins[i]
+  // use required server-side plugins
+  for (let i = 0; i < defaultServerPlugins.length; i++) {
+    const plugin = defaultServerPlugins[i]
+    const name = plugin.name
 
-    usePlugin(plugin)
-  }
-
-  // add actions
-  for (let i = 0; i < actions.length; i++) {
-    useAction(actions[i])
-  }
-
-  // plugins used on the client-side
-  for (let i = 0; i < clientPlugins.length; i++) {
-    const plugin = clientPlugins[i]
-
-    if (plugin.metadata) {
-      appMetadata.plugins.push({
-        name: plugin.name,
-        metadata: plugin.metadata
-      })
+    if (serverPlugins[name]) {
+      usePlugin(serverPlugins[name])
+    } else {
+      usePlugin(plugin)
     }
+  }
 
-    if (plugin.actions) {
-      appMetadata.actions = appMetadata.actions.concat(plugin.actions)
+  // use required client-side plugins
+  for (let i = 0; i < defaultClientPlugins.length; i++) {
+    const plugin = defaultClientPlugins[i]
+    const name = plugin.name
+
+    if (clientPlugins[name]) {
+      useClientPlugin(clientPlugins[name])
+    } else {
+      useClientPlugin(plugin)
+    }
+  }
+
+  // use actions
+  for (let i = 0; i < defaultActions.length; i++) {
+    const action = defaultActions[i]
+    const id = action.id
+
+    if (actions[id]) {
+      useAction(actions[id])
+    } else {
+      useAction(action)
     }
   }
 
   return {
     usePlugin,
+    useClientPlugin,
     useAction,
     setup: initialize(appSetup, appActionData, appDataModels, appMetadata)
   }
