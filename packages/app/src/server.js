622133b18b8fca3bf5caa40fad94cc662db47f16
Thomas David <tom@mightysmile.com>
2024-11-27 21:42:38 +0100

    fix(app): remove redundant package

--- packages/app/src/server.js
+++
@@ -1,8 +0,0 @@
-import createApp from '@dooksa/create-app/server'
-import clientPlugins from '@dooksa/plugins/client'
-import actions from '@dooksa/actions'
-
-export default createApp({
-  clientPlugins,
-  actions
-})
