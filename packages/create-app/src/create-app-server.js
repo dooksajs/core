5469279a01c653f071b280ec85d89e276d8f974c
Thomas David <tom@mightysmile.com>
2024-11-27 21:27:36 +0100

    feat(create-app-server): use a common return shape for append methods

--- packages/create-app/src/create-app-server.js
+++ packages/create-app/src/create-app-server.js
@@ -4,71 +4,10 @@ import defaultServerPlugins, { httpStart } from '@dooksa/plugins/server'
 import defaultActions from '@dooksa/actions'
 
 /**
- * @import {Plugin} from '../../create-plugin/src/index.js'
+ * @import {Plugin, PluginMetadata, ActiveAction} from '../../create-plugin/src/index.js'
+ * @import {AppPlugin} from './append-plugin.js'
  * @import {Action} from '@dooksa/create-action/types'
  */
-
-/**
- * @param {Action[]} appActionData
- */
-function appendAction (appActionData) {
-  /**
-   * @param {Action} action
-   */
-  return (action) => {
-    appActionData.push(action)
-  }
-}
-
-/**
- * Append client plugins
- * @param {Object} appMetadata
- * @param {Array} appMetadata.actions
- * @param {Array} appMetadata.plugins
- */
-function appendClientPlugin (appMetadata) {
-  /**
-   * @param {Plugin} plugin
-   */
-  return function (plugin) {
-    if (plugin.metadata) {
-      appMetadata.plugins.push({
-        name: plugin.name,
-        metadata: plugin.metadata
-      })
-    }
-
-    if (plugin.actions) {
-      appMetadata.actions = appMetadata.actions.concat(plugin.actions)
-    }
-  }
-}
-
-function initialize (appSetup, appActionData, appDataModels, appMetadata) {
-  /**
-   * Initialize server-side Dooksa!
-   * @param {Object} param
-   * @param {Object} [param.options={}]
-   */
-  return ({ options = {} }) => {
-    options.data = appDataModels
-    options.action = { actions: appActionData }
-    options.metadata = appMetadata
-
-    // setup plugins
-    for (let i = 0; i < appSetup.length; i++) {
-      const plugin = appSetup[i]
-
-      plugin.setup(options[plugin.name])
-      appSetup.splice(i, 1)
-      i--
-    }
-
-    appSetup = []
-
-    return httpStart(options.http)
-  }
-}
 
 /**
  * @typedef {Object} ServerPlugins
@@ -108,6 +47,120 @@ function initialize (appSetup, appActionData, appDataModels, appMetadata) {
  */
 
 /**
+ * @typedef {Object} AppClientPlugin
+ * @property {Function} use
+ * @property {{ name: string, metadata: PluginMetadata }[]} metadata
+ * @property {ActiveAction[]} actions
+ */
+
+/**
+ * @typedef {Object} AppAction
+ * @property {Function} use
+ * @property {Action[]} items
+ */
+
+/**
+ * Append actions
+ * @returns {AppAction}
+ */
+function appendAction () {
+  const actions = []
+
+  return {
+    /**
+     * Use action
+     * @param {Action} action
+     */
+    use (action) {
+      actions.push(action)
+    },
+    /**
+     * List of compiled actions
+     * @returns {Action[]}
+     */
+    get items () {
+      return actions
+    }
+  }
+}
+
+/**
+ * Append client plugins
+ * @returns {AppClientPlugin}
+ */
+function appendClientPlugin () {
+  let actions = []
+  const metadata = []
+
+  return {
+    /**
+     * Use client side plugin
+     * @param {Plugin} plugin
+     */
+    use (plugin) {
+      if (plugin.metadata) {
+        metadata.push({
+          name: plugin.name,
+          metadata: plugin.metadata
+        })
+      }
+
+      if (plugin.actions) {
+        actions = actions.concat(plugin.actions)
+      }
+    },
+    /**
+     * Collection of client plugin metadata
+     */
+    get metadata () {
+      return metadata
+    },
+    /**
+     * Collection of client plugin actions
+     */
+    get actions () {
+      return actions
+    }
+  }
+}
+
+/**
+ * Initialize Dooksa!
+ * @param {AppPlugin} serverPlugins
+ * @param {AppClientPlugin} clientPlugins
+ * @param {AppAction} actions
+ */
+function initialize (serverPlugins, clientPlugins, actions) {
+  /**
+   * Initialize server-side Dooksa!
+   * @param {Object} param
+   * @param {Object} [param.options={}]
+   */
+  return ({ options = {} }) => {
+    options.data = serverPlugins.models
+    options.action = { actions: actions.items }
+    options.metadata = {
+      plugins: clientPlugins.metadata,
+      actions: clientPlugins.actions
+    }
+
+    // setup plugins
+    const appSetup = serverPlugins.setup
+
+    for (let i = 0; i < appSetup.length; i++) {
+      const plugin = appSetup[i]
+
+      plugin.setup(options[plugin.name])
+    }
+
+    // clear setup queue
+    serverPlugins.setup = []
+
+    return httpStart(options.http)
+  }
+}
+
+/**
  * Create Dooksa app
  * @param {Object} data
  * @param {ServerPlugins} [data.serverPlugins={}] - Overwrite core server plugins
@@ -119,65 +172,57 @@ export default function createAppServer ({
   serverPlugins = {},
   actions = {}
 } = {}) {
-  const appPlugins = []
-  const appSetup = []
-  const appActionData = []
-  const appMetadata = {
-    plugins: [],
-    actions: []
-  }
-  const appDataModels = {
-    values: {},
-    schema: [],
-    names: []
-  }
-  const usePlugin = appendPlugin(appPlugins, appSetup, appDataModels)
-  const useAction = appendAction(appActionData)
-  const useClientPlugin = appendClientPlugin(appMetadata)
+  const appServerPlugins = appendPlugin()
+  const appClientPlugins = appendClientPlugin()
+  const appActions = appendAction()
 
   // use data
-  usePlugin(data)
+  appServerPlugins.use(data)
 
   // use required server-side plugins
   for (let i = 0; i < defaultServerPlugins.length; i++) {
-    const plugin = defaultServerPlugins[i]
+    let plugin = defaultServerPlugins[i]
     const name = plugin.name
 
     if (serverPlugins[name]) {
-      usePlugin(serverPlugins[name])
-    } else {
-      usePlugin(plugin)
+      plugin = serverPlugins[name]
     }
+
+    appServerPlugins.use(plugin)
   }
 
   // use required client-side plugins
   for (let i = 0; i < defaultClientPlugins.length; i++) {
-    const plugin = defaultClientPlugins[i]
+    let plugin = defaultClientPlugins[i]
     const name = plugin.name
 
     if (clientPlugins[name]) {
-      useClientPlugin(clientPlugins[name])
-    } else {
-      useClientPlugin(plugin)
+      plugin = clientPlugins[name]
     }
+
+    appClientPlugins.use(plugin)
   }
 
   // use actions
   for (let i = 0; i < defaultActions.length; i++) {
-    const action = defaultActions[i]
+    let action = defaultActions[i]
     const id = action.id
 
     if (actions[id]) {
-      useAction(actions[id])
-    } else {
-      useAction(action)
+      action = actions[id]
     }
+
+    appActions.use(action)
   }
 
   return {
-    usePlugin,
-    useClientPlugin,
-    useAction,
-    setup: initialize(appSetup, appActionData, appDataModels, appMetadata)
+    usePlugin: appServerPlugins.use,
+    useClientPlugin: appClientPlugins.use,
+    useAction: appActions.use,
+    setup: initialize(
+      appServerPlugins,
+      appClientPlugins,
+      appActions
+    )
   }
 }
