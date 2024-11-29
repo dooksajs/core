1e4c434a4ae2dafe2c50384ab1acffe40571562f
Thomas David <tom@mightysmile.com>
2024-11-29 17:46:14 +0100

    feat(components): export default components

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -2,6 +2,14 @@ import base from './base/index.js'
 import extra from './extra/index.js'
 import bootstrap from './bootstrap/index.js'
 
+/**
+ * @type {import('@dooksa/create-component').Component[]} List of component IDs
+ */
+export const components = []
+  .concat(base)
+  .concat(extra)
+  .concat(bootstrap)
+
 export {
   base,
   extra,
