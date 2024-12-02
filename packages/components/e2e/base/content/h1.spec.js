8e5e747c2e602666d5242a607b6009b867ac4569
Thomas David <tom@mightysmile.com>
2024-12-02 11:02:21 +0100

    test(e2e): use accessability fixture for header tests

--- packages/components/e2e/base/content/h1.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('has one h1 element', async ({ page }) => {
-  await page.goto('/_/component/h1')
-
-  const elements = await page.locator('h1').count()
-
-  await expect(elements).toBe(1)
-})
