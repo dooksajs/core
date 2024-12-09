77d0709924cfa14fcd9f4b7a0f1edd5efac99c31
Thomas David <tom@mightysmile.com>
2024-12-09 17:06:10 +0100

    fix(fetch): #client imports

--- packages/plugins/src/client/fetch.js
+++ packages/plugins/src/client/fetch.js
@@ -1,5 +1,5 @@
 import { createPlugin } from '@dooksa/create-plugin'
-import { stateGetValue, stateSetValue, stateAddListener } from './data.js'
+import { stateGetValue, stateSetValue, stateAddListener } from '#client'
 
 const fetchRequestQueue = {}
 const fetchRequestCache = {}
