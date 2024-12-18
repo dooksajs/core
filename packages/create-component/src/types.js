a05ef7799f0f1d4af7b32accaa6cc422a8959dfa
Thomas David <tom@mightysmile.com>
2024-12-18 10:58:49 +0100

    types(component): update types for component extension and metadata structure in create-component package
    - Add new `ComponentExtend` type to define how to extend components with properties, mixins, events, and children.
    - Update `ComponentMetadata` to include `parentId`, `mixins`, and more detailed information about the component's origin and capabilities.
    - Introduce a new `ComponentOptionItem` for specifying attribute values, replacements, and toggles in component instances.
    - Enhance `ComponentProperty` to specify node attributes or properties with replacement and toggle options.
    - Update `ComponentInstance` to include parent ID and hash for better tracking of extended components.
    - Add `ComponentMixinMetadata` for metadata about mixins applied to the component.
    - Introduce new event types such as `component/beforeCreate`, `component/created`, etc., in `ComponentEvent`.
    - Improve documentation and comments throughout the codebase.

--- packages/create-component/src/types.js
+++ packages/create-component/src/types.js
@@ -1,10 +1,10 @@
 /**
- * @typedef {'none'|'sm'|'md'|'lg'} BreakpointNoneLg
- * @typedef {'sm'|'md'|'lg'|'always'} BreakpointAlwaysLg
- * @typedef {'sm'|'md'|'lg'|'xl'|'xxl'} BreakpointSmXXL
- * @typedef {'sm'|'md'|'lg'|'xl'|'xxl'|'always'} BreakpointAlwaysXXL
- * @typedef {'10'|'25'|'50'|'75'} Opacity
- * @typedef {'0'|'1'|'2'|'3'|'4'|'5'} Spacer
+ * @typedef {'none'|'sm'|'md'|'lg'} BreakpointNoneLg - A breakpoint with values for none, small, medium, and large.
+ * @typedef {'sm'|'md'|'lg'|'always'} BreakpointAlwaysLg - A breakpoint with values for small, medium, large, and always.
+ * @typedef {'sm'|'md'|'lg'|'xl'|'xxl'} BreakpointSmXXL - A breakpoint with values for small, medium, large, extra-large, and extra-extra-large.
+ * @typedef {'sm'|'md'|'lg'|'xl'|'xxl'|'always'} BreakpointAlwaysXXL - A breakpoint with values for small, medium, large, extra-large, extra-extra-large, and always.
+ * @typedef {'10'|'25'|'50'|'75'} Opacity - An opacity value in percent.
+ * @typedef {'0'|'1'|'2'|'3'|'4'|'5'} Spacer - A spacer value for spacing properties.
  * @typedef {'primary'|
  * 'primarySubtle'|
  * 'secondary'|
@@ -25,7 +25,7 @@
  * 'bodySecondary'|
  * 'bodyTertiary'|
  * 'black'|
- * 'white'} ColorExtra
+ * 'white'} ColorExtra - An extra color value with subtle variants.
  * @typedef {'primary'|
  * 'secondary'|
  * 'success'|
@@ -33,43 +33,47 @@
  * 'warning'|
  * 'info'|
  * 'light'|
- * 'dark'} Color
+ * 'dark'} Color - A base color value.
  */
 
 /**
- * Create a modified component
+ * Extend component
  * @typedef {Object} ComponentExtend
- * @property {ComponentProperty[]} [properties]
- * @property {ComponentMixinMetadata} [metadata]
- * @property {ComponentEvent[]} [events]
- * @property {boolean} [clearDefaultEvents]
- * @property {Object} [options]
- * @property {Array<Component|ComponentInstance>} [children]
+ * @property {ComponentProperty[]} [properties] - Properties to modify the component.
+ * @property {ComponentMixinMetadata} [metadata] - Metadata for mixins applied to the component.
+ * @property {ComponentEvent[]} [events] - Events to add or override.
+ * @property {boolean} [clearDefaultEvents] - Clear default events when extending the component.
+ * @property {Object} [options] - Options for customizing the component.
+ * @property {Array<Component|ComponentInstance>} [children] - Child components to include in the component.
  */
 
 /**
+ * Component data and metadata
  * @typedef {ComponentDataValues & ComponentData & ComponentMetadata} Component
  */
 
 /**
+ * Metadata for a component
  * @typedef {Object} ComponentMetadata
- * @property {'link'|'section'|'img'|'input'|'element'} [type]
- * @property {string} [hash] - Component hash
- * @property {Function} [component] - Lazy load component
- * @property {boolean} [isLoaded] - Marks if web component is loaded
- * @property {string} [parentId] - Parent template Id if component was extended
- * @property {ComponentMixinMetadata[]} [mixins]
+ * @property {'link'|'section'|'img'|'input'|'element'} [type] - Type of the component.
+ * @property {string} [hash] - Hash value associated with the component.
+ * @property {Function} [component] - Lazy load custom web component.
+ * @property {boolean} [isLoaded] - Flag indicating if the web component is loaded.
+ * @property {string} [parentId] - Id of the parent component if the component was extended.
+ * @property {ComponentMixinMetadata[]} [mixins]  - Metadata for mixins applied to the component.
  */
 
 /**
+ * Options data structure
  * @typedef {Object.<string, (Object.<string,ComponentOptionItem>|ComponentOptionItem)>} ComponentOption
  */
 
 /**
+ * Option item for a specific property
  * @typedef {Object} ComponentOptionItem
- * @property {string} name - Name of attribute/setter
- * @property {boolean} [replace] - Replace property value
- * @property {boolean} [toggle] - toggle property value
+ * @property {string} name - Name of the attribute or setter.
+ * @property {boolean} [replace] - Replace property value.
+ * @property {boolean} [toggle] - Toggle property value.
  * @property {Object.<string, (string|boolean)>} [values] - Attribute values
  * @property {string|boolean} [value] - Attribute value
  * @property {Function} [computedValue]
@@ -77,96 +81,107 @@
  */
 
 /**
+ * Component property modification behaviour
  * @typedef {Object} ComponentProperty
- * @property {string} name - Name of node property
- * @property {boolean} [replace] - Join value
- * @property {boolean} [toggle] - toggle property value
- * @property {string|boolean} value - Node value
+ * @property {string} name - Name of element attribute or property.
+ * @property {boolean} [replace] - Replace property value.
+ * @property {boolean} [toggle] - Toggle property value.
+ * @property {string|boolean} value - Property value.
  */
 
 /**
+ * Component instance data
  * @typedef {Object} ComponentInstance
- * @property {string} id - Component instance Id
- * @property {'link'|'section'|'img'|'input'|'element'} [type]
+ * @property {string} id - Unique identifier for the component instance.
+ * @property {'link'|'section'|'img'|'input'|'element'} [type] - Type of the component instance.
  * @property {string} [parentId] - The parent component Id if instance was extended
- * @property {string} hash - Hash of parent component
- * @property {Array<Component|ComponentInstance|string>} [children]
- * @property {ComponentEvent[]} [events]
- * @property {ComponentProperty[]} [properties]
+ * @property {string} hash - Hash value associated with the parent component.
+ * @property {Array<Component|ComponentInstance|string>} [children] - Child components or IDs.
+ * @property {ComponentEvent[]} [events] - Events attached to the component instance.
+ * @property {ComponentProperty[]} [properties] - Properties of the component instance.
  */
 
 /**
+ * Metadata for a mixin
  * @typedef {Object} ComponentMixinMetadata
- * @property {string} id
- * @property {string} [hash]
+ * @property {string} id - Identifier for the mixin.
+ * @property {string} [hash] - Hash value associated with the mixin.
  */
 
 /**
+ * Base event listener types
  * @typedef {'component/beforeCreate'|
  * 'component/created'|
  * 'component/mounted'|
  * 'component/childrenAfterUpdate'|
- * 'component/childrenBeforeUpdate'} ComponentEventOn
+ * 'component/childrenBeforeUpdate'} ComponentEventOn - Base events listeners for components.
  */
 
 /**
+ * Event definition for a component
  * @typedef {Object} ComponentEvent
- * @property {ComponentEventOn} on
- * @property {string} actionId
+ * @property {ComponentEventOn | string} on - Event type
+ * @property {string} actionId - Unique identifier for the event action.
  */
 
 /**
+ * Data values and metadata for a component
  * @typedef {Object} ComponentDataValues
- * @property {'link'|'section'|'img'|'input'|'element'} [type]
- * @property {Array<Component|ComponentInstance|string>} [children] - Child components
- * @property {Array<Component|ComponentInstance|string>} [allowedChildren] - Permitted nested components
+ * @property {'link'|'section'|'img'|'input'|'element'} [type] - Type of the component.
+ * @property {Array<Component|ComponentInstance|string>} [children] - Child components or IDs.
+ * @property {Array<Component|ComponentInstance|string>} [allowedChildren] - Allowed nested components.
  * @property {Object[]} [content] - Define content structure
  * @property {string} content[].name - Name of content property
  * @property {string} content[].nodePropertyName - Property name
- * @property {ComponentEvent[]} [events]
- * @property {Object.<string, boolean>} [eventTypes]
- * @property {ComponentProperty[]} [properties]
- * @property {ComponentOption} [options]
- * @property {Object[]} [styles]
- * @property {string} styles[].name - Style property name
+ * @property {ComponentEvent[]} [events] - Events attached to the component.
+ * @property {Object.<string, boolean>} [eventTypes] - Event types enabled or disabled.
+ * @property {ComponentProperty[]} [properties] - Properties of the component.
+ * @property {ComponentOption} [options] - Options for customizing the component.
+ * @property {Object[]} [styles] - Define styling properties.
+ * @property {string} styles[].name - Style property name.
  * @property {'unit'|'box-shadow'|'font-family'|'rgba'|'units'|'number'} styles[].type - Style property name
  */
 
 /**
+ * @import {EventEmit} from '../../plugins/src/client/event.js'
  * @callback EventEmitCallback
- * @param {import('../../plugins/src/event.js').EventEmit} param
+ * @param {EventEmit} param
  */
 
 /**
+ * Initialize a template with components and events
  * @callback TemplateInitialize
- * @param {Object} component
- * @param {string} component.id
- * @param {string} [component.parentId]
- * @param {string} [component.rootId]
- * @param {string} [component.groupId]
- * @param {Component} [component.template]
- * @param {EventEmitCallback} emit
+ * @param {Object} component - Component data to initialize.
+ * @param {string} component.id - Component ID.
+ * @param {string} [component.parentId] - Parent component ID if extending.
+ * @param {string} [component.rootId] - Root template ID.
+ * @param {string} [component.groupId] - Group template ID.
+ * @param {Component} [component.template] - Template component data.
+ * @param {EventEmitCallback} emit - Callback function for emitting events.
  */
 
 /**
+ * Data structure for a component
  * @typedef {Object} ComponentData
- * @property {string} id - Component id
- * @property {Function} [component] - Lazy load component
- * @property {TemplateInitialize} [initialize] - Constructor function to create a component instance
- * @property {string} tag - Element tag name
+ * @property {string} id - Unique identifier for the component.
+ * @property {Function} [component] - Lazy load component function.
+ * @property {TemplateInitialize} [initialize] - Constructor function to create a component instance.
+ * @property {string} tag - Element tag name.
  */
 
 /**
+ * Infix definition with base class, possible infixes, separator, and suffix
  * @typedef {Object} Infix
- * @property {string} className - Base class name
- * @property {InfixItem[]} infixes - Possible class affixes
- * @property {string} [separator='-'] - Infix separator
- * @property {string} [suffix] - Suffix value
+ * @property {string} className - Base class name for the component.
+ * @property {InfixItem[]} infixes - Possible class names that may include breakpoints.
+ * @property {string} [separator='-'] - Separator between infixes.
+ * @property {string} [suffix] - Suffix value after infixes.
  */
 
 /**
+ * Infix item with values, name, and separator
  * @typedef {Object} InfixItem
- * @property {Object.<string, string>} [values] - A map of all possible infix values
- * @property {string} name - The key name to match the possible values
- * @property {string} [separator] - Infix instance separator
+ * @property {Object.<string, string>} [values] - A map of all possible infix values.
+ * @property {string} name - The key name to match the possible values.
+ * @property {string} [separator] - Separator within an infix item.
  */
