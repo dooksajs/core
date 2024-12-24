20b5eec2cd9d844fb44bfebb1af3216b5185230d
Thomas David <tom@mightysmile.com>
2024-12-24 08:02:43 +0100

    feat(components): new state select collection component

---
+++ packages/components/src/bootstrap/state/state-select-collection.js
@@ -0,0 +1,85 @@
+import { createIcon } from '#extra'
+import {
+  createSelect,
+  createDiv,
+  createOption,
+  createText,
+  createLabel
+} from '@dooksa/components/base'
+import labelRequiredStar from '../form/label-required-star.js'
+
+export default createDiv({
+  metadata: { id: 'state-select-collection' },
+  children: [
+    createLabel({
+      children: [
+        createIcon({
+          options: {
+            icon: 'mdi:folder-search',
+            margin: [{
+              direction: 'end',
+              strength: '2'
+            }]
+          }
+        }),
+        createText({
+          options: {
+            value: 'Collections'
+          }
+        }),
+        labelRequiredStar
+      ],
+      events: [
+        {
+          on: 'component/created',
+          actionId: 'label-html-for'
+        }
+      ]
+    }),
+    createDiv({
+      children: [
+        createSelect({
+          children: [
+            createOption({
+              children: [
+                createText({
+                  options: { value: '- Select collection -' }
+                })
+              ],
+              options: {
+                selected: true,
+                value: ''
+              }
+            })
+          ],
+          options: {
+            name: 'name',
+            required: true
+          },
+          events: [
+            {
+              on: 'component/created',
+              actionId: 'input-id'
+            },
+            {
+              on: 'component/created',
+              actionId: 'action-input-name-prefix-action'
+            },
+            {
+              on: 'component/created',
+              actionId: 'state-select-collection-options'
+            }
+          ]
+        })
+      ],
+      options: {
+        margin: [
+          {
+            direction: 'start',
+            strength: '4'
+          }
+        ]
+      }
+    })
+  ]
+})
