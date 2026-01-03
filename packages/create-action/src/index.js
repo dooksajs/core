2bb56d5ecc196217641896cdfe8bf534a38a6113
Thomas David <tom@mightysmile.com>
2026-01-03 13:29:24 +0100

    docs(create-action): add module documentation and update types export path
    Add JSDoc module documentation to describe the package purpose. Update types export path to use a more explicit directory structure.

--- packages/create-action/src/index.js
+++ packages/create-action/src/index.js
@@ -1,8 +1,13 @@
+/**
+ * @module @dooksa/create-action
+ * @description Action compilation and parsing utilities for the Dooksa framework
+ */
+
 import availableMethods from './available-methods.js'
 import compileAction from './compile-action.js'
 import createAction from './create-action.js'
 
-export * from './types.js'
+export * from '../types/index.js'
 
 export {
   createAction,
