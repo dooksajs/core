a69a5dd613b7ab512a75516df11c9110d1c57bb7
Thomas David <tom@mightysmile.com>
2026-01-03 13:25:07 +0100

    docs: add token_textContent action method and JSDoc type definitions

--- packages/create-action/src/available-methods.js
+++ packages/create-action/src/available-methods.js
@@ -1,3 +1,51 @@
+/**
+ * @typedef {Object} AvailableMethods
+ * @description Object mapping of all available action method names to true.
+ * Used by parseAction and createAction to identify valid action methods.
+ * @property {boolean} action_dispatch - Dispatch an action
+ * @property {boolean} action_getValue - Get action value
+ * @property {boolean} action_getContextValue - Get action context value
+ * @property {boolean} action_getPayloadValue - Get action payload value
+ * @property {boolean} variable_getValue - Get variable value
+ * @property {boolean} variable_setValue - Set variable value
+ * @property {boolean} component_remove - Remove component
+ * @property {boolean} component_renderChildren - Render component children
+ * @property {boolean} state_addListener - Add state listener
+ * @property {boolean} state_deleteListener - Delete state listener
+ * @property {boolean} state_deleteValue - Delete state value
+ * @property {boolean} state_find - Find state value
+ * @property {boolean} state_generateId - Generate state ID
+ * @property {boolean} state_getSchema - Get state schema
+ * @property {boolean} state_getValue - Get state value
+ * @property {boolean} state_setValue - Set state value
+ * @property {boolean} editor_getSchema - Get editor schema
+ * @property {boolean} icon_render - Render icon
+ * @property {boolean} fetch_getAll - Fetch all items
+ * @property {boolean} fetch_getById - Fetch by ID
+ * @property {boolean} list_filter - Filter list
+ * @property {boolean} list_map - Map list
+ * @property {boolean} list_indexOf - Get list index
+ * @property {boolean} list_push - Push to list
+ * @property {boolean} list_sort - Sort list
+ * @property {boolean} list_splice - Splice list
+ * @property {boolean} operator_compare - Compare values
+ * @property {boolean} operator_eval - Evaluate operator
+ * @property {boolean} query_fetch - Fetch query
+ * @property {boolean} query_filter - Filter query
+ * @property {boolean} route_currentId - Get current route ID
+ * @property {boolean} route_currentPath - Get current route path
+ * @property {boolean} route_navigate - Navigate to route
+ * @property {boolean} token_textContent - Token text content
+ * @property {boolean} string_replace - Replace string
+ * @property {boolean} regex_pattern - Regex pattern
+ */
+
+/**
+ * Object mapping of all available action method names to true.
+ * Used by parseAction and createAction to identify valid action methods.
+ *
+ * @type {AvailableMethods}
+ */
 const availableMethods = {
   action_dispatch: true,
   action_getValue: true,
@@ -32,6 +80,7 @@ const availableMethods = {
   route_currentId: true,
   route_currentPath: true,
   route_navigate: true,
+  token_textContent: true,
   string_replace: true,
   regex_pattern: true
 }
