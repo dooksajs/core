aa56162fdc07d64ef53c4567f0e2217b048c61cf
Thomas David <tom@mightysmile.com>
2026-01-15 06:27:02 +0100

    feat(playground): update server plugin import path
    Changed the import path for the `development` plugin from `@dooksa/plugins/server` to `@dooksa/plugins/dev`. This aligns the import with the correct package structure.

--- packages-private/playground/src/app-server.js
+++ packages-private/playground/src/app-server.js
@@ -1,5 +1,5 @@
 import createApp from '@dooksa/create-app/server'
-import { development } from '@dooksa/plugins/server'
+import { development } from '@dooksa/plugins/dev'
 
 export default async function createAppServer (options) {
   const app = createApp()
