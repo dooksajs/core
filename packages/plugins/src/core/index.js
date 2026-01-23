292e19d0cbe091e2ef95fdbd3c3162c881283d9c
Thomas David <tom@mightysmile.com>
2026-01-23 08:15:39 +0100

    feat(plugins): import core plugins

---
+++ packages/plugins/src/core/index.js
@@ -0,0 +1,46 @@
+export {
+  state,
+  stateAddListener,
+  stateDeleteListener,
+  stateDeleteValue,
+  stateFind,
+  stateGenerateId,
+  stateGetSchema,
+  stateGetValue,
+  stateSetValue,
+  stateUnsafeSetValue
+} from './state.js'
+
+export {
+  action,
+  actionDispatch,
+  actionGetContextValue,
+  actionGetPayloadValue,
+  actionGetValue,
+  actionIfElse
+} from './action.js'
+
+export {
+  list,
+  listFilter,
+  listIndexOf,
+  listMap,
+  listPush,
+  listSort,
+  listSplice
+} from './list.js'
+
+export {
+  error,
+  errorClearErrors,
+  errorGetErrorCount,
+  errorGetErrors,
+  errorLogError
+} from './error.js'
+
+export { api, apiGetAll, apiGetById } from './api.js'
+export { event, eventEmit } from './event.js'
+export { metadata } from './metadata.js'
+export { operator, operatorCompare, operatorEval } from './operator.js'
+export { string, stringReplace } from './string.js'
+export { variable, variableGetValue, variableSetValue } from './variable.js'
