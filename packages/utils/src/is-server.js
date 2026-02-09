676c5603653baaccb476b60464ba65e902cc9bb4
Thomas David <tom@mightysmile.com>
2026-02-09 22:33:51 +0100

    feat(utils): add JSDoc documentation to isEnvServer function

--- packages/utils/src/is-server.js
+++ packages/utils/src/is-server.js
@@ -1,3 +1,7 @@
+/**
+ * Checks if the code is running in a server environment
+ * @returns {boolean} True if running in a server environment
+ */
 export default function isEnvServer () {
   return (typeof process !== 'undefined') && (typeof process.versions.node !== 'undefined')
 }
